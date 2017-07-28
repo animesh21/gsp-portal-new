2aac9c40a15d772f126031d500dd5c5b4edc8ccd
Animesh Kumar <animesh0721@gmail.com>
2017-07-28 18:02:58 +0530

    Removes CreateUser bug

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,22 +2,25 @@
 header('Access-Control-Allow-Origin: *');
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class User_model extends CI_Model {
-	public function __construct() {
+class User_model extends CI_Model
+{
+    public function __construct()
+    {
         parent::__construct();
     }
 
-	public function UserLogin($argPost) {
+    public function UserLogin($argPost)
+    {
         $query = $this->db->select('*')
-                ->from('gsp_user')
-                ->where(array('email' => $argPost['email'], 'password' => $argPost['password'], 'status' => '0'))
-                ->get();
+            ->from('gsp_user')
+            ->where(array('email' => $argPost['email'], 'password' => $argPost['password'], 'status' => '0'))
+            ->get();
         if ($query->num_rows() > 0) {
             $row = $query->row();
             $userData = array(
                 'USERNAME' => $row->username,
                 'USER_ID' => $row->id,
-                'status'=> $row->status,
+                'status' => $row->status,
                 'lastid' => $row->lastQuestionId
             );
             $this->session->set_userdata($userData);
@@ -25,95 +28,102 @@ class User_model extends CI_Model {
         }
         return false;
     }
+
     public function CreateUser($argPost)
     {
         $query = $this->db->select('COUNT(*) count')
-                ->from('gsp_user')
-                ->get();
-        
+            ->from('gsp_user')
+            ->get();
+
         $row = $query->row();
-        
-         $update = array(
-                'email' => $argPost['email'],
-                'password' => $argPost['Password'],
-                'username' => $argPost['username']
-                
-                );
-                $this->db->insert('gsp_user',$update);//insert if does not exist
-                $query = $this->db->select('COUNT(*) count')
-                        ->from('gsp_user')
-                        ->get();
-                $row = $query->row();       
-                return $row->count;
+
+        $update = array(
+            'email' => $argPost['email'],
+            'password' => $argPost['Password'],
+            'username' => $argPost['username']
+        );
+        $this->db->insert('gsp_user', $update);//insert if does not exist
+
+        $query = $this -> db
+                       -> select('id')
+                       -> from('gsp_user')
+                       -> where('email', $argPost['email'])
+                       -> get();
+        $row = $query -> row();
+        return $row -> id;
     }
-    public function Login($argPost) {
+
+    public function Login($argPost)
+    {
         $query = $this->db->select('*')
-                ->from('gsp_user')
-                ->where(array('email' => $argPost['email'], 'password' => $argPost['password'], 'status' => '0'))
-                ->get();
+            ->from('gsp_user')
+            ->where(array('email' => $argPost['email'], 'password' => $argPost['password'], 'status' => '0'))
+            ->get();
         if ($query->num_rows() > 0) {
             $row = $query->row();
             return (int)$row->id;
         }
         return null;
     }
+
     /*
      * Get States
      */
     public function getStates()
     {
-        $states=array(''=>'Select State');
-        $data=$this->db->get_where('states', array('country_id =' => '1'))->result();
-        foreach($data as $d)
-        {
-            $states[$d->id]=$d->name;
+        $states = array('' => 'Select State');
+        $data = $this->db->get_where('states', array('country_id =' => '1'))->result();
+        foreach ($data as $d) {
+            $states[$d->id] = $d->name;
         }
         return $states;
     }
-    
+
     /*
      * Get Cities
      */
     public function getCIties($argID)
     {
-        $cities=array();
-        $data=$this->db->get_where('cities', array('state_id =' => $argID))->result();
+        $cities = array();
+        $data = $this->db->get_where('cities', array('state_id =' => $argID))->result();
         return $data;
     }
- 
+
     /*
      * Get Cities All New Added Code By Siddhant
      */
     public function getCitiesAll($argID)
     {
-        $cities=array();
-        $data=$this->db->get_where('cities', array('state_id =' => $argID))->result();
+        $cities = array();
+        $data = $this->db->get_where('cities', array('state_id =' => $argID))->result();
         //echo $this->db->last_query(); exit;
-        foreach($data as $d)
-        {
-            $cities[$d->id]=$d->name;
+        foreach ($data as $d) {
+            $cities[$d->id] = $d->name;
         }
         return $cities;
     }
-	public function ChangePassword($pass)
+
+    public function ChangePassword($pass)
     {
-        $this->db->set('password',$pass['pass'])  
-        ->where( array('id' => $this->session->userdata('USER_ID'))) //which row want to upgrade  
-        ->update('gsp_user');
+        $this->db->set('password', $pass['pass'])
+            ->where(array('id' => $this->session->userdata('USER_ID')))//which row want to upgrade
+            ->update('gsp_user');
         print_r("Update");
         $this->session->sess_destroy();
         return true;
     }
-	function CheckPassword($pass)
-	{
-		$current = $this->db->select('*')->where( 'id' , $this->session->userdata('USER_ID'))->from('gsp_user')->get()->row()->password;
-        if($current == $pass['oldPassword'])
-			return true;
-		else
-			return false;
-	}
 
-    public function forgetPassword() {
+    function CheckPassword($pass)
+    {
+        $current = $this->db->select('*')->where('id', $this->session->userdata('USER_ID'))->from('gsp_user')->get()->row()->password;
+        if ($current == $pass['oldPassword'])
+            return true;
+        else
+            return false;
+    }
+
+    public function forgetPassword()
+    {
         //echo $argEmail; exit;
         $this->load->helper('string');
         $email_count = $this->db->get_where('gsp_user', array('email' => $this->input->post('val')))->num_rows();
@@ -129,7 +139,7 @@ class User_model extends CI_Model {
                 $config['mailtype'] = 'html';
                 $this->email->initialize($config);
                 $from = "info@greenschoolsprogramme.org";
-                $to=$this->input->post('email');
+                $to = $this->input->post('email');
                 $subject = "GSP Forget Password";
                 $msg = "Dear &nbsp;";
                 $msg .= $this->input->post('email') . "," . "<br>";
@@ -151,4 +161,5 @@ class User_model extends CI_Model {
         }
     }
 }
+
 ?>
