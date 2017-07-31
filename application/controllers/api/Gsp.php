4aa3d53ace581a53433586201db31769a91a7183
Animesh Kumar <animesh0721@gmail.com>
2017-07-31 12:04:31 +0530

    Adds access-control-allow-origin headers

--- application/controllers/api/Gsp.php
+++ application/controllers/api/Gsp.php
@@ -1,7 +1,7 @@
 <?php
-//header('Access-Control-Allow-Origin: *');
-//header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-//header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 
 
 defined('BASEPATH') OR exit('No direct script access allowed');
