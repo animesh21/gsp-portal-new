f4f604fc3e416e1c5f8e0b2810bd1c3d18394a9e
Animesh Kumar <animesh0721@gmail.com>
2017-08-09 16:38:45 +0530

    Changes by Siddhartha Sir

--- application/controllers/Waste.php
+++ application/controllers/Waste.php
@@ -2,24 +2,24 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Waste extends CI_Controller {
-	
-	public function __construct() {
+
+    public function __construct() {
         parent::__construct();
         $this->load->helper(array('form', 'security', 'common_helper'));
         $this->load->library('form_validation');
         $this->load->model('Answer_model');
-    	$user = 1;
-    	if ($this->session->userdata('USER_ID') == '') {
-        
+        $user = 1;
+        if ($this->session->userdata('USER_ID') == '') {
+
             redirect('login');
 
         }
     }
 
     public function index() {
-    	$data['title'] = 'Waste';
+        $data['title'] = 'Waste';
         if ($this->session->userdata('USER_ID') == '') {
-        
+
             redirect('login');
 
         }
@@ -46,33 +46,33 @@ class Waste extends CI_Controller {
             ('USER_ID'), 'Pictures_of_Audit_Team');
 //            print_r($data['other']['Q1G1']);
 //            print_r($data['other']['Q1G2']);
-            
+
             if(isset($data['other']['Q1G1']) && isset($data['other']['Q1G2']))
-            {	 
-            	if($data['other']['Q1G1'] < 6 && $data['other']['Q1G2'] < 6)
-		 {  
-			$this->load->view('PrimaryWaste',$data);
-		 }
-		else
-		{
-			$this->load->view('waste',$data);
-		}
-	     }	
+            {
+                if($data['other']['Q1G1'] < 6 && $data['other']['Q1G2'] < 6)
+                {
+                    $this->load->view('PrimaryWaste',$data);
+                }
+                else
+                {
+                    $this->load->view('waste',$data);
+                }
+            }
             else
             {
-	    	$this->load->view('waste',$data);//$this->load->view('waste',$data); this is where problem was, I told you that 2 views might be loaded.
-	    }
-	  }
+                $this->load->view('waste',$data);//$this->load->view('waste',$data); this is where problem was, I told you that 2 views might be loaded.
+            }
+        }
     }
-   public function set()
+    public function set()
     {
-        
+
         $post = $this->input->post();
         $this->Answer_model->submitAnswers($post,7);
-	updateProgress($this->session->userdata('USER_ID'), 100);   
+        updateProgress($this->session->userdata('USER_ID'), 100);
         //print_r($post);
-        redirect(base_url("school"));
+        redirect(base_url("feedback"));
     }
-    
-	
+
+
 }
