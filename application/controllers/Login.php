8fbbc06de6d2570ab363a229ad53c1f5627ab853
Siddhant Kulshreshtha <sidd_013@rediffmail.com>
2017-08-24 15:48:27 +0530

    Adding Erro Messages On Wrong Username Password

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -17,20 +17,18 @@ class Login extends CI_Controller {
         //Validation
         $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
         $this->form_validation->set_rules('password', 'Password', 'trim|required');
-		$this->form_validation->set_rules('state', 'State', 'required');
-		
         if ($this->form_validation->run() == FALSE) {
             $this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
         } else {
             $post = $this->security->xss_clean($this->input->post());
             $varCheckLogin = $this->User_model->UserLogin($post);
-
+            
             if ($varCheckLogin) {
                 redirect(base_url('school'));
             } else {
                 $this->session->set_flashdata('error', 'Invalid Email/Password!');
             }
-            print_r(1);
+            
         }
         $this->load->view('login', $data);
     }
@@ -39,6 +37,7 @@ class Login extends CI_Controller {
 
         $data['states'] = $this->User_model->getStates();
         $this->form_validation->set_rules('captcha', 'Captcha', 'callback_validate_captcha');
+        $this->form_validation->set_rules('state', 'State', 'required');
         if ($this->form_validation->run() == FALSE) {
             $this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
             $this->load->helper('captcha');
