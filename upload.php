7dcd008b5035c4541de8d446a290bf41c00db57a
Animesh Kumar <animesh0721@gmail.com>
2017-09-02 19:28:30 +0530

    Removes echo

--- upload.php
+++ upload.php
@@ -1,5 +1,5 @@
 <?php
-echo 'hi'; exit;
+
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
