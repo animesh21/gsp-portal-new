e1b2bccf7f764df02b15ef47fc1ec5a13a2d991e
Animesh Kumar <animesh0721@gmail.com>
2017-09-02 18:40:44 +0530

    Adds upload script for mobile uploads to be saved

---
+++ upload.php
@@ -0,0 +1,35 @@
+<?php
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+/*
+ * Access-Control headers are received during OPTIONS requests
+ */
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
+$varParams = json_decode(file_get_contents('php://input'), true);
+if (!empty($varParams['profile_image'])) {
+//    $rand = rand();
+//    $imagename = $rand.'.jpeg';
+    $imagename = $varParams['fileName'];
+    $destination = './uploads/files/'.$imagename;
+    file_put_contents($destination, base64_decode($varParams['profile_image']));
+//    $new_image_name = $_FILES["file"]["name"];
+//    $tmpFilePath = $_FILES['file']['tmp_name'];
+//    $filePath = "./uploads/";
+//    move_uploaded_file($tmpFilePath, $filePath . $new_image_name);
+}
+?>
