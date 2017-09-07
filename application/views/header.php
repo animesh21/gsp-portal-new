25481fd93f2daa3a24c8803fc5f1dab18008c3fd
Sidd024 <siddhartha2488@gmail.com>
2017-09-07 07:04:45 -0700

    GoogleAnalyticsObject Code
    GoogleAnalyticsObject Code ADD IN HEADER.PHP LINE 29

--- application/views/header.php
+++ application/views/header.php
@@ -15,6 +15,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/css/custom.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/css/responsive.css" rel="stylesheet">
+
+<!---GoogleAnalyticsObject Code ---->		
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-106078591-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
     <style>
         #myBtn {
             cursor: pointer;
