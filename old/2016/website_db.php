e8e718697a83011320b0c806138c4b19aaff15a9
guru-vachan <guruvachanj@gmail.com>
2017-09-27 14:33:41 +0530

    Update website_db.php

--- old/2016/website_db.php
+++ old/2016/website_db.php
@@ -1,8 +1,8 @@
 <?php
 
 define("DB_Hostname", "localhost");
-define("DB_Username", "gspaudit17_usr");
-define("DB_Password", "Sma46o!u8");
+define("DB_Username", "audit2016");
+define("DB_Password", "ByJ^zyY7K(m");
 define("DB_databasename", "gsp_audit16");
 
 $link = mysql_connect(DB_Hostname, DB_Username, DB_Password) or die('Error connecting to mysql');
