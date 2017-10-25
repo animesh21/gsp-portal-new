6902e303cb6a541708e38e2a7946a4ea86a0bebf
guru-vachan <guruvachanj@gmail.com>
2017-10-25 14:06:21 +0530

    Update air.php

--- application/views/admin/survey/air.php
+++ application/views/admin/survey/air.php
@@ -495,7 +495,7 @@
             <tbody>
                 <?php foreach ($arrImages as $a) { ?>
                     <tr>
-                        <td><img src="<?php echo base_url() ?>uploads/files/<?php echo $a->file_name ?>" class="img-responsive" width="100" height="100" /></td>
+                       <td> <a href="<?php echo base_url() ?>uploads/files/Aditi_Primary_PUC_Certificate_wind-mill_1.jpg" download="Aditi_Primary_PUC_Certificate_wind-mill_1.jpg"><img src="<?php echo base_url() ?>uploads/files/<?php echo $a->file_name ?>" class="img-responsive" width="100" height="100" /></a></td>
                         <td><?php echo str_replace(str_replace(' ', '_', $school->name . "_PUC_Certificate_"), " ", $a->file_name); ?></td>
                         <td><a href="<?php echo base_url() ?>admin/audit_started_2017/download_file/.<?php echo $a->file_name; ?>"><span class="glyphicon glyphicon-download-alt"></span></a></td>
                        
