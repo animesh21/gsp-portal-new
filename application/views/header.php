26a0c374dd2cd18946bca6153f30450516f5dc60
Sidd024 <siddhartha2488@gmail.com>
2017-11-02 06:06:08 -0700

    Open The Feedback Option
    Open The Feedback Option

--- application/views/header.php
+++ application/views/header.php
@@ -145,7 +145,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         id="link" >Waste
                     </li>
                     <li class="row <?php echo ($this->router->fetch_class() == 'feedback') ? "current" : "missing"; ?>"
-                        id="link">
+                        id="link" onClick="javascript:window.location.href='<?php echo base_url("feedback") ?>';">
                         Feedback
                     </li>
                 </ol>
