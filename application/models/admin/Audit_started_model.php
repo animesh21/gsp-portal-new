2027f41abe2c46d2d6aa0581466f8ba08816d1e6
Sidd024 <siddhartha2488@gmail.com>
2017-11-06 01:47:28 -0800

    Get All Excel By Condition
    Get All Excel By Condition

--- application/models/admin/Audit_started_model.php
+++ application/models/admin/Audit_started_model.php
@@ -114,6 +114,153 @@ class Audit_started_model extends CI_Model {
 
         return $output;
     }
+	
+    /*
+     * Generate Excel Data
+     */
+
+    public function getExcelDataByProgress($progress) {
+        $output = "";
+        $arrRecord = $this->db->select('a.*, b.name AS state_name, c.name AS district_name, d.password')
+                        ->from('gsp_school AS a')
+                        ->join('states AS b', 'a.state=b.id', 'left')
+                        ->join('cities AS c', 'a.district=c.id', 'left')
+                        ->join('gsp_user AS d', 'a.userid=d.id', 'left')
+                        ->where($progress)
+						->order_by('a.id', 'desc')
+                        ->get()->result();
+        //echo '<pre>'; print_r($arrRecord); exit;
+        $k = 1;
+        $isdCode = '+91';
+        $output .= '"S.No",';
+        $output .= '"School ID",';
+        $output .= '"School Name",';
+        $output .= '"Address1",';
+        $output .= '"Address2",';
+        $output .= '"Country",';
+        $output .= '"State",';
+        $output .= '"District",';
+        $output .= '"City",';
+        $output .= '"Pincode",';
+        $output .= '"ISD Code",';
+        $output .= '"STD Code",';
+        $output .= '"Landline Number",';
+        $output .= '"School Email",';
+        $output .= '"Principal Name",';
+        $output .= '"Principal Mobile",';
+        $output .= '"Coordinator Name",';
+        $output .= '"Coordinator Email",';
+        $output .= '"Coordinator Mobile",';
+        $output .= '"Password",';
+        $output .= '"Date & Time",';
+        $output .= '"Completeness",';
+        $output .= "\n";
+        foreach ($arrRecord as $a) {
+            $output .= '"' . $k . '",';
+            $output .= '"' . $a->id . '",';
+            $output .= '"' . $a->name . '",';
+            $output .= '"' . $a->address1 . '",';
+            $output .= '"' . $a->address2 . '",';
+            $output .= '"' . $a->country . '",';
+            $output .= '"' . $a->state_name . '",';
+            $output .= '"' . $a->district_name . '",';
+            $output .= '"' . $a->city . '",';
+            $output .= '"' . $a->pincode . '",';
+            $output .= '"' . $isdCode . '",';
+            $output .= '"' . $a->std . '",';
+            $output .= '"' . $a->telephone . '",';
+            $output .= '"' . $a->schoolemail . '",';
+            $output .= '"' . $a->principle_name . '",';
+            $output .= '"' . $a->mobile . '",';
+            $output .= '"' . $a->coname . '",';
+            $output .= '"' . $a->coemail . '",';
+            $output .= '"' . $a->comobile . '",';
+            $output .= '"' . $a->password . '",';
+            //$output .='"'.date('d-m-Y H:i:s', strtotime($row['datetime'])).'",';
+            $output .= '"' . date('Y-m-d H:i:s', strtotime($a->date_added)) . '",';
+            $output .= '"' . $a->progress . '%",';
+            $output .= "\n";
+            $k++;
+        }
+
+        return $output;
+    }
+
+    /*
+     * Generate Excel Data
+     */
+
+    public function getExcelDataByProgress1($progress) {
+        $output = "";
+        $arrRecord = $this->db->select('a.*, b.name AS state_name, c.name AS district_name, d.password')
+                        ->from('gsp_school AS a')
+                        ->join('states AS b', 'a.state=b.id', 'left')
+                        ->join('cities AS c', 'a.district=c.id', 'left')
+                        ->join('gsp_user AS d', 'a.userid=d.id', 'left')
+						->join('gsp_aduit_submitted AS e', 'a.userid=e.userid', 'left')
+                        ->where($progress)
+						->where("e.status='1'")
+						->order_by('a.id', 'desc')
+                        ->get()->result();
+        //echo '<pre>'; print_r($arrRecord); exit;
+        $k = 1;
+        $isdCode = '+91';
+        $output .= '"S.No",';
+        $output .= '"School ID",';
+        $output .= '"School Name",';
+        $output .= '"Address1",';
+        $output .= '"Address2",';
+        $output .= '"Country",';
+        $output .= '"State",';
+        $output .= '"District",';
+        $output .= '"City",';
+        $output .= '"Pincode",';
+        $output .= '"ISD Code",';
+        $output .= '"STD Code",';
+        $output .= '"Landline Number",';
+        $output .= '"School Email",';
+        $output .= '"Principal Name",';
+        $output .= '"Principal Mobile",';
+        $output .= '"Coordinator Name",';
+        $output .= '"Coordinator Email",';
+        $output .= '"Coordinator Mobile",';
+        $output .= '"Password",';
+        $output .= '"Date & Time",';
+        $output .= '"Completeness",';
+        $output .= "\n";
+        foreach ($arrRecord as $a) {
+            $output .= '"' . $k . '",';
+            $output .= '"' . $a->id . '",';
+            $output .= '"' . $a->name . '",';
+            $output .= '"' . $a->address1 . '",';
+            $output .= '"' . $a->address2 . '",';
+            $output .= '"' . $a->country . '",';
+            $output .= '"' . $a->state_name . '",';
+            $output .= '"' . $a->district_name . '",';
+            $output .= '"' . $a->city . '",';
+            $output .= '"' . $a->pincode . '",';
+            $output .= '"' . $isdCode . '",';
+            $output .= '"' . $a->std . '",';
+            $output .= '"' . $a->telephone . '",';
+            $output .= '"' . $a->schoolemail . '",';
+            $output .= '"' . $a->principle_name . '",';
+            $output .= '"' . $a->mobile . '",';
+            $output .= '"' . $a->coname . '",';
+            $output .= '"' . $a->coemail . '",';
+            $output .= '"' . $a->comobile . '",';
+            $output .= '"' . $a->password . '",';
+            //$output .='"'.date('d-m-Y H:i:s', strtotime($row['datetime'])).'",';
+            $output .= '"' . date('Y-m-d H:i:s', strtotime($a->date_added)) . '",';
+            $output .= '"' . $a->progress . '%",';
+            $output .= "\n";
+            $k++;
+        }
+
+        return $output;
+    }
+
+    	
+	
 
     /*
      * Get School By Id
