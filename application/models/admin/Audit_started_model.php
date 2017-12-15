5308582eff912309213b33ad20eb0af609aae4b8
Sidd024 <siddhartha2488@gmail.com>
2017-12-15 04:15:28 -0800

    Add Model Partner Deatils Function
    Add Model Partner Deatils Function

--- application/models/admin/Audit_started_model.php
+++ application/models/admin/Audit_started_model.php
@@ -23,6 +23,20 @@ class Audit_started_model extends CI_Model {
                         ->order_by('a.id', 'desc')
                         ->get()->result();
     }
+    
+    public function getPartnerData() {
+	  $this->db->where('a.id=2752 OR a.id=2314 OR a.id=4621 OR a.id=4052 OR a.id=4130 OR a.id=2026 OR a.id=2729 OR
+a.id=3203 OR a.id=4234 OR a.id=2043 OR a.id=4194 OR a.id=4248 OR a.id=4079 OR a.id=2075 OR a.id=2015 OR a.id=4868 OR a.id=2342 OR a.id=2023 OR a.id=4056 OR a.id=4239 OR a.id=4160 OR a.id=2494 OR a.id=2831 OR a.id=2044 OR a.id=4193 OR a.id=2229 OR a.id=2311 OR a.id=2329 OR a.id=4598 OR a.id=4195 OR a.id=2298 OR a.id=4175 OR a.id=4190 OR a.id=2013 OR a.id=4602 OR a.id=4257 OR a.id=2488 OR a.id=2322 OR a.id=4050 OR a.id=4596 OR a.id=2297 OR a.id=2312 OR a.id=4616 OR a.id=4112 OR a.id=4617 OR a.id=2316 OR a.id=2058 OR a.id=2324 OR a.id=4088 OR a.id=4600 OR a.id=3451 OR a.id=4149 OR a.id=5478 OR a.id=4831 OR a.id=3510 OR a.id=4004 OR a.id=3485 OR a.id=4077 OR a.id=4089 OR a.id=4875 OR a.id=3978 OR a.id=3473 OR a.id=4086 OR a.id=3986 OR a.id=5425 OR a.id=4070 OR a.id=3970 OR a.id=4057 OR a.id=4153 OR a.id=4183 OR a.id=3950 OR a.id=4074 OR a.id=4076 OR a.id=4080 OR a.id=4388 OR a.id=3997 OR a.id=3984 OR a.id=5309 OR a.id=4101 OR a.id=4833 OR a.id=5387 OR a.id=4899 OR a.id=3429 OR a.id=3361 OR a.id=3430 OR a.id=4323 OR a.id=5398 OR a.id=5516 OR a.id=5469 OR a.id=3996 OR a.id=5597 OR a.id=5615 OR a.id=4016 OR a.id=3800 OR a.id=4071 OR a.id=4211 OR a.id=4078 OR a.id=5442 OR a.id=3952 OR a.id=4008 OR a.id=4095 OR a.id=4108 OR a.id=4109 OR a.id=3981 OR a.id=4673 OR a.id=3988 OR a.id=4121 OR a.id=4012 OR a.id=4106 OR a.id=5569 OR a.id=5385 OR a.id=3982 OR a.id=3408 OR a.id=4075 OR a.id=4028 OR a.id=4024 OR a.id=4023 OR a.id=4235 OR a.id=4044 OR a.id=4110 OR a.id=3513 OR a.id=4241 OR a.id=3490 OR a.id=3471 OR a.id=4990 OR a.id=5427 OR a.id=4103 OR a.id=4051 OR a.id=4058 OR a.id=4148 OR a.id=4113 OR a.id=3794 OR a.id=4922 OR a.id=3438 OR a.id=3965 OR a.id=4011 OR a.id=3455 OR a.id=4155 OR a.id=4328 OR a.id=3804 OR a.id=5370 OR a.id=3448 OR a.id=4735 OR a.id=4042 OR a.id=4002 OR a.id=5452 OR a.id=3355 OR a.id=4177 OR a.id=4001 OR a.id=3979 OR a.id=4587 OR a.id=5443 OR a.id=3447 OR a.id=4010 OR a.id=3474 OR a.id=4053 OR a.id=3359 OR a.id=5539 OR a.id=3985 OR a.id=3487 OR a.id=5401 OR a.id=3786 OR a.id=3469 OR a.id=3466 OR a.id=3437 OR a.id=3519 OR a.id=3435 OR a.id=3789 OR a.id=3518 OR a.id=3454 OR a.id=4865 OR a.id=3465 OR a.id=3805 OR a.id=3760 OR a.id=3426 OR a.id=3439 OR a.id=3840 OR a.id=3432 OR a.id=3516 OR a.id=3758 OR a.id=3511 OR a.id=3460 OR a.id=3480 OR a.id=3527 OR a.id=3523 OR a.id=3453 OR a.id=3420 OR a.id=3746 OR a.id=3423 OR a.id=3850 OR a.id=3419 OR a.id=3517 OR a.id=3441 OR a.id=3457 OR a.id=3524 OR a.id=3492 OR a.id=3489 OR a.id=3479 OR a.id=3525 OR a.id=3491 OR a.id=3481 OR a.id=3443 OR a.id=4059 OR a.id=3442 OR a.id=3971 OR a.id=3822 OR a.id=3488 OR a.id=3790 OR a.id=3476 OR a.id=2665 OR a.id=3980 OR a.id=3825 OR a.id=3751 OR a.id=3820 OR a.id=4349 OR a.id=3846 OR a.id=3509 OR a.id=3456 OR a.id=3808 OR a.id=3515 OR a.id=3458 OR a.id=3977 OR a.id=3989 OR a.id=4840 OR a.id=4191 OR a.id=3434 OR a.id=5558 OR a.id=4333 OR a.id=4350 OR a.id=5614 OR a.id=5630 OR a.id=4068 OR a.id=4007');
+	  $data=$this->db->select('a.*, b.name AS state_name,c.name As district_name')
+                        ->from('gsp_school AS a')
+                        ->join('states AS b', 'a.state=b.id', 'left')
+                        ->join('cities AS c', 'a.district=c.id', 'left')
+                        ->order_by('a.id', 'desc')
+                        ->get()->result();
+	 return $data;				
+	}	
+	
+	
     public function row_delete($id){
     $user_id=$this->db->select('a.userid')
                        ->from('gsp_school AS a')
