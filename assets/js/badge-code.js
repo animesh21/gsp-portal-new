5e1cb791c75d46c86996190cfb3392e1416d2532
Sidd024 <siddhartha2488@gmail.com>
2017-12-18 04:03:52 -0800

    badge-code.js add
    badge-code.js add

---
+++ assets/js/badge-code.js
@@ -0,0 +1,26 @@
+﻿/********************************************************************************/
+/********************************************************************************/
+/*Function Calculate Current Date*/
+function get_current_date(){
+	var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    if(dd<10){ dd='0'+dd; } 
+    if(mm<10){ mm='0'+mm; } 
+    var today = mm+'/'+dd+'/'+yyyy;
+	return today;
+}
+/********************************************************************************/
+/********************************************************************************/
+/*Function Calculate Current Date Difference*/
+function get_date_difference(end_date)
+{
+var d1 = new Date(end_date);
+var d2 = new Date(get_current_date());
+var timeDiff = d1.getTime() - d2.getTime();
+var DaysDiff = timeDiff / (1000 * 3600 * 24);
+   if(DaysDiff > 0){
+	  $('.badge').append('<img src=http://localhost/gsp/assets/img/logo.png width=100px />');
+   }
+}
