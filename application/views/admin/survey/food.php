462bbaf58d634d82952968b3d20652a8521516d2
guru-vachan <guruvachanj@gmail.com>
2017-12-28 18:15:25 +0530

    Update food.php

--- application/views/admin/survey/food.php
+++ application/views/admin/survey/food.php
@@ -224,14 +224,18 @@
                 <div class="form-group" id="meals11" style="display: block;">
                     <label class="control-label"><span class="cube">2(a)</span>If yes, then midday meal is served under:</label>
                     <ul class="list-inline">
-                        <p> <?php if(getFiled('Q5F1S1', $schoolUserID) == 1){ 
-			    ``		echo ("Government Scheme") ;}
-			          elseif(getFiled('Q5F1S1', $schoolUserID) == 2){
-					  echo ("School Personal initiative") ;}
-				  elseif(getFiled('Q5F1S1', $schoolUserID) == 3){
-					  echo ("others") ;}
+                         <?php 
+				if(getFiled('Q5F1S1', $schoolUserID) == 1){ 
+			    		echo "Government Scheme" ;
+		    		} 
+			     	if(getFiled('Q5F1S1', $schoolUserID) == 2){
+					echo "School Personal initiative" ;
+				  }
+				if(getFiled('Q5F1S1', $schoolUserID) == 3){
+				echo "others" ;
+				  }
 			    	?>
-			    </p> 
+			   
 
 
                     </ul>
