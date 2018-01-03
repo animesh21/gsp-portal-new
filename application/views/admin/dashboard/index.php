f850b1827eba3ca5d5e06c53d077fcaa48e2e8ec
Sidd024 <siddhartha2488@gmail.com>
2018-01-03 00:51:22 -0800

    Change Code
    Change Code

--- application/views/admin/dashboard/index.php
+++ application/views/admin/dashboard/index.php
@@ -1,53 +1,18 @@
 <h1>Dashboard</h1>
 
-<div class="schools-statics "> <a href="<?php base_url()?>dashboard/total_school_registration" target="_blank">
+<div class="schools-statics "> <a href="<?php base_url()?>dashboard/audit_pahse_1" target="_blank">
         <div class="statics-circle"> <span><?php echo $Schools;?></span>
             <label>view</label>
         </div>
     </a>
-    <div class="title">Total School Registrations</div>
+    <div class="title">Gsp Audit Phase 1</div>
 </div>
-<div class="schools-statics"> <a href="<?php base_url()?>dashboard/school_that_started_audit">
+<div class="schools-statics"> <a href="<?php base_url()?>dashboard/audit_pahse_2" target="_blank">
         <div class="statics-circle"> <span>
 		<?php echo $audit1; ?>
 		</span>
             <label>view</label>
         </div>
     </a>
-    <div class="title">Schools That Started The Audit</div>
-</div>
-<div class="schools-statics"> <a href="<?php base_url()?>dashboard/school_that_complete_audit">
-        <div class="statics-circle"> <span><?php echo $completecount;?></span>
-            <label>view</label>
-        </div>
-    </a>
-    <div class="title">Schools Completed The Audit</div>
-</div>
-<div class="schools-statics"> <a href="<?php base_url()?>dashboard/school_that_submit_audit">
-        <div class="statics-circle"> <span><?php echo $submitcount;?></span>
-            <label>view</label>
-        </div>
-    </a>
-    <div class="title">Schools Submitted The Audit</div>
-</div>
-<div class="schools-statics"><a href="<?php base_url()?>dashboard/audit_started_but_not_complete" target="_blank">
-        <div class="statics-circle"> <span><?php echo $uncompletecount;?></span>
-            <label>view</label>
-        </div>
-    </a>
-    <div class="title">Schools Started The Audit But Did Not Complete</div>
-</div>
-<div class="schools-statics"> <a href="<?php base_url()?>dashboard/schools_did_not_start_the_audit">
-        <div class="statics-circle"> <span><?php echo $notstartcount;?></span>
-            <label>view</label>
-        </div>
-    </a>
-    <div class="title">Schools Did Not Start The Audit</div>
-</div>
-<div class="schools-statics"> <a href="<?php base_url()?>dashboard/registration_2017">
-        <div class="statics-circle"> <span><?php echo$schoolcount;?></span>
-            <label>view</label>
-        </div>
-    </a>
-    <div class="title">2017 Registration</div>
+    <div class="title">Gsp Audit Phase 2</div>
 </div>
