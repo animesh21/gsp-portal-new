d097226c048f69f7de476e518698898d720ad58e
Sidd024 <siddhartha2488@gmail.com>
2018-01-11 23:02:29 -0800

    Add UDISE Code
    Add UDISE Code

--- application/views/admin/audit/audit_started.php
+++ application/views/admin/audit/audit_started.php
@@ -6,6 +6,7 @@
           <tr role="row">
             <th>S.No</th>
             <th>Sch.Id</th>
+											 <th>UDISE Code</th>
             <th>School Name</th>
             <th>State</th>
             <th>City</th>
@@ -23,6 +24,7 @@
             <tr role="row" class="<?php echo ($i%2==0) ? "even" : "odd"; ?>">
                 <td><?php echo $i; ?></td>
                 <td><?php echo $r->id; ?></td>
+												   	<td><?php echo $r->udise; ?></td>
                 <td><?php echo $r->name; ?></td>
                 <td><?php echo $r->state_name; ?></td>
                 <td><?php echo $r->city; ?></td>
