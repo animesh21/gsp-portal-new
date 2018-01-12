8c6663abec24f0fff5282cb76c160e805432768a
Sidd024 <siddhartha2488@gmail.com>
2018-01-12 01:47:44 -0800

    Change Code For mail sending
    Change Code For mail sending

--- application/views/admin/audit/audit_started_2017.php
+++ application/views/admin/audit/audit_started_2017.php
@@ -1,4 +1,4 @@
-
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
 <!----- Badge Model Code  ------>
 <div class="modal fade" id="myModal1" role="dialog">
   <div class="modal-dialog modal-md">
