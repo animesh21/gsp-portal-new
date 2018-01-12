9d2249575fcd8e547c07b6d07e59a45e6a42e33f
Sidd024 <siddhartha2488@gmail.com>
2018-01-11 23:05:18 -0800

    ADD UDISE Code
    ADD UDISE Code

--- application/views/admin/audit/complete_school.php
+++ application/views/admin/audit/complete_school.php
@@ -6,6 +6,7 @@
           <tr role="row">
            <th>S.No</th>
             <th>Sch.Id</th>
+	    <th>UDISE Code</th>
             <th>School Name</th>
             <th>pincode</th>
             <th>City</th>
@@ -26,10 +27,11 @@
             <tr role="row" class="<?php echo ($i%2==0) ? "even" : "odd"; ?>">
                 <td><?php echo $i; ?></td>
                 <td><?php echo $r->id; ?></td>
+		<td><?php echo $r->udise; ?></td>
                 <td><?php echo $r->name; ?></td>
                 <td><?php echo $r->pincode; ?></td>
                 <td><?php echo $r->city; ?></td>
-               <td><?php echo $r->state_name; ?></td>
+                <td><?php echo $r->state_name; ?></td>
                 <td><?php echo $r->district_name; ?></td>
                 <td><?php echo $r->coname; ?></td>
                 <td><?php echo $r->coemail; ?></td>
