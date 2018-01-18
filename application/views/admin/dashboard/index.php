c52dfeb8505cf8ac8f760b4d8b55106c5988bce3
Sidd024 <siddhartha2488@gmail.com>
2018-01-18 06:31:50 -0800

    Change Code
    Change Code

--- application/views/admin/dashboard/index.php
+++ application/views/admin/dashboard/index.php
@@ -1,7 +1,7 @@
 <h1>Dashboard</h1>
 
 <div class="schools-statics "> <a href="<?php base_url()?>dashboard/audit_pahse_1" target="_blank">
-        <div class="statics-circle"> <span><?php echo $Schools;?></span>
+        <div class="statics-circle"> <span><?php echo $audit1;?></span>
             <label>view</label>
         </div>
     </a>
@@ -9,7 +9,7 @@
 </div>
 <div class="schools-statics"> <a href="<?php base_url()?>dashboard/audit_pahse_2" target="_blank">
         <div class="statics-circle"> <span>
-		<?php echo $audit1; ?>
+		<?php echo $audit2; ?>
 		</span>
             <label>view</label>
         </div>
