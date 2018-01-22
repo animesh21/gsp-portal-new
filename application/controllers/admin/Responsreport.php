9cd9c0ff148a9f7aa984f875ef008caa40d12531
Sidd024 <siddhartha2488@gmail.com>
2018-01-22 07:50:31 -0800

    Download Response Report
    Download Response Report

---
+++ application/controllers/admin/Responsreport.php
@@ -0,0 +1,82 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Responsreport extends CI_Controller {
+    /*
+     * Constructor 
+     */
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper(array('form', 'security', 'common_helper'));
+        $this->load->model(array('Answer_model', 'file','User_model'));
+        $this->load->helper(array('download', 'common_helper', 'form'));
+        $this->load->model('admin/Audit_started_model');
+	    $this->load->library('pagination');
+    }
+
+    public function index() {
+        $data['main'] = 'Response Report 2017';
+        $data['title'] = 'Home | Response Report 2017';
+    }	
+
+    /*
+     * Generate Pdf
+     */
+
+    public function pdf($argID) {
+        $data['title'] = 'Home';
+        $data['id'] = $argID;
+        $data['states'] = getStates();
+        $data['school'] = $this->Audit_started_model->getSchoolById($argID);
+        $data['schoolUserID'] = $data['school']->userid;
+        $data['files'] = $this->file->getFilesData($this->session->userdata('USER_ID'));
+        $data['filesfules'] = $this->file->getFilesDatafules($this->session->userdata('USER_ID'));
+        $data['airQualityMonitering'] = $this->file->AirQuality($this->session->userdata('USER_ID'));
+        $data['pucCertificate'] = $this->file->pucCertificate($this->session->userdata('USER_ID'));
+        $data['electricityBills'] = $this->file->getElectricityFulesBillsData($this->session->userdata('USER_ID'));
+        $data['fulesBills'] = $this->file->getEnergyFulesBillsData($this->session->userdata('USER_ID'));
+        $data['appliancesBills'] = $this->file->getEnergyApplianceBillsData($this->session->userdata('USER_ID'));
+        $data['alternativeResource'] = $this->file->alternativeResource($this->session->userdata('USER_ID'));
+        $data['energySupport'] = $this->file->getEnergySupportData($this->session->userdata('USER_ID'));
+        //food
+        $data['midDayMeal'] = $this->file->getMidDayMeal($this->session->userdata('USER_ID'));
+        $data['canteen'] = $this->file->getCanteen($this->session->userdata('USER_ID'));
+        $data['files'] = $this->file->getFoodFilesData($this->session->userdata('USER_ID'));
+        $data['uppc'] = $this->file->getFoodUPPCData($this->session->userdata('USER_ID'));
+        //land
+        $data['greenCover'] = uploadHelper($this->session->userdata('USER_ID'), 'Green_Cover');
+        //waste
+        $data['segregationClassroom'] = uploadHelper($this->session->userdata('USER_ID'), 'Segregation_Source_Classrooms');
+        $data['solidWaste'] = uploadHelper($this->session->userdata('USER_ID'), 'Audit_Team_Weighing_Solid_Waste');
+        $data['composingPit'] = uploadHelper($this->session->userdata('USER_ID'), 'Composting_Pit');
+        $data['recycleMachine'] = uploadHelper($this->session->userdata('USER_ID'), 'Recycling_Machine');
+        $data['eWaste'] = uploadHelper($this->session->userdata('USER_ID'), 'E-Waste');
+        $data['houseKeeping'] = uploadHelper($this->session->userdata('USER_ID'), 'Housekeeping');
+        $data['burningWaste'] = uploadHelper($this->session->userdata('USER_ID'), 'Buring_Waste');
+        $data['teamDoingWaste'] = uploadHelper($this->session->userdata('USER_ID'), 'Audit_Team_Doing_Survey');
+        $data['ewasteStorage'] = uploadHelper($this->session->userdata('USER_ID'), 'E-Waste_Storage');
+        $data['disposing'] = uploadHelper($this->session->userdata('USER_ID'), 'Disposing');
+        $data['eWasteDisposing'] = uploadHelper($this->session->userdata('USER_ID'), 'E-Waste_Disposing_Certificate');
+        $data['pictures'] = uploadHelper($this->session->userdata('USER_ID'), 'Pictures_of_Audit_Team');
+        //water  
+        $data['task4supporting'] = uploadHelper($this->session->userdata('USER_ID'), 'Task_4_Supporting_docs');
+        $data['waterTreatment'] = uploadHelper($this->session->userdata('USER_ID'), 'Water_Treatment_Process');
+        $data['flowChartHandDrawn'] = uploadHelper($this->session->userdata('USER_ID'), 'Flow_Chart_Hand_Drwan');
+        $data['supportDocWater'] = uploadHelper($this->session->userdata('USER_ID'), 'Supporting_Document_Water');
+        $rank=getFiled('Q1G2',$data['schoolUserID']);
+        if ($rank >= 6 ) { 
+            $html1 = $this->load->view('school_pdf', $data, true);
+             
+            } else {
+               $html1 = $this->load->view('primary-school-pdf', $data, true);
+            }
+            $pdfFilePath = "school_pdf_name.pdf";
+            $this->load->library('dompdf_lib');
+			$this->dompdf->load_html($html1);
+		    $this->dompdf->render();
+		    $this->dompdf->stream("welcome.pdf", array("Attachment" => false));
+    }
+}
+
