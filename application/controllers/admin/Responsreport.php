d75d8d24e620af9ab83f74a440a992328d7ca42d
Sidd024 <siddhartha2488@gmail.com>
2018-01-22 07:54:12 -0800

    Change Code
    Change Code

--- application/controllers/admin/Responsreport.php
+++ application/controllers/admin/Responsreport.php
@@ -26,15 +26,20 @@ class Responsreport extends CI_Controller {
      */
 
     public function pdf($argID) {
+	 ini_set('memory_limit', '256M');
+	 ini_set('max_execution_time', 300);   
         $data['title'] = 'Home';
         $data['id'] = $argID;
         $data['states'] = getStates();
-        $data['school'] = $this->Audit_started_model->getSchoolById($argID);
-        $data['schoolUserID'] = $data['school']->userid;
+        $data['school'] = $this->Audit_started_model->getSchoolByIdRecoad($argID);
+	//echo '<pre>'; print_r($data['school']);  die();  
+        $data['schoolUserID'] = $data['school'][0]->userid;
         $data['files'] = $this->file->getFilesData($this->session->userdata('USER_ID'));
         $data['filesfules'] = $this->file->getFilesDatafules($this->session->userdata('USER_ID'));
         $data['airQualityMonitering'] = $this->file->AirQuality($this->session->userdata('USER_ID'));
-        $data['pucCertificate'] = $this->file->pucCertificate($this->session->userdata('USER_ID'));
+        $data['SupportiveAir'] = $this->file->SupportiveAir($this->session->userdata('USER_ID'));
+       
+	$data['pucCertificate'] = $this->file->pucCertificate($this->session->userdata('USER_ID'));
         $data['electricityBills'] = $this->file->getElectricityFulesBillsData($this->session->userdata('USER_ID'));
         $data['fulesBills'] = $this->file->getEnergyFulesBillsData($this->session->userdata('USER_ID'));
         $data['appliancesBills'] = $this->file->getEnergyApplianceBillsData($this->session->userdata('USER_ID'));
@@ -45,8 +50,9 @@ class Responsreport extends CI_Controller {
         $data['canteen'] = $this->file->getCanteen($this->session->userdata('USER_ID'));
         $data['files'] = $this->file->getFoodFilesData($this->session->userdata('USER_ID'));
         $data['uppc'] = $this->file->getFoodUPPCData($this->session->userdata('USER_ID'));
+        $data['AuditTeamD'] = $this->file->AuditTeamD($this->session->userdata('USER_ID'));	    
         //land
-        $data['greenCover'] = uploadHelper($this->session->userdata('USER_ID'), 'Green_Cover');
+        $data['greenCover'] = $this->file->Green_Cover($this->session->userdata('USER_ID'));
         //waste
         $data['segregationClassroom'] = uploadHelper($this->session->userdata('USER_ID'), 'Segregation_Source_Classrooms');
         $data['solidWaste'] = uploadHelper($this->session->userdata('USER_ID'), 'Audit_Team_Weighing_Solid_Waste');
@@ -55,7 +61,7 @@ class Responsreport extends CI_Controller {
         $data['eWaste'] = uploadHelper($this->session->userdata('USER_ID'), 'E-Waste');
         $data['houseKeeping'] = uploadHelper($this->session->userdata('USER_ID'), 'Housekeeping');
         $data['burningWaste'] = uploadHelper($this->session->userdata('USER_ID'), 'Buring_Waste');
-        $data['teamDoingWaste'] = uploadHelper($this->session->userdata('USER_ID'), 'Audit_Team_Doing_Survey');
+        $data['teamDoingWaste'] = $this->file->AuditTeamWaste($this->session->userdata('USER_ID'));
         $data['ewasteStorage'] = uploadHelper($this->session->userdata('USER_ID'), 'E-Waste_Storage');
         $data['disposing'] = uploadHelper($this->session->userdata('USER_ID'), 'Disposing');
         $data['eWasteDisposing'] = uploadHelper($this->session->userdata('USER_ID'), 'E-Waste_Disposing_Certificate');
@@ -64,19 +70,23 @@ class Responsreport extends CI_Controller {
         $data['task4supporting'] = uploadHelper($this->session->userdata('USER_ID'), 'Task_4_Supporting_docs');
         $data['waterTreatment'] = uploadHelper($this->session->userdata('USER_ID'), 'Water_Treatment_Process');
         $data['flowChartHandDrawn'] = uploadHelper($this->session->userdata('USER_ID'), 'Flow_Chart_Hand_Drwan');
-        $data['supportDocWater'] = uploadHelper($this->session->userdata('USER_ID'), 'Supporting_Document_Water');
+        $data['supportDocWater'] = $this->file->SupportingWater($this->session->userdata('USER_ID'));
+        //echo '<pre>'; print_r($data);die;
         $rank=getFiled('Q1G2',$data['schoolUserID']);
+        
         if ($rank >= 6 ) { 
-            $html1 = $this->load->view('admin/survey/school_pdf', $data, true);
+           $html1 = $this->load->view('admin/survey/school_pdf', $data, true);
              
             } else {
                $html1 = $this->load->view('admin/survey/primary-school-pdf', $data, true);
             }
-            $pdfFilePath = "school_pdf_name.pdf";
-            $this->load->library('dompdf_lib');
+			
+			  $this->load->library('dompdf_lib');
+			  $this->dompdf->set_paper("A4");
 			$this->dompdf->load_html($html1);
 		    $this->dompdf->render();
 		    $this->dompdf->stream("welcome.pdf", array("Attachment" => false));
+        
     }
 }
 
