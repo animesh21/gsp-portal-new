1f3ffe8c0072fd219b6b302f5343d982c186d2bc
Sidd024 <siddhartha2488@gmail.com>
2018-01-30 22:21:06 -0800

    Change Code
    Change Code

--- application/models/admin/Dashboard_model.php
+++ application/models/admin/Dashboard_model.php
@@ -200,6 +200,17 @@ class Dashboard_model extends CI_Model {
                         ->order_by('a.id', 'desc')
                         ->get()->result();
     }
+    
+     public function getData_phase_2_1() {
+        return $this->db->select('a.*, b.name AS state_name,c.name As district_name')
+                        ->from('gsp_school AS a')
+                        ->join('states AS b', 'a.state=b.id', 'left')
+			            ->join('cities AS c', 'a.district=c.id', 'left')
+			            ->where('a.date_added>=', '2017-11-30 00:00:00')
+                        ->order_by('a.id', 'desc')
+                        ->get()->result();
+    }	
+	
 	
 	public function totalschool_phase_2()
 	{
