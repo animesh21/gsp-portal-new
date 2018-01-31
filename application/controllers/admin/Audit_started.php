8c2d7ef3d27c0b3b30c6263fd3070a029287e46a
Sidd024 <siddhartha2488@gmail.com>
2018-01-31 01:13:40 -0800

    Change Code
    Change Code

--- application/controllers/admin/Audit_started.php
+++ application/controllers/admin/Audit_started.php
@@ -76,75 +76,99 @@ class Audit_started extends CI_Controller {
 	/*
      * Send Filter Feedback
      */ 
+	/*
+     * Send Filter Feedback
+     */ 
 	public function filter_email() {
-	  	//echo '<pre>'; print_r($_POST); exit;
-		$filed='';
-		$byMail=$this->input->post('email');
-		if($byMail=='coemail')
-		{
-		   $filed="a.coemail";
-		}else if($byMail=='schoolemail')
-		{
-		   $filed="a.schoolemail";
+	//echo '<pre>'; print_r($_POST); exit;
+	$filed = '';
+	$byMail = $this->input->post('email');
+	if ($byMail == 'coemail') {
+	    $filed = "a.coemail";
+	} else if ($byMail == 'schoolemail') {
+	    $filed = "a.schoolemail";
+	}
+	$category=array();
+	$aid=array();
+	$byProgress = $this->input->post('progress');
+	$byCategory = $this->input->post('school_category');
+	$bySchoolType = $this->input->post('school_type');
+	$byAidType = $this->input->post('school_aid');
+	$byState = $this->input->post('state');
+	$bySchoolName = $this->input->post('schoolname');
+	$rating = $this->input->post('rating');
+	$query = "SELECT a.id, a.udise, a.userid, a.name, a.country, a.state, a.district, a.city, a.progress, a.Q1S1, a.Q2G1, a.Q9G1, $filed, b.name AS state_name, c.name AS district_name,a.remark FROM tbl_sendmail AS a LEFT JOIN states AS b ON a.state=b.id LEFT JOIN cities AS c ON a.district=c.id WHERE";
+	$conditions = array();
+
+	//State
+	if (!empty($byState)) {
+	    $conditions[] = " a.state='$byState'";
+	}
+	
+	//school Name
+	if (!empty($bySchoolName)) {
+	    $conditions[] = " a.name LIKE '%$bySchoolName%'";
+	}
+	
+	//Schoo Type
+	if (!empty($bySchoolType)) {
+	    foreach($bySchoolType as $a)
+	    {
+		$aid[]=" a.Q2G1='$a'";
+	    }
+	    $conditions[] = "(".implode(' OR ', $aid).")";
+	}
+	
+	//Progress Condition
+	if (!empty($byProgress)) {
+	    ///$conditions[] = "a.progress='$byProgress'";
+	    foreach ($byProgress as $p)
+	    {
+		//$test  ="a.progress='$p'";
+		$test[]=" a.progress='$p'";
+	    }
+	    $conditions[]="(".implode(' OR ', $test).")";
+	}
+	
+	
+	//Category
+	if (!empty($byCategory)) {
+	    foreach ($byCategory as $c)
+	    {
+		$category[]=" a.Q1S1='$c'";
+	    }
+	    $conditions[] = "(".implode(' OR ', $category).")";
+	    //echo '<pre>'; print_r($conditions); exit;
+	}
+
+	//School type aid
+	if (!empty($byAidType)) {
+	    foreach ($byAidType as $u)
+	    {
+	    $Aid[] = "a.Q9G1='$u'";
 		}
-		$byProgress=$this->input->post('progress');
-		$byCategory=$this->input->post('school_category');
-		$bySchoolType=$this->input->post('school_type');
-		$byAidType=$this->input->post('school_aid');
-		$byState=$this->input->post('state');
-		$bySchoolName=$this->input->post('schoolname');
-		$rating=$this->input->post('rating');
-		$query = "SELECT a.id, a.udise, a.userid, a.name, a.country, a.state, a.district, a.city, a.progress, a.Q1S1, a.Q2G1, a.Q9G1, $filed, b.name AS state_name, c.name AS district_name,a.remark FROM tbl_sendmail AS a LEFT JOIN states AS b ON a.state=b.id LEFT JOIN cities AS c ON a.district=c.id";
-        $conditions = array();
-		
-		//Progress Condition
-		if(! empty($byProgress)) {
-         $conditions[] = "a.progress='$byProgress'";
-        }
-		
-		//Category
-		if(! empty($byCategory)) {
-         $conditions[] = "a.Q1S1='$byCategory'";
-        }
-		
-		//Schoo Type
-		if(! empty($bySchoolType)) {
-         $conditions[] = "a.Q2G1='$bySchoolType'";
-        }
-		
-		//School type aid
-		if(! empty($byAidType)) {
-         $conditions[] = "a.Q9G1='$byAidType'";
-        }
-		
-		//State
-		if(! empty($byState)) {
-         $conditions[] = "a.state='$byState'";
-        }
-		
-		//school Name
-		if(! empty($bySchoolName)) {
-         $conditions[] = "a.name LIKE '%$bySchoolName%'";
-        }
+		$conditions[]="(".implode(' OR ', $Aid).")";
+	}
+
 	//school rating
-	if(! empty($rating)) {
-         $conditions[] = "a.remark LIKE '%$rating%'";
-        }	
-		
-		$sql = $query;
-		
-        if (count($conditions) > 0) {
-          $sql .= " WHERE " . implode(' AND ', $conditions);
-        }
-		//echo $sql; exit;
-        $query =$this->db->query($sql);
-		///$data=$query->result_array();
-		
-		$data['states'] = getStates();
-	        $data['states'][0]="All"; 
-		$data['main'] = 'admin/audit/feedback-with-filter';
-		$data['record']=$query->result_array();
-        $this->load->view('admin/includes/template', $data);
+	if (!empty($rating)) {
+	    $conditions[] = "a.remark LIKE '%$rating%'";
+	}
+
+	$sql = $query;
+
+	if (count($conditions) > 0) {
+	    $sql .= implode(' AND ', $conditions);
+	}
+        //echo $sql; exit;
+	$query = $this->db->query($sql);
+	///$data=$query->result_array();
+
+	$data['states'] = getStates();
+	$data['states'][0] = "All";
+	$data['main'] = 'admin/audit/feedback-with-filter';
+	$data['record'] = $query->result_array();
+	$this->load->view('admin/includes/template', $data);
     }
 
 }
