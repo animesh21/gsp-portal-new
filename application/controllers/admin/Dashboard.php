bd9f39110fc448e470011820b669353d7b010a94
Sidd024 <siddhartha2488@gmail.com>
2018-01-30 22:25:18 -0800

    Change Code
    Change Code

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -137,7 +137,7 @@ class Dashboard extends CI_Controller {
     {
         $data['main']='admin/audit/register_2017_phase_2';
         $data['title']='Registration 2017';
-        $data['record']=$this->Dashboard_model->getData_phase_2();
+        $data['record']=$this->Dashboard_model->getData_phase_2_1();
         $data['Schools'] = $this->School_model->getSchools_phase_2();
         $data['schoolcount']=$this->Dashboard_model->schoolCount_phase_2();
         $this->load->view('admin/includes/template', $data);
