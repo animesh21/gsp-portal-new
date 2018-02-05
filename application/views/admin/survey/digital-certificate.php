e889ce8084fbfc6b0d55116dc512ccaad7086189
Sidd024 <siddhartha2488@gmail.com>
2018-02-05 00:54:23 -0800

    Change Code
    Change Code

--- application/views/admin/survey/digital-certificate.php
+++ application/views/admin/survey/digital-certificate.php
@@ -5,13 +5,12 @@
     </head>
     <body>
 	<?php
-	
 	if (!empty($students)) {
 	    for ($i = 0; $i < count($students); $i++) {
 			//echo '<pre>'; print_r($students);exit;
 		if ($students[$i]['name'] !="") {
 		    echo "<div class='container' 
-style='background:url(assets/img/images/Certificate-2017.jpg) 5% 65%  no-repeat; width:1024px; height:791px; margin:0px auto;'><div class='first_name'>" . $students[$i]['name'] . "&nbsp;&nbsp;" .  $students[$i]['grade'] . "</div>&nbsp;<div class='school_name'>".$students[$i]['school_name']."</div></div>";
+style='background:url(assets/img/images/Certificate-2017.jpg) 0% -17%  no-repeat; width:1124px; height:791px; margin:0px auto;'><div class='first_name'>" . $students[$i]['name'] . "&nbsp;&nbsp;" .  $students[$i]['grade'] . "</div>&nbsp;<div class='school_name'>".$students[$i]['school_name']."</div></div>";
 		}
 	    }
 	}
@@ -19,15 +18,17 @@ style='background:url(assets/img/images/Certificate-2017.jpg) 5% 65%  no-repeat;
 	    for ($i = 0; $i < count($staffadmin); $i++) {
 		if ($staffadmin != '') {
 		    echo "<div class='container' 
-style='background:url(assets/img/images/Certificate-2017.jpg) 5% 65%  no-repeat; width:1024px; height:791px; margin:0px auto;'><div class='first_name'>" . $staffadmin['teacher'] . "</div>&nbsp;<div class='school_name'>" .  $staffadmin['school'] . "</div></div>";
+style='background:url(assets/img/images/Certificate-2017.jpg) 0% -17%  no-repeat; width:1124px; height:791px; margin:0px auto;'><div class='first_name'>" . $staffadmin['teacher'] . "</div>&nbsp;<div class='school_name'>" .  $staffadmin['school'] . "</div></div>";
 		}
 	    }
 	}
+	
+	
 	?>
     </body>
 </html>
 <style type="text/css">
-    .first_name{ position:absolute; top:420px; left:240px}
-	.school_name{ position:absolute; top:469px; left:100px}
-    .grade{position:absolute; top:280px; left:650px}
+    .first_name{ position:absolute; top:400px; left:240px}
+	.school_name{ position:absolute; top:449px; left:100px}
+    .grade{position:absolute; top:260px; left:650px}
 </style>
