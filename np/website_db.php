2b875365582e85dcea38095a1d7a5c1dd98fd0f6
yhamender <30542583+yhamender@users.noreply.github.com>
2018-02-07 17:25:54 +0530

    Add Host Database Settings
    add

--- np/website_db.php
+++ np/website_db.php
@@ -1,9 +1,9 @@
 <?php
 
-define("DB_Hostname", "localhost");
-		define("DB_Username", "audit2016");
-		define("DB_Password", "ByJ^zyY7K(m");
-		define("DB_databasename", "gsp_audit");
+define("DB_Hostname", "148.72.232.125");
+		define("DB_Username", "studio_gsp2015");
+		define("DB_Password", "Studio@123");
+		define("DB_databasename", "gsp_2015");
 
 $link = mysql_connect(DB_Hostname, DB_Username, DB_Password) or die('Error connecting to mysql');
 $mysql_db = mysql_select_db(DB_databasename, $link);
