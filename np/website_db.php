47d6e68bd35dc6e8a1a4a9ea2922a36af7f6c735
Sidd024 <siddhartha2488@gmail.com>
2018-02-07 22:30:51 -0800

    Change Database ConnectivityCode
    Change Database ConnectivityCode

--- np/website_db.php
+++ np/website_db.php
@@ -1,10 +1,8 @@
 <?php
-
-define("DB_Hostname", "43.255.154.31");
-		define("DB_Username", "studio_gsp2015");
-		define("DB_Password", "Studio@123");
-		define("DB_databasename", "gsp_2015");
-
+define("DB_Hostname", "localhost");
+define("DB_Username", "audit2016");
+define("DB_Password", "ByJ^zyY7K(m");
+define("DB_databasename", "gsp_audit");
 $link = mysql_connect(DB_Hostname, DB_Username, DB_Password) or die('Error connecting to mysql');
 $mysql_db = mysql_select_db(DB_databasename, $link);
 ?>
