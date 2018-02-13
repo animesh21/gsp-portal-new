570ea159e5df2b9d5ec3ca0fba37b81b8f5947c2
Sidd024 <siddhartha2488@gmail.com>
2018-02-13 04:59:46 -0800

    Change Code in Performacne Report Code
    Change Code in Performacne Report Code

--- application/controllers/admin/Performance_report.php
+++ application/controllers/admin/Performance_report.php
@@ -126,47 +126,18 @@ class Performance_report extends CI_Controller {
             
             
     
-    public function PdfById() {
+   public function PdfById($argsID) {
         $this->config->load('array_config');
-        
-        $data = array(
-          'gsp_userid' => $this->input->post('dname')
-            );
-           //print_r($data['gsp_userid']);
-            //die();
-        $d['performance'] = $this->Performance_model->getDataById($data['gsp_userid']);
-        //print_r($d);
-        $html=$this->load->view('admin/performance_report/Performance', $d,true);
-           
-
-            //this the the PDF filename that user will get to download
-            $pdfFilePath = "performance-report.pdf";
-
-            //load mPDF library
-           $this->load->library('M_pdf');
-
-            //generate the PDF from the given html
-           $this->m_pdf->pdf->WriteHTML($html);
-
-            //download it.
-            $this->m_pdf->pdf->Output($pdfFilePath, "I");
-        
+        $data['performance'] = $this->Performance_model->getDataById($argsID);
+        $html=$this->load->view('admin/performance_report/Performance', $data,true);
+		    $this->load->library('dompdf_lib');
+		    $this->dompdf->set_paper("A4");
+			$this->dompdf->load_html($html);
+		    $this->dompdf->render();
+		    $this->dompdf->stream("performance-report.pdf", array("Attachment" => false));
     }
     
-//    public function excel()
-//    {
-//        $arrUserID=array();
-//        $this->load->dbutil();
-//        $record= $this->Performance_model->getData();
-//        foreach($record as $r)
-//        {
-//            $arrUserID[]=$r->userid;
-//        }
-//        //echo '<pre>'; print_r($arrUserID); exit;
-//        $row = $this->Performance_model->getExcel($arrUserID);
-//        $name = 'registration2017_by_state.csv';
-//        force_download($name, $row);
-//    }
+
 	
 	 public function landPrimary()
 	{
