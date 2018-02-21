73081717d24c049e48a441997dd74f2788dde10c
Sidd024 <siddhartha2488@gmail.com>
2018-02-21 01:19:52 -0800

    Change Code
    Change Code

--- application/models/admin/Performance_model.php
+++ application/models/admin/Performance_model.php
@@ -544,7 +544,7 @@ return $this->db->select('a.*,b.name AS state_name, c.name AS district_name')->f
 	 public function getSchoolgeneralPrimarySchool_phase2()
 		{
 			
-			return $this->db->select('a.*,b.userid,b.questionid,b.answer,c.name,c.id,d.category,d.population ,e.name AS state_name, f.name AS district_name')
+			return $this->db->select('a.*,b.userid,b.questionid,b.answer,c.name,c.id,d.category,d.population ,e.name AS state_name, f.name AS district_name, c.id AS school_id')
 							->from('tbl_general_phase_2 AS a')	
 			                ->join('gsp_answers As b','a.userid=b.userid','left')
 							->join('gsp_school AS c','b.userid=c.userid', 'left')
