a992e45e352c06ad4dfc2e6b17fd68b9b5bc1af2
Sidd024 <siddhartha2488@gmail.com>
2018-02-25 22:43:57 -0800

    Change in Code
    Change in Code

--- application/models/admin/Performance_model.php
+++ application/models/admin/Performance_model.php
@@ -561,7 +561,7 @@ return $this->db->select('a.*,b.name AS state_name, c.name AS district_name')->f
 	 
 		{
 			
-			return $this->db->select('a.*,b.userid,b.questionid,b.answer,c.name,c.id,d.category,d.population ,e.name AS state_name, f.name AS district_name')
+			return $this->db->select('a.*,b.userid,b.questionid,b.answer,c.name,c.id,d.category,d.population ,e.name AS state_name, f.name AS district_name, c.schoolemail AS schoolemail')
 							->from('tbl_total_phase2 AS a, c.schoolemail AS schoolemail')	
 			                ->join('gsp_answers As b','a.userid=b.userid','left')
 							->join('gsp_school AS c','b.userid=c.userid', 'left')
@@ -578,7 +578,7 @@ return $this->db->select('a.*,b.name AS state_name, c.name AS district_name')->f
 	 
 		{
 			
-			return $this->db->select('a.*,b.userid,b.questionid,b.answer,c.name,c.id,d.category,d.population ,e.name AS state_name, f.name AS district_name')
+			return $this->db->select('a.*,b.userid,b.questionid,b.answer,c.name,c.id,d.category,d.population ,e.name AS state_name, f.name AS district_name, c.schoolemail AS schoolemail')
 							->from('tbl_total_phase2 AS a, c.schoolemail AS schoolemail')	
 			                ->join('gsp_answers As b','a.userid=b.userid','left')
 							->join('gsp_school AS c','b.userid=c.userid', 'left')
