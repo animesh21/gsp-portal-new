25e2ffd6a4c0b3b6e099f232dc0d4b281c5bcaf9
yhamender <30542583+yhamender@users.noreply.github.com>
2018-02-28 15:39:47 +0530

    Change Database Settings
    Change database Confrigation Settings

--- np/config.php
+++ np/config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "audit2016";
-$password = "ByJ^zyY7K(m";
-$dbname = "gsp_audit";
+$username = "GSP_2016";
+$password = "Studio@123";
+$dbname = "GSP_2016";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$dbname);
 // Check connection
