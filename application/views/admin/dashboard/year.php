65edc867cca61e02d2b040af145582197888fe3a
guru-vachan <guruvachanj@gmail.com>
2018-02-28 18:45:58 +0530

    Create year.php

---
+++ application/views/admin/dashboard/year.php
@@ -0,0 +1,18 @@
+<h1>Dashboard</h1>
+
+<div class="schools-statics "> <a href="#" target="_blank">
+        <div class="statics-circle"> <span>2</span>
+            <label>view</label>
+        </div>
+    </a>
+    <div class="title">Phase</div>
+</div>
+<div class="schools-statics"> <a href="#" target="_blank">
+        <div class="statics-circle"> <span>
+		0
+		</span>
+            <label>Phase</label>
+        </div>
+    </a>
+    <div class="title">2018</div>
+</div>
