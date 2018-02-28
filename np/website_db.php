63aaf51b5e598c8746e62f82d088f4d4ed01fa08
yhamender <30542583+yhamender@users.noreply.github.com>
2018-02-28 15:40:56 +0530

    Change Database Settings
    Change in database

--- np/website_db.php
+++ np/website_db.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_Hostname", "localhost");
-define("DB_Username", "audit2016");
-define("DB_Password", "ByJ^zyY7K(m");
-define("DB_databasename", "gsp_audit");
+define("DB_Username", "GSP_2016");
+define("DB_Password", "Studio@123");
+define("DB_databasename", "GSP_2016");
 $link = mysql_connect(DB_Hostname, DB_Username, DB_Password) or die('Error connecting to mysql');
 $mysql_db = mysql_select_db(DB_databasename, $link);
 ?>
