ed7a33dc2d6ead2c78e2875e138e22160fc587ac
yhamender <30542583+yhamender@users.noreply.github.com>
2018-02-28 15:36:03 +0530

    Add Database Settings
    Configure Database Settings

--- np/2015/website_db.php
+++ np/2015/website_db.php
@@ -1,8 +1,8 @@
 <?php
      	define("DB_Hostname", "localhost");
-		define("DB_Username", "root");
-		define("DB_Password", "");
-		define("DB_databasename", "gsp_audit");
+		define("DB_Username", "GSP_2015");
+		define("DB_Password", "Studio@123");
+		define("DB_databasename", "GSP_2015");
 	
 	$link = mysql_connect(DB_Hostname,DB_Username,DB_Password) or die('Error connecting to mysql');
 	$mysql_db = mysql_select_db(DB_databasename, $link);
