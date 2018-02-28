492ce86ce932118f011b3dbb839b56e0651a9159
guru-vachan <guruvachanj@gmail.com>
2018-02-28 19:03:41 +0530

    Create year_18.php

---
+++ application/views/admin/dashboard/year_18.php
@@ -0,0 +1,18 @@
+<h1>Dashboard</h1>
+
+<div class="schools-statics "> <a href="#" target="_blank">
+        <div class="statics-circle"> <span>0</span>
+            <label>view</label>
+        </div>
+    </a>
+    <div class="title">Gsp Audit Phase 1</div>
+</div>
+<div class="schools-statics"> <a href="#" target="_blank">
+        <div class="statics-circle"> <span>
+		  0
+		</span>
+            <label>view</label>
+        </div>
+    </a>
+    <div class="title">Gsp Audit Phase 2</div>
+</div>
