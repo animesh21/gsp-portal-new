a845e9669faef2f21240e890f4c6d76669361b39
guru-vachan <guruvachanj@gmail.com>
2018-03-08 14:37:15 +0530

    Update Performance.php

--- application/views/admin/performance_report/Performance.php
+++ application/views/admin/performance_report/Performance.php
@@ -357,19 +357,15 @@ if ($data == 3 || $data == 4) { ?>
           <?php
                     $arrImages = getUploadData(str_replace(' ', '_', $performance[0]->name) . '_PUC_Certificate', $schoolUserID);
 	
-                                if(count($arrImages)>0){
-                                  <h4><?php echo 'NO' ; ?></h4>
-          			  <p><?php echo 'Road worthiness certificate should be obtained for all the vehicles. If your vehicles have this certificate, 
-				      it means that they are in good operating condition and meet the acceptable standards for safe driving.'; ?></p>
+                                if(count($arrImages)>0) { ?>
+                                  <h4> <?php echo "NO" ; ?> </h4>
+          			  <p> <?php echo "Road worthiness certificate should be obtained for all the vehicles<br/>If your vehicles have this certificate,it means that they are in good operating condition and meet the acceptable standards for safe driving."; ?></p>
                                 } else {
-                                   <h4><?php echo 'Yes'; ?></h4>
-         			   <p><?php echo 'It is good that your school has a road worthiness certificate for your vehicles. 
-				    This means that your vehicles are in good operating condition and meet the acceptable standards for safe driving.'; ?></p>
+	      			  <h4><?php echo "Yes"; ?></h4>
+         			   <p><?php echo "It is good that your school has a road worthiness certificate for your vehicles<br/>This means that your vehicles are in good operating condition and meet the acceptable standards for safe driving."; ?></p>
                                 }
-                                
-
-                                
-                                ?>
+	      
+               ?>
           
         </div>
       </li>	    
