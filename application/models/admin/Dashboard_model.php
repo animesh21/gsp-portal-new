985c92259234bce346023044a158658682536200
guru-vachan <guruvachanj@gmail.com>
2018-03-12 12:04:36 +0530

    Update Dashboard_model.php

--- application/models/admin/Dashboard_model.php
+++ application/models/admin/Dashboard_model.php
@@ -264,6 +264,17 @@ class Dashboard_model extends CI_Model {
 		
 	}
 	
+	public function school_that_complete_audit_phase_2_count()
+	{
+	        $this->db->select('a.*, b.name AS state_name,c.name As district_name')
+					->from('gsp_school AS a')
+					->join('states AS b', 'a.state=b.id', 'left')
+					->join('cities AS c', 'a.district=c.id', 'left')
+					->where('a.complete_status','0')
+                                        ->where('a.progress',100)
+	      return $this->db->count_all_results('gsp_school');		
+	}		
+	
 	public function getschool_that_complete_audit_phase_2() {
 		$this->db->where('progress = 100');
 		$this->db->where('date_added >', '2017-11-29 00:00:00');
