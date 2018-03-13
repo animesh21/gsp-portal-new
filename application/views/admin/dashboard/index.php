bb3e166355b32fe1bec5a36ce4f87c21b4c12f68
guru-vachan <guruvachanj@gmail.com>
2018-03-13 14:19:50 +0530

    Update index.php

--- application/views/admin/dashboard/index.php
+++ application/views/admin/dashboard/index.php
@@ -1,7 +1,7 @@
 <h1>Dashboard</h1>
 
 <div class="schools-statics "> <a href="<?php base_url()?>dashboard/year18" target="_blank">
-        <div class="statics-circle"> <span>0</span>
+        <div class="statics-circle"> <span>2</span>
             <label>Phase</label>
         </div>
     </a>
