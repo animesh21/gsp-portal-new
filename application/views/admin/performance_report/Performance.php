7afa391f31c6d2286e9760e3437281befbfa676d
guru-vachan <guruvachanj@gmail.com>
2018-03-17 14:48:17 +0530

    Update Performance.php

--- application/views/admin/performance_report/Performance.php
+++ application/views/admin/performance_report/Performance.php
@@ -2075,15 +2075,24 @@ if ($data == 3 || $data == 4) { ?>
 //4) Does your school recycle any generated waste?
 $waste_recycle = getFiled('Q8Wa1', $schoolUserID);
 if ($waste_recycle == "Y") {
-    $rw=(getFiled('Q6Wa1S5', $schoolUserID) != '') ? getFiled('Q6Wa1S5', $schoolUserID) : 0;
-	$rw1=(getFiled('Q6Wa2S8', $schoolUserID) != '') ? getFiled('Q6Wa2S8', $schoolUserID) : 0;
-	$rw2=(getFiled('Q6Wa3S3', $schoolUserID) != '') ? getFiled('Q6Wa3S3', $schoolUserID) : 0;
+    $rw=(getFiled('Q8Wa1S5', $schoolUserID) != '') ? getFiled('Q8Wa1S5', $schoolUserID) : 0;
+	$rw1=(getFiled('Q8Wa2S8', $schoolUserID) != '') ? getFiled('Q8Wa2S8', $schoolUserID) : 0;
+	$rw2=(getFiled('Q8Wa3S3', $schoolUserID) != '') ? getFiled('Q8Wa3S3', $schoolUserID) : 0;
+	$rw3=(getFiled('Q8Wa4S1', $schoolUserID) != '') ? getFiled('Q8Wa4S1', $schoolUserID) : 0;
+	$rw4=(getFiled('Q8Wa5S1', $schoolUserID) != '') ? getFiled('Q8Wa5S1', $schoolUserID) : 0;
+	$rw5=(getFiled('Q8Wa6S1', $schoolUserID) != '') ? getFiled('Q8Wa6S1', $schoolUserID) : 0;
+	$rw6=(getFiled('Q8Wa7S1', $schoolUserID) != '') ? getFiled('Q8Wa7S1', $schoolUserID) : 0;
   
-    $rcw=(getFiled('Q6Wa1S6', $schoolUserID) != '') ? getFiled('Q6Wa1S6', $schoolUserID) : 0;
-	$rcw1=(getFiled('Q6Wa2S9', $schoolUserID) != '') ? getFiled('Q6Wa2S9', $schoolUserID) : 0;
-	$rcw2=(getFiled('Q6Wa3S4', $schoolUserID) != '') ? getFiled('Q6Wa3S4', $schoolUserID) : 0;
-    $total_generated_waste = $rw + $rw1 + $rw2;
-    $total_recycled_waste = $rcw + $rcw1 + $rcw2;
+    $rcw=(getFiled('Q8Wa1S6', $schoolUserID) != '') ? getFiled('Q8Wa1S6', $schoolUserID) : 0;
+	$rcw1=(getFiled('Q8Wa2S9', $schoolUserID) != '') ? getFiled('Q8Wa2S9', $schoolUserID) : 0;
+	$rcw2=(getFiled('Q8Wa3S4', $schoolUserID) != '') ? getFiled('Q8Wa3S4', $schoolUserID) : 0;
+	$rcw3=(getFiled('Q8Wa4S2', $schoolUserID) != '') ? getFiled('Q8Wa4S2', $schoolUserID) : 0;
+	$rcw4=(getFiled('Q8Wa5S2', $schoolUserID) != '') ? getFiled('Q8Wa5S2', $schoolUserID) : 0;
+	$rcw5=(getFiled('Q8Wa6S2', $schoolUserID) != '') ? getFiled('Q8Wa6S2', $schoolUserID) : 0;
+	$rcw6=(getFiled('Q8Wa7S2', $schoolUserID) != '') ? getFiled('Q8Wa7S2', $schoolUserID) : 0;
+	
+    $total_generated_waste = $rw + $rw1 + $rw2 +$rw3 + $rw4 + $rw5 +$rw6;
+    $total_recycled_waste = $rcw + $rcw1 + $rcw2 + $rcw3 + $rcw4 + $rcw5 + $rcw6;
     $waste_recycled_percent = round((($total_recycled_waste * 100) / $total_generated_waste), 2);
     if ($waste_recycled_percent == 100) {    //100%
         $waste_msg41 = "School recycles 100% of the waste";
