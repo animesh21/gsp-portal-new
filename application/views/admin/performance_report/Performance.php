048391af745653db960e5a5bf4c3924a07f9a839
Sidd024 <siddhartha2488@gmail.com>
2018-03-30 03:46:27 -0700

    Change Code
    Change Code

--- application/views/admin/performance_report/Performance.php
+++ application/views/admin/performance_report/Performance.php
@@ -41,13 +41,6 @@ h3{ font-size:13px!important;}
 .cover figure img {
     width: 700px;
 }
-h2 span {
-    display: inline-block;
-    font-size:10px;
-	position:absolute;
-    left:580px;
-	height:40px;
-}
 </style>
 </head>
 <body>
