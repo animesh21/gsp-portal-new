eea720bd244d9a3bba3596be278bb2f624c9df0b
Sidd024 <siddhartha2488@gmail.com>
2018-04-19 22:49:53 -0700

    Change In Code
    Change In Code

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -43,7 +43,7 @@ class User_model extends CI_Model
    public function UserLoginDownload($argPost)
     {
 		$msg=''; 
-        $query = $this->db->select('*')
+        $query = $this->db->select('a.*, b.*')
             ->from('gsp_user As a')
             ->join('gsp_school AS b', 'b.coemail=a.email', 'left') 		
             ->where(array('a.email' => $argPost['email'], 'a.password' => $argPost['password']), 'b.progress'=>'100', 'b.complete_status'=>'1')
