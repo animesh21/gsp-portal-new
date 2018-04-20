107c02b81af74209e1e093c582ff589f8403bce6
Sidd024 <siddhartha2488@gmail.com>
2018-04-19 22:52:18 -0700

    Change In Code
    Change In Code

--- application/controllers/DownloadLogin.php
+++ application/controllers/DownloadLogin.php
@@ -1,42 +1,232 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class DownloadLogin extends CI_Controller {
-
-    public function __construct() {
+class User_model extends CI_Model
+{
+    public function __construct()
+    {
         parent::__construct();
-        $this->load->helper(array('form', 'security'));
-        $this->load->library('form_validation');
-		$this->load->model('admin/Audit_started_model');
-        $this->load->model('User_model');
-        $this->load->model('School_model');
     }
 
-    public function index() {
-        $data['title'] = 'Green School';
-        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-        $this->form_validation->set_rules('password', 'Password', 'trim|required');
-       if ($this->form_validation->run() == FALSE) {
-            $this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
-        } else {
-            $post = $this->security->xss_clean($this->input->post());
-            $varCheckLogin = $this->User_model->UserLoginDownload($post);
-            if ($varCheckLogin) {
-			    if($varCheckLogin=="success"){
-                redirect(base_url('download/downloadFiles'));
-				}
-				elseif($varCheckLogin=="warning")
-				{
-				  $this->session->set_flashdata('error', "The School Have Completed The GSP Audit So The School Can't Submit Audit Again");
-				}
-            } else if($varCheckLogin=="error") {
-                $this->session->set_flashdata('error', 'Invalid Email/Password!');
-            }
-        }
-	     $this->load->view('download-login');
+   public function UserLogin($argPost)
+    {
+		$msg=''; 
+        $query = $this->db->select('*')
+            ->from('gsp_user')
+            ->where(array('email' => $argPost['email'], 'password' => $argPost['password']))
+            ->get();
+        if ($query->num_rows() > 0) {
+            $row = $query->row();
+			if($row->login_status==0)
+			{
+				$userData = array(
+					'USERNAME' => $row->username,
+					'USER_ID' => $row->id,
+					'status' => $row->status,
+					'lastid' => $row->lastQuestionId
+				);
+            	$this->session->set_userdata($userData);
+            	$msg='success';
+			}
+			else
+			{
+			 	$msg='warning';
+			}
+        }else
+		{
+		   $msg='error';
+		}
+        return $msg;
     }
-    	
+   
+   public function UserLoginDownload($argPost)
+    {
+		$msg=''; 
+        $query = $this->db->select('a.*, b.*')
+            ->from('gsp_user As a')
+            ->join('gsp_school AS b', 'b.coemail=a.email', 'left') 		
+            ->where(array('a.email' => $argPost['email'], 'a.password' => $argPost['password']), 'b.progress'=>'100', 'b.complete_status'=>'1'))
+            ->get();
+        if ($query->num_rows() > 0) {
+            $row = $query->row();
+			if($row->login_status==1)
+			{
+				$userData = array(
+					'USERNAME' => $row->username,
+					'USER_ID' => $row->id,
+					'status' => $row->status,
+					'lastid' => $row->lastQuestionId
+				);
+            	$this->session->set_userdata($userData);
+            	$msg='success';
+			}
+			else
+			{
+			 	$msg='warning';
+			}
+        }else
+		{
+		   $msg='error';
+		}
+        return $msg;
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    public function CreateUser($argPost)
+    {
+        $query = $this->db->select('COUNT(*) count')
+            ->from('gsp_user')
+            ->get();
+
+        $row = $query->row();
+
+        $update = array(
+            'email' => $argPost['email'],
+            'password' => $argPost['Password'],
+            'username' => $argPost['username']
+        );
+		 $query1 = $this->db->select('COUNT(*) as count')
+                ->from('gsp_user')
+                ->where('email', $argPost['email'])    
+                ->get()->result();
+        
+        if($query1[0]->count==0){
+        $this->db->insert('gsp_user', $update);//insert if does not exist
+        }
+        $query = $this->db
+            ->select('id')
+            ->from('gsp_user')
+            ->where('email', $argPost['email'])
+            ->get();
+        $row = $query->row();
+        return $row->id;
+    }
+
+    public function Login($argPost)
+    {
+        $query = $this->db->select('*')
+            ->from('gsp_user')
+            ->where(array('email' => $argPost['email'], 'password' => $argPost['password'], 'status' => '0'))
+            ->get();
+        if ($query->num_rows() > 0) {
+            $row = $query->row();
+            return (int)$row->id;
+        }
+        return null;
+    }
+
+    /*
+     * Get States
+     */
+    public function getStates()
+    {
+        $states = array('' => 'Select State');
+        $data = $this->db->get_where('states', array('country_id =' => '1'))->result();
+        foreach ($data as $d) {
+            $states[$d->id] = $d->name;
+        }
+        return $states;
+    }
+
+    /*
+     * Get Cities
+     */
+    public function getCIties($argID)
+    {
+        $cities = array();    
+	$this->db->order_by("name");
+        $data = $this->db->get_where('cities', array('state_id =' => $argID))->result();
+        return $data;
+    }
+
+    /*
+     * Get Cities All New Added Code By Siddhant
+     */
+    public function getCitiesAll($argID)
+    {
+        $cities = array();
+        $data = $this->db->get_where('cities', array('state_id =' => $argID))->result();
+        //echo $this->db->last_query(); exit;
+        foreach ($data as $d) {
+            $cities[$d->id] = $d->name;
+        }
+        return $cities;
+    }
+
+    public function ChangePassword($pass)
+    {
+        $this->db->set('password', $pass['pass'])
+            ->where(array('id' => $this->session->userdata('USER_ID')))//which row want to upgrade
+            ->update('gsp_user');
+        print_r("Update");
+        $this->session->sess_destroy();
+        return true;
+    }
+
+    function CheckPassword($pass)
+    {
+        $current = $this->db->select('*')->where('id', $this->session->userdata('USER_ID'))->from('gsp_user')->get()->row()->password;
+        if ($current == $pass['oldPassword'])
+            return true;
+        else
+            return false;
+    }
+
+    public function forgetPassword()
+    {
+        //echo $argEmail; exit;
+        $this->load->helper('string');
+        $email_count = $this->db->get_where('gsp_user', array('email' => $this->input->post('val')))->num_rows();
+        //echo $this->db->last_query(); exit;
+        if ($email_count > 0) {
+            $varNewPass = strtolower(random_string('alnum', 5));
+            $arr = array(
+                'password' => $varNewPass
+            );
+            $this->db->where('email', $this->input->post('val'));
+            if ($this->db->update('gsp_user', $arr)) {
+                $query = $this->db->select('*')
+                    ->from('gsp_user')
+                    ->where('email', $this->input->post('val'))
+                    ->get()->row();
+//                echo '<pre>';                print_r($query); exit;
+
+                $this->load->library('email');
+                $config['mailtype'] = 'html';
+                $this->email->initialize($config);
+                $from = "support@greenschoolsprogramme.org";
+                $to = $query->email;
+                $subject = "GSP Forget Password";
+                $msg = "Dear &nbsp;";
+                $msg .= $query->username . "," . "<br><br>";
+                $msg .= "Your new password for GSP Login.<br><br>";
+                $msg .= $varNewPass . "<br><br>";
+                $msg .= "Regards,<br/>GSP Team.<br><br>";
+                $this->email->to($to);
+                $this->email->from($from, "Green Schools Programme");
+                $this->email->subject($subject);
+                $this->email->message($msg);
+                $this->email->send();
+//                echo $this->email->print_debugger();
+//                die();
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
 
 ?>
