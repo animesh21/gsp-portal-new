1b17b813533d7cbe852f93e22b21ec2a08733cfd
Sidd024 <siddhartha2488@gmail.com>
2018-04-24 01:33:08 -0700

    Summary File Of 2017 Report Details
    Summary File Of 2017 Report Details

---
+++ application/views/admin/audit/summary.php
@@ -0,0 +1,51 @@
+<style type="text/css">
+    .panel{border-radius: 0px !important;}
+    .danger{color:red !important; font-size: 12px !important; font-weight: bold !important;}
+    .list-unstyled li{ list-style:none; padding:3px 3px 3px 3px;}
+    .list-unstyled li input[type="checkbox"]{ opacity:0.7;  padding:3px 3px 3px 3px;}
+    .list-unstyled li input[type="radio"]{ opacity:0.7;  padding:3px 3px 3px 3px;}
+    .list-inline li{ list-style:none; padding:3px 3px 3px 3px;}
+    .list-inline li input[type="checkbox"]{ opacity:0.7;  padding:3px 3px 3px 3px;}
+    .list-inline li input[type="radio"]{ opacity:0.7;  padding:3px 3px 3px 3px;}
+	.sorting_1{ background:#e86549!important; width:60px!important; color:#FFFFFF!important;}
+</style>
+<div class="top-area">
+  <h2>Summary Of Audit</h2>
+</div>
+<div class="row">
+  <div class="col-md-12">
+    <table class="display dataTable no-footer">
+      <thead>
+        <tr role="row">
+          <th>Phase</th>
+          <th>Total School Registrations</th>
+          <th>Schools That Started The Audit</th>
+          <th>School submitted the Audit in Phase 1</th>
+          <th>Schools Started The Audit But Did Not Complete</th>
+          <th>Schools Did Not Start The Audit</th>
+          <th>2017 Registration</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr role="row" class="odd">
+          <td class="sorting_1">Phase-1</td>
+          <td>2863</td>
+          <td>1794</td>
+          <td>1194</td>
+          <td>570</td>
+          <td>1069</td>
+          <td>1758</td>
+        </tr>
+        <tr role="row" class="even">
+          <td class="sorting_1">Phase-2</td>
+          <td>1784</td>
+          <td>603</td>
+          <td>71</td>
+          <td>596</td>
+          <td>1181</td>
+          <td>187</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
