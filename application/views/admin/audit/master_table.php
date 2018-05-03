17de9c41c24f8c083f348786722d3b95d80da555
Sidd024 <siddhartha2488@gmail.com>
2018-05-03 06:43:30 -0700

    Change Code
    Change Code

--- application/views/admin/audit/master_table.php
+++ application/views/admin/audit/master_table.php
@@ -1,52 +1,77 @@
 <div class="top-area">
-    <h2>Master View</h2>
-    
+  <h2>Master View</h2>
 </div>
-<div style="overflow: scroll"> 
-    <table class="display dataTable no-footer" id="tablePerformance">
-	<thead>
-	    <tr>
-		<th>S.No</th>
-		<th>School Id</th>
-		<th>Name of the School/Institution</th>
-		<th>Principal&#39;s Name</th>
-                <th>Principal Email</th>
-		<th>GSP Coordintor Name</th>
-		<th>GSP Coordintor Email</th>
-                <th>Participate Year</th>
-                <th>Audit submit 2015</th>
-                <th>Audit submit 2016</th>
-                <th>Audit submit 2017</th>
-		
-	    </tr> 
-	</thead>
-	<tbody>
-	    <?php 
+<div style="overflow: scroll">
+  <table class="display dataTable no-footer" id="tablePerformance">
+    <thead>
+      <tr>
+        <th>S.No</th>
+        <th>School Id</th>
+        <th>Name of the Schools submitting GSP Audit in 2017</th>
+        <th>State</th>
+        <th>District</th>
+        <th>City</th>
+        <th>2017 Submission</th>
+        <th>2016 Submission</th>
+        <th>2015 submission</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php 
 		ini_set('memory_limit','-1');
-                foreach ($record as $r){ 
-                    //print_r($r);                    die();
-	$r->gsp_15 = (!empty($r->gsp_15)) ? 'Yes': 'NO';
-        $r->year_2016 = (!empty($r->year_2016)) ? 'Yes': 'NO';
-        $r->year_17 = (!empty($r->year_17)) ? 'Yes': 'NO';
+        foreach ($record as $r){ 
 		?>
-    	    <tr>
-    		<td><?php echo $r->id; ?></td>
-    		<td><?php echo $r->schooid; ?></td>
-    		<td><?php echo $r->school_name; ?></td>
-    		<td><?php echo $r->principal_name; ?></td>
-    		<td><?php echo $r->email; ?></td>
-                <td><?php echo $r->co_email; ?></td>
-                <td><?php echo $r->co_name; ?></td>
-    		<td><?php echo $r->years; ?></td>
-    		<td><?php echo $r->gsp_15; ?></td>
-    		<td><?php echo $r->year_2016; ?></td>
-                <td><?php echo $r->year_17; ?></td>
-    		
-    	    </tr>
-    <?php
+      <tr>
+        <td><?php echo $r->id; ?></td>
+        <td><?php echo $r->school_id; ?></td>
+        <td><?php echo $r->school_name; ?></td>
+        <td><?php echo $r->state; ?></td>
+        <td><?php echo $r->district; ?></td>
+        <td><?php echo $r->city; ?></td>
+        <?php 
+		     if($r->submission_2017>=70):
+		   echo "<td style='background-color: green; color:#000;'>".$r->submission_2017."</td>";
+		  elseif($r->submission_2017>=50 && $percentage<=69.9):
+		  		   echo "<td style='background:yellow; color:000;'>".$r->submission_2017."</td>";
+		  elseif($r->submission_2017>=35 && $percentage<=49.9):
+		  		   echo "<td style='background:orange; color:000;'>".$r->submission_2017."</td>";
+		  elseif($r->submission_2017<=34.9):
+		  		   echo "<td style='background-color: red; color:#000;'>".$r->submission_2017."</td>";
+		  else:
+		    echo "<td>Not Rated</td>";
+		  endif;
+		  ?>
+        <?php 
+		  if($r->submission_2016_rank==1):
+		   echo "<td style='background-color: green; color:#000;'>".$r->submission_2016."</td>";
+		  elseif($r->submission_2016_rank==2):
+		  		   echo "<td style='background:yellow; color:000;'>".$r->submission_2016."</td>";
+		  elseif($r->submission_2016_rank==3):
+		  		   echo "<td style='background:orange; color:000;'>".$r->submission_2016."</td>";
+		  elseif($r->submission_2016_rank==4):
+		  		   echo "<td style='background-color: red; color:#000;'>".$r->submission_2016."</td>";
+		  else:
+		    echo "<td>NA</td>";
+		  endif;
+		?>
+        <?php 
+		  if($r->submission_2015_rank==1):
+		   echo "<td style='background-color: green; color:#000;'>".$r->submission_2015."</td>";
+		  elseif($r->submission_2015_rank==2):
+		  		   echo "<td style='background:yellow; color:000;'>".$r->submission_2015."</td>";
+		  elseif($r->submission_2015_rank==3):
+		  		   echo "<td style='background:orange; color:000;'>".$r->submission_2015."</td>";
+		  elseif($r->submission_2015_rank==4):
+		  		   echo "<td style='background-color: red; color:#000;'>".$r->submission_2015."</td>";
+		  else:
+		    echo "<td>NA</td>";
+		  endif;
+		
+		?>
+      </tr>
+      <?php
 }
 ?>
-	</tbody>
-    </table>
-</div> 
-
+    </tbody>
+  </table>
+</div>
