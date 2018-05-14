88cc49758199a10ed59fe365a6a3390e24fd7581
guru-vachan <guruvachanj@gmail.com>
2018-05-14 18:29:39 +0530

    Update Webhook.php

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -9,7 +9,7 @@
 class Webhook extends CI_Controller {
     public function push() {
         echo 'Code is pushed to the repository';
-        exec('cd /usr/share/nginx/html/mountedspace/greenschoolsprogramme/audit2017/scripts');
+        exec('cd /usr/share/nginx/html/mountedspace/greenschoolsprogramme/audit/scripts');
         echo exec('sudo -u greenschool_u bash ./gitpull.sh');
         echo "\n\rI am ";
         echo exec('whoami');
