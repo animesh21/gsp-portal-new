eaf2cd1b9deecb1602dacfcf504d70dfa6f9fec1
guru-vachan <guruvachanj@gmail.com>
2018-05-17 15:08:27 +0530

    Update index.php

--- application/views/admin/dashboard/index.php
+++ application/views/admin/dashboard/index.php
@@ -1,15 +1,6 @@
 <h1>Dashboard</h1>
 
-<?php
- echo '<pre>';
- print_r($_SERVER);
- echo "hiiiii";
- echo basename($_SERVER['SCRIPT_NAME']);
- echo $_SERVER['SCRIPT_NAME'] ;
- echo "guru";
- echo str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
- die();
- ?>
+
 
 <div class="schools-statics "> <a href="<?php base_url()?>dashboard/year18" target="_blank">
         <div class="statics-circle"> <span>2</span>
