3b0f7b01e2a33dfd8476c63be8952dfeedd6fe53
Sidd024 <siddhartha2488@gmail.com>
2018-05-22 02:19:25 -0700

    Change Code
    Change Code

--- application/views/login.php
+++ application/views/login.php
@@ -50,42 +50,7 @@
                     $(document).ready(function () {
                         $("li").removeAttr("title");
                     });
-
-					
-				/***Browser Compatability Code Test***/	
-					
-					$(document).ready(function(){
-  var browser = '';
-var browserVersion = 0;
-
-if (/Opera[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
-    browser = 'Opera';
-} else if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)) {
-    browser = 'MSIE';
-} else if (/Navigator[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
-    browser = 'Netscape';
-} else if (/Chrome[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
-    browser = 'Chrome';
-} else if (/Safari[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
-    browser = 'Safari';
-    /Version[\/\s](\d+\.\d+)/.test(navigator.userAgent);
-    browserVersion = new Number(RegExp.$1);
-} else if (/Firefox[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
-    browser = 'Firefox';
-}
-if(browser==="Opera" || browser==="MSIE" || browser==="Netscape" || browser==="Safari"){
-	window.location.href="<?php echo base_url("login/browser_check"); ?>";  
-}
-else if(browser==="Chrome" || browser==="Firefox"){
-	window.location.href="<?php echo base_url("login"); ?>";  
-}
-});
-					
-					
-					
-					
-					
-					
+		
                 </script>
     <script src="<?php echo base_url(''); ?>assets/js/main.js"></script>
     <!-- Gem jQuery -->
