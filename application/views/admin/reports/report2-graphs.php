ef9724bdae226b50b9832a1220a9f48554105e2e
Sidd024 <siddhartha2488@gmail.com>
2018-05-24 02:04:17 -0700

    Change Button Name
    Change Button Name

--- application/views/admin/reports/report2-graphs.php
+++ application/views/admin/reports/report2-graphs.php
@@ -4,7 +4,7 @@
     <label for="pwd">Select First State :</label>
     <?php echo form_dropdown('state1', $states, $this->input->post('state1'), array('class' => 'form-control', 'id' => 'country-select')); ?></div>
   <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><br/>
-    <input type="submit" class="btn" style="background-color: rgb(232, 101, 73); color: #fff;" value="Compare" name="compare" onclick="return Validate()">
+    <input type="submit" class="btn" style="background-color: rgb(232, 101, 73); color: #fff;" value="Show" name="compare" onclick="return Validate()">
   </div>
   <?php echo form_close();?> </div>
 <div class="wrapper">
