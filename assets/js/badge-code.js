63770ee2710551a4603cd3381065011b9cb86298
Sidd024 <siddhartha2488@gmail.com>
2018-06-14 04:27:08 -0700

    Change In Badge Code Js
    Change In Badge Code Js

--- assets/js/badge-code.js
+++ assets/js/badge-code.js
@@ -21,6 +21,6 @@ var d2 = new Date(get_current_date());
 var timeDiff = d1.getTime() - d2.getTime();
 var DaysDiff = timeDiff / (1000 * 3600 * 24);
    if(DaysDiff > 0){
-	  $('.badges').append('<a href="http://www.greenschoolsprogramme.org/"><img src=http://www.greenschoolsprogramme.org/audit2017/assets/img/images/GSPLogocolour.jpg width=120px /></a>');
+	  $('.badges').append('<a href="http://www.greenschoolsprogramme.org/"><img src=http://www.greenschoolsprogramme.org/audit/assets/img/images/GSPLogocolour.jpg width=120px /></a>');
    }
 }
