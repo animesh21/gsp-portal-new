2063eb59227fd3fefd886986cdab97468699ff05
rohit12371 <31234754+rohit12371@users.noreply.github.com>
2018-07-02 16:13:15 +0530

    change code
    change code

--- 18/application/views/food.php
+++ 18/application/views/food.php
@@ -500,21 +500,14 @@ data-target="#airModal" type="button">UPLOAD FILES </button>
     </thead>
     <tbody>
       <?php foreach ($midDayMeal as $f) { ?>
-	  <?php if($extension == "jpg" || $extension == "jpeg"){ ?>    
-		<td> <img style="width:62px; height:46px;" ;" src="<?php echo base_url() ?>uploads/files/<?php echo $f->file_name ?>" class="img-responsive" /></td>
-                <?php }else{ ?>
-	     <td><img style="width:62px; height:46px;" src="<?php echo base_url() ?>assets/img/download.jpg" class="img-responsive" /></td>
-               <?php }?> 
-                <?php $name = str_replace(" ", "_", $f->name . "_Mid_Day_Meal_"); ?>
-				
-                <td class="upload edit"><?php echo str_replace($name, "", $f->file_name); ?></td>
-                <td><a href="javascript:void(0)" class="air-delete-files" data-id="<?php echo $f->id; ?>"><img src="<?php echo base_url(); ?>assets/front/images/delete.png" style="position:relative; top:5px" /></a></td>
-                <td><a href="<?php echo base_url() ?>uploads/files/<?php echo $f->file_name; ?>" download="<?php echo $f->file_name; ?>"><span class="glyphicon glyphicon-download-alt"></span></a></td>
-       
-	    </tr>
-        <?php } ?>
-	  
-      
+      <tr id="index<?php echo $f->id; ?>">
+        <td><iframe style="width:62px; height:46px;" ;" src="<?php echo base_url() ?>uploads/files/<?php echo $f->file_name ?>" class="img-responsive" width="50" height="50"></iframe></td>
+        <?php $name = str_replace(" ", "_", $f->name . "_Mid_Day_Meal_"); ?>
+        <td class="upload edit"><?php echo str_replace($name, "", $f->file_name); ?></td>
+        <td><a href="javascript:void(0)" class="air-delete-files" data-id="<?php echo $f->id; ?>"><img src="<?php echo base_url(); ?>assets/front/images/delete.png" style="position:relative; top:5px" /></a></td>
+        <td><a href="<?php echo base_url() ?>uploads/files/<?php echo $f->file_name; ?>" download="<?php echo $f->file_name; ?>"><span class="glyphicon glyphicon-download-alt"></span></a></td>
+      </tr>
+      <?php } ?>
     </tbody>
   </table>
 </div>
