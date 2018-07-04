ba864b9f208b0af3fa39859f80aa7df2b02ed5e2
Sidd024 <siddhartha2488@gmail.com>
2018-07-04 01:05:19 -0700

    Change Code
    Change Code

--- 18/application/views/PrimaryWaste.php
+++ 18/application/views/PrimaryWaste.php
@@ -27,7 +27,9 @@
             <div id="samplt" class="fin-prog progress-bar" role="progressbar" style="width: <?php echo progressBarValue($this->session->userdata('USER_ID')).'%'; ?>; height: 35px;"><?php echo progressBarValue($this->session->userdata('USER_ID')).'%'; ?></div>
         </div>
         <div class="intro">
-            <h1>WASTE<span style="text-transform:capitalize">(Primary)</span></h1>
+            <h1>WASTE<span style="text-transform:capitalize">(Primary)</span> <small class="pull-right" style="position: relative;
+    top: -28px;
+    font-weight: bolder;"><i class="h6"><strong>If the new changes are not saved, please press Ctrl+F5</strong></i></small></h1>
             <div class="video"> <a class="lptext" href="#" data-toggle="modal" data-target="#videoModal"><img src="http://greenschoolsprogramme.org/audit2015/upload/templates/tfr_responsive/images/video-icon.png" style="width: 45px; height: 40px; margin-top:47px;"></a></div>
         </div>
         <p> Schools can provide excellent educational opportunities for creating awareness about waste and its management.<br>
