e087a2f52ce6818fbd5378e35b08c7153d7779e3
Sidd024 <siddhartha2488@gmail.com>
2018-07-11 02:24:15 -0700

    Change Code
    Change Code

--- 18/application/models/admin/Report.php
+++ 18/application/models/admin/Report.php
@@ -62,7 +62,7 @@ class Report extends CI_Model {
 	   $this->db->where("state",$region);
 	   $this->db->where("complete_status",'0');
 	   $this->db->where("progress=",'5');
-	   $this->db->where("date_added <",'2017-11-20'); 
+	   /*$this->db->where("date_added <",'2017-11-20'); */
 	   $data=$this->db->select("COUNT(id) AS countlabel")->from('gsp_school')->get()->row();
 	   return $data->countlabel;
 	  
@@ -75,7 +75,7 @@ class Report extends CI_Model {
 	   $this->db->where("complete_status",'0');
 	   $this->db->where("progress >",'5');
 	   $this->db->where("progress <",'100');
-	   $this->db->where("date_added <",'2017-11-20'); 
+	  /* $this->db->where("date_added <",'2017-11-20'); */
 	   $data=$this->db->select("COUNT(id) AS countlabel")->from('gsp_school')->get()->row();
 		return $data->countlabel;
 	  
@@ -87,7 +87,7 @@ class Report extends CI_Model {
 	   $this->db->where("state",$region);
 	   $this->db->where("complete_status",'1');
 	   $this->db->where("progress =",'100');
-	   $this->db->where("date_added <",'2017-11-20'); 
+	   /*$this->db->where("date_added <",'2017-11-20'); */
 	   $data=$this->db->select("COUNT(id) AS countlabel")->from('gsp_school')->get()->row();
 	   return $data->countlabel;
 	   
@@ -97,7 +97,7 @@ class Report extends CI_Model {
     public function singleregisterparticipationByZone($region)
 	{
 	   $this->db->where("state",$region);
-	   $this->db->where("date_added<",'2017-11-20'); 
+	  /* $this->db->where("date_added<",'2017-11-20'); */
 	   $data=$this->db->select("COUNT(id) AS countlabel")->from('gsp_school')->get()->row();
 	   return $data->countlabel;
 	}
