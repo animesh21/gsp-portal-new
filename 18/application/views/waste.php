d084667166d05483a80b286997feac9fcadd7574
rohit12371 <31234754+rohit12371@users.noreply.github.com>
2018-07-11 17:01:10 +0530

    change questio no 9 t0 11
    change questio no 9 t0 11

--- 18/application/views/waste.php
+++ 18/application/views/waste.php
@@ -4063,7 +4063,7 @@ if($("input[name='Q4Wa2S1']").prop('checked') == false && $("input[name='Q4Wa2S2
 	<script>
 	$("#wastenext").click(function(e){
 if($("input[name='Q15Wa2S1']").prop('checked') == false && $("input[name='Q15Wa2S2']").prop('checked') == false && $("input[name='Q15Wa2S3']").prop('checked') == false && $("input[name='Q15Wa2S4']").prop('checked') == false){
-	 alert("9(b) What kind of waste is being burnt/ incinerated?");
+	 alert("11(b) What kind of waste is being burnt/ incinerated?");
 	 e.preventDefault();
 }
 });	
