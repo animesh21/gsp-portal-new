43954744f349a02526edf267a71c12e080e63652
Sidd024 <siddhartha2488@gmail.com>
2018-07-17 12:09:28 +0530

    Change Code
    Change Code

--- 18/application/models/School_model.php
+++ 18/application/models/School_model.php
@@ -177,8 +177,8 @@ class School_model extends CI_Model
             try {
 		$getProgress=$this->db->select("*")->from("gsp_school")->where(array('userid' => $data['userid']))->get()->row();
 		$progress= $getProgress->progress;
-		if($progress>$data['completeness']){
-		unset($data['completeness']);
+		if($progress>$data['progress']){
+		unset($data['progress']);
                 $this->db->set($data['questionid'], $data['answer'])
                     ->where(array('userid' => $data['userid']))//which row want to upgrade
                     ->update('gsp_school');
