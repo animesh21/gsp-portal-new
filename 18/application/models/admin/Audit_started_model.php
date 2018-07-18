802a52355e68a766c1b8a6a9ac7abd984df0e835
Sidd024 <siddhartha2488@gmail.com>
2018-07-18 06:33:11 -0700

    add getallPartnerData
    add getallPartnerData

--- 18/application/models/admin/Audit_started_model.php
+++ 18/application/models/admin/Audit_started_model.php
@@ -33,7 +33,285 @@ class Audit_started_model extends CI_Model {
                         ->order_by('a.id', 'desc')
                         ->get()->result();
 	                    return $data;				
-	}			
+	}	
+	
+	
+	public function getallPartnerData() {
+	   
+        $isdCode = '+91';
+        $output .= '"S.No",';
+        $output .= '"School ID",';
+		$output .= '"UDISE Code",';
+        $output .= '"School Name",';
+        $output .= '"Address1",';
+        $output .= '"Address2",';
+        $output .= '"Country",';
+        $output .= '"State",';
+        $output .= '"District",';
+        $output .= '"City",';
+        $output .= '"Pincode",';
+        $output .= '"ISD Code",';
+        $output .= '"STD Code",';
+        $output .= '"Landline Number",';
+        $output .= '"School Email",';
+        $output .= '"Principal Name",';
+        $output .= '"Principal Mobile",';
+        $output .= '"Coordinator Name",';
+        $output .= '"Coordinator Email",';
+        $output .= '"Coordinator Mobile",';
+        $output .= '"Password",';
+        $output .= '"Date & Time",';
+        $output .= '"Completeness",';
+        
+		$output .= "\n"; 
+		 $output .= "\n";
+		 $output .= '"","","","","","","","","Church of South India","","","","","","","",';
+		$output .= "\n"; 
+		 $output .= "\n";
+		
+		$data=get_partner(1);
+		$k = 1;
+        foreach ($data as $a) 
+		
+		
+		{
+            $output .= '"' . $k . '",';
+            $output .= '"' . $a->id . '",';
+			$output .= '"' . $a->udise . '",';
+            $output .= '"' . $a->name . '",';
+            $output .= '"' . $a->address1 . '",';
+            $output .= '"' . $a->address2 . '",';
+            $output .= '"' . $a->country . '",';
+            $output .= '"' . $a->state_name . '",';
+            $output .= '"' . $a->district_name . '",';
+            $output .= '"' . $a->city . '",';
+            $output .= '"' . $a->pincode . '",';
+            $output .= '"' . $isdCode . '",';
+            $output .= '"' . $a->std . '",';
+            $output .= '"' . $a->telephone . '",';
+            $output .= '"' . $a->schoolemail . '",';
+            $output .= '"' . $a->principle_name . '",';
+            $output .= '"' . $a->mobile . '",';
+            $output .= '"' . $a->coname . '",';
+            $output .= '"' . $a->coemail . '",';
+            $output .= '"' . $a->comobile . '",';
+            $output .= '"' . $a->password . '",';
+            //$output .='"'.date('d-m-Y H:i:s', strtotime($row['datetime'])).'",';
+            $output .= '"' . date('Y-m-d H:i:s', strtotime($a->date_added)) . '",';
+            $output .= '"' . $a->progress . '%",';
+            $output .= "\n";
+            $k++;
+        }
+		
+		 $output .= "\n"; 
+		 $output .= "\n";
+		 $output .= '"","","","","","","","","Jawahar Navodaya Vidyalaya","","","","","","","",';
+		 $output .= "\n"; 
+		 $output .= "\n";
+		
+		$data=get_partner(2);
+		 
+		$k = 1;
+		
+        foreach ($data as $a) {
+		    
+            $output .= '"' . $k . '",';
+            $output .= '"' . $a->id . '",';
+			$output .= '"' . $a->udise . '",';
+            $output .= '"' . $a->name . '",';
+            $output .= '"' . $a->address1 . '",';
+            $output .= '"' . $a->address2 . '",';
+            $output .= '"' . $a->country . '",';
+            $output .= '"' . $a->state_name . '",';
+            $output .= '"' . $a->district_name . '",';
+            $output .= '"' . $a->city . '",';
+            $output .= '"' . $a->pincode . '",';
+            $output .= '"' . $isdCode . '",';
+            $output .= '"' . $a->std . '",';
+            $output .= '"' . $a->telephone . '",';
+            $output .= '"' . $a->schoolemail . '",';
+            $output .= '"' . $a->principle_name . '",';
+            $output .= '"' . $a->mobile . '",';
+            $output .= '"' . $a->coname . '",';
+            $output .= '"' . $a->coemail . '",';
+            $output .= '"' . $a->comobile . '",';
+            $output .= '"' . $a->password . '",';
+            //$output .='"'.date('d-m-Y H:i:s', strtotime($row['datetime'])).'",';
+            $output .= '"' . date('Y-m-d H:i:s', strtotime($a->date_added)) . '",';
+            $output .= '"' . $a->progress . '%",';
+            $output .= "\n";
+            $k++;
+        }
+		
+		
+		
+		 $output .= "\n"; 
+		 $output .= "\n";
+		 $output .= '"","","","","","","","","Kendriya Vidyalaya Sangathan (KVS)","","","","","","","",';
+		 $output .= "\n"; 
+		 $output .= "\n";
+		
+		$data=get_partner(3);
+		 
+		
+		 $k = 1;
+        foreach ($data as $a) {
+		   
+            $output .= '"' . $k . '",';
+            $output .= '"' . $a->id . '",';
+			$output .= '"' . $a->udise . '",';
+            $output .= '"' . $a->name . '",';
+            $output .= '"' . $a->address1 . '",';
+            $output .= '"' . $a->address2 . '",';
+            $output .= '"' . $a->country . '",';
+            $output .= '"' . $a->state_name . '",';
+            $output .= '"' . $a->district_name . '",';
+            $output .= '"' . $a->city . '",';
+            $output .= '"' . $a->pincode . '",';
+            $output .= '"' . $isdCode . '",';
+            $output .= '"' . $a->std . '",';
+            $output .= '"' . $a->telephone . '",';
+            $output .= '"' . $a->schoolemail . '",';
+            $output .= '"' . $a->principle_name . '",';
+            $output .= '"' . $a->mobile . '",';
+            $output .= '"' . $a->coname . '",';
+            $output .= '"' . $a->coemail . '",';
+            $output .= '"' . $a->comobile . '",';
+            $output .= '"' . $a->password . '",';
+            //$output .='"'.date('d-m-Y H:i:s', strtotime($row['datetime'])).'",';
+            $output .= '"' . date('Y-m-d H:i:s', strtotime($a->date_added)) . '",';
+            $output .= '"' . $a->progress . '%",';
+            $output .= "\n";
+            $k++;
+        }
+		
+		$output .= "\n"; 
+		 $output .= "\n";
+		 $output .= '"","","","","","","","","Montfortian Education Foundation","","","","","","","",';
+		 $output .= "\n"; 
+		 $output .= "\n";
+		
+		$data=get_partner(4);
+		 
+		$k = 1;
+		
+        foreach ($data as $a) {
+		    
+            $output .= '"' . $k . '",';
+            $output .= '"' . $a->id . '",';
+			$output .= '"' . $a->udise . '",';
+            $output .= '"' . $a->name . '",';
+            $output .= '"' . $a->address1 . '",';
+            $output .= '"' . $a->address2 . '",';
+            $output .= '"' . $a->country . '",';
+            $output .= '"' . $a->state_name . '",';
+            $output .= '"' . $a->district_name . '",';
+            $output .= '"' . $a->city . '",';
+            $output .= '"' . $a->pincode . '",';
+            $output .= '"' . $isdCode . '",';
+            $output .= '"' . $a->std . '",';
+            $output .= '"' . $a->telephone . '",';
+            $output .= '"' . $a->schoolemail . '",';
+            $output .= '"' . $a->principle_name . '",';
+            $output .= '"' . $a->mobile . '",';
+            $output .= '"' . $a->coname . '",';
+            $output .= '"' . $a->coemail . '",';
+            $output .= '"' . $a->comobile . '",';
+            $output .= '"' . $a->password . '",';
+            //$output .='"'.date('d-m-Y H:i:s', strtotime($row['datetime'])).'",';
+            $output .= '"' . date('Y-m-d H:i:s', strtotime($a->date_added)) . '",';
+            $output .= '"' . $a->progress . '%",';
+            $output .= "\n";
+            $k++;
+        }
+		
+		
+		$output .= "\n"; 
+		 $output .= "\n";
+		 $output .= '"","","","","","","","","Mount Litera Zee Schools","","","","","","","",';
+		 $output .= "\n"; 
+		 $output .= "\n";
+		
+		$data=get_partner(5);
+		 
+		$k = 1;
+		
+        foreach ($data as $a) {
+		    
+            $output .= '"' . $k . '",';
+            $output .= '"' . $a->id . '",';
+			$output .= '"' . $a->udise . '",';
+            $output .= '"' . $a->name . '",';
+            $output .= '"' . $a->address1 . '",';
+            $output .= '"' . $a->address2 . '",';
+            $output .= '"' . $a->country . '",';
+            $output .= '"' . $a->state_name . '",';
+            $output .= '"' . $a->district_name . '",';
+            $output .= '"' . $a->city . '",';
+            $output .= '"' . $a->pincode . '",';
+            $output .= '"' . $isdCode . '",';
+            $output .= '"' . $a->std . '",';
+            $output .= '"' . $a->telephone . '",';
+            $output .= '"' . $a->schoolemail . '",';
+            $output .= '"' . $a->principle_name . '",';
+            $output .= '"' . $a->mobile . '",';
+            $output .= '"' . $a->coname . '",';
+            $output .= '"' . $a->coemail . '",';
+            $output .= '"' . $a->comobile . '",';
+            $output .= '"' . $a->password . '",';
+            //$output .='"'.date('d-m-Y H:i:s', strtotime($row['datetime'])).'",';
+            $output .= '"' . date('Y-m-d H:i:s', strtotime($a->date_added)) . '",';
+            $output .= '"' . $a->progress . '%",';
+            $output .= "\n";
+            $k++;
+        }
+		
+		$output .= "\n"; 
+		 $output .= "\n";
+		 $output .= '"","","","","","","","","Satya Bharti Foundation","","","","","","","",';
+		 $output .= "\n"; 
+		 $output .= "\n";
+		
+		$data=get_partner(6);
+		 
+		
+		$k = 1;
+        foreach ($data as $a) {
+		    
+            $output .= '"' . $k . '",';
+            $output .= '"' . $a->id . '",';
+			$output .= '"' . $a->udise . '",';
+            $output .= '"' . $a->name . '",';
+            $output .= '"' . $a->address1 . '",';
+            $output .= '"' . $a->address2 . '",';
+            $output .= '"' . $a->country . '",';
+            $output .= '"' . $a->state_name . '",';
+            $output .= '"' . $a->district_name . '",';
+            $output .= '"' . $a->city . '",';
+            $output .= '"' . $a->pincode . '",';
+            $output .= '"' . $isdCode . '",';
+            $output .= '"' . $a->std . '",';
+            $output .= '"' . $a->telephone . '",';
+            $output .= '"' . $a->schoolemail . '",';
+            $output .= '"' . $a->principle_name . '",';
+            $output .= '"' . $a->mobile . '",';
+            $output .= '"' . $a->coname . '",';
+            $output .= '"' . $a->coemail . '",';
+            $output .= '"' . $a->comobile . '",';
+            $output .= '"' . $a->password . '",';
+            //$output .='"'.date('d-m-Y H:i:s', strtotime($row['datetime'])).'",';
+            $output .= '"' . date('Y-m-d H:i:s', strtotime($a->date_added)) . '",';
+            $output .= '"' . $a->progress . '%",';
+            $output .= "\n";
+            $k++;
+        }
+		
+		
+
+        return $output;
+    }
+		
+	
 	
 	
     public function row_delete($id){
