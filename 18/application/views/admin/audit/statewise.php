5474fb1f4888b64d9288addd8e07c58fa96d126f
Sidd024 <siddhartha2488@gmail.com>
2018-07-19 05:52:18 -0700

    addd all up in the list
    addd all up in the list

--- 18/application/views/admin/audit/statewise.php
+++ 18/application/views/admin/audit/statewise.php
@@ -6,7 +6,7 @@
     <div class="col-lg-5">
         <div class="input-group">
 
-            <?php echo form_dropdown('state',array('1'=>'All','0'=>$states),$this->input->post('state'),array('class' => 'form-control', 'id' => 'country-select')); ?>
+            <?php echo form_dropdown('state',$states,$this->input->post('state'), array('class' => 'form-control', 'id' => 'country-select')); ?>
 
         </div>
 
