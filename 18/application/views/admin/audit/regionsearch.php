f7d0f5388c37386560e62abdecbd14dfb6b057fe
Sidd024 <siddhartha2488@gmail.com>
2018-07-20 04:52:07 -0700

    code remove
    code remove

--- 18/application/views/admin/audit/regionsearch.php
+++ 18/application/views/admin/audit/regionsearch.php
@@ -1,6 +1,3 @@
-<div class="top-area">
-    <h2>REGISTRATION BY STATE  <a href="<?php echo base_url('admin/audit_started/excel/' . $val); ?>"><button class="exportBtn">Export to Excel</button></a></h2>
-</div>
 <div class="row" style="margin: 20px 0; min-height: 30px">
 
     <?php echo form_open(base_url('admin/audit_started/regionsearch')); ?>
