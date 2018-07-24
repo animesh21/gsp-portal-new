74d1387766a2f044115864bbbd6d930cd2b516ac
Sidd024 <siddhartha2488@gmail.com>
2018-07-24 06:19:22 -0700

    Change Code
    Change Code

--- 18/application/views/food.php
+++ 18/application/views/food.php
@@ -2444,7 +2444,7 @@ e.preventDefault();
 /**Q6(a) Validation**/	
 $("#foodnext").click(function(e){
  if($("input[name='Q9F1']").val()==='Y'){
-  if($("input[name='Q9F2']").val()==""){
+  if($("input[name='Q9F2']").val()=="" && $("input[name='Q9F2']").attr("placeholder")==""){
 	 alert("Q6(a) Please Provide Details.."); 
 	 e.preventDefault();
    }
