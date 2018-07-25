3fff84514e7743bfb089df48954f72f8c6c895ea
rohit12371 <31234754+rohit12371@users.noreply.github.com>
2018-07-25 13:41:50 +0530

    add tab all
    add tab all

--- 18/application/views/admin/dashboard/year18_phase1.php
+++ 18/application/views/admin/dashboard/year18_phase1.php
@@ -1,9 +1,44 @@
 <h1>Dashboard</h1>
-
 <div class="schools-statics "> <a href="<?php base_url()?>total18_registration" target="_blank">
-        <div class="statics-circle"> <span><?php echo count($school); ?></span>
-            <label>view</label>
-        </div>
-    </a>
-    <div class="title">Total School Registration</div>
+  <div class="statics-circle"> <span><?php echo count($school); ?></span>
+    <label>view</label>
+  </div>
+  </a>
+  <div class="title">Total School Registration</div>
 </div>
+<div class="schools-statics"> <a href="<?php base_url()?>total18_startedtheaudit">
+  <div class="statics-circle"> <span> <?php echo count($startedtheaudit); ?> </span>
+    <label>view</label>
+  </div>
+  </a>
+  <div class="title">Schools That Started The Audit</div>
+</div>
+<div class="schools-statics"> <a href="<?php base_url()?>total18_CompletedAuditButNotSubmitted">
+  <div class="statics-circle"> <span><?php echo count($CompletedAuditButNotSubmitted);?></span>
+    <label>view</label>
+  </div>
+  </a>
+  <div class="title">Schools Completed The Audit But Not Submitted</div>
+</div>
+<div class="schools-statics"> <a href="<?php base_url()?>total18_SubmittedTheAudit">
+  <div class="statics-circle"> <span><?php echo count($SubmittedTheAudit);?></span>
+    <label>view</label>
+  </div>
+  </a>
+  <div class="title">Schools Submitted The Audit</div>
+</div>
+<div class="schools-statics"><a href="<?php base_url()?>total18_StartedAuditButDidNotComplete">
+  <div class="statics-circle"> <span><?php echo count($StartedAuditButDidNotComplete);?></span>
+    <label>view</label>
+  </div>
+  </a>
+  <div class="title">Schools Started The Audit But Did Not Complete</div>
+</div>
+<div class="schools-statics"> <a href="<?php base_url()?>total18_NotStartTheAudit">
+  <div class="statics-circle"> <span><?php echo count($NotStartTheAudit);?></span>
+    <label>view</label>
+  </div>
+  </a>
+  <div class="title">Schools Did Not Start The Audit</div>
+</div>
+
