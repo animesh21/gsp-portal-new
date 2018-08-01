6b26c61a40ea8740da2d08008f8a633f4fa93111
rohit12371 <31234754+rohit12371@users.noreply.github.com>
2018-08-01 17:44:17 +0530

    change code for graph in state
    change code for graph in state

--- 18/application/views/admin/audit/search.php
+++ 18/application/views/admin/audit/search.php
@@ -26,6 +26,17 @@
     </span>
     <?php echo form_close(); ?>
 </div>
+<div id="container"> </div>
+   <p><strong>Export Graph:</strong></p>
+  <select id="ExportOption" style="border-radius:0px;">
+    <option value="PNG">PNG Image</option>
+    <option value="JPEG">JPEG Image</option>
+    <option value="PDF">PDF Document</option>
+    <option value="SVG">SVG Vector Image</option>
+  </select>
+  <button id="buttonExport" class="btn btn-danger" style="background: #e86549 !important; border:1px solid #e86549; border-radius:0px;">Export chart</button>
+  <button id="buttonPrint" class="btn btn-danger" style="background: #e86549 !important; border:1px solid #e86549; border-radius:0px;">Print chart</button>
+
 <table class="display dataTable no-footer tablepluging" cellspacing="0" width="100%" role="grid" aria-describedby="example_info" style="width: 100%;">
     <thead>
         <tr role="row">
@@ -72,3 +83,88 @@
 <div class="clearfix">&nbsp;</div>
 <div class="clearfix">&nbsp;</div>
 <div class="clearfix">&nbsp;</div>
+
+
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/data.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script type="text/javascript">
+  var chart= Highcharts.chart('container',{
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Schools Participation Report'
+        },
+        xAxis: {
+            categories: [
+			'<?php echo getStateById($state_id1); ?>'
+			],
+            crosshair: true
+        },
+		
+		legend: {
+        enabled: false
+    },
+        exporting: { enabled: false },
+        credits: {enabled: false},
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0;font-size:12px;">{series.name}: </td>' +
+                '<td style="padding:0;font-size:12px;">{point.y:.1f}</td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+		
+		 series: {
+            borderWidth: 0,
+            dataLabels: {
+                enabled: true,
+                format: '{point.y:.1f}'
+            }
+        },
+		
+		
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Registered for Audit',
+            data: [<?php echo count($record); ?>] //NORTH
+            //Registration, Audit Started, Audit Completed, Feedback Recieved
+        }
+		],
+});
+// the button handler    
+    $('#buttonExport').click(function() {
+        var e = document.getElementById("ExportOption");
+        var ExportAs = e.options[e.selectedIndex].value;   
+        
+        if(ExportAs == 'PNG')
+        {
+            chart.exportChart({type: 'image/png', filename: 'my-png'}, {subtitle: {text:''}});
+        }
+        if(ExportAs == 'JPEG')
+        {
+            chart.exportChart({type: 'image/jpeg', filename: 'my-jpg'}, {subtitle: {text:''}});
+        }
+        if(ExportAs == 'PDF')
+        {
+            chart.exportChart({type: 'application/pdf', filename: 'my-pdf'}, {subtitle: {text:''}});
+        }
+        if(ExportAs == 'SVG')
+        {
+            chart.exportChart({type: 'image/svg+xml', filename: 'my-svg'}, {subtitle: {text:''}});
+        }
+    }); 
+	
+    $('#buttonPrint').click(function() {
+        chart.setTitle(null, { text: ' ' });
+        chart.print();
+        chart.setTitle(null, { text: 'Click and drag in the plot area to zoom in' });
+    });
+</script>
