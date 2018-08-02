699b442761ec5738995bf46be1b25b1b5f56397c
rohit12371 <31234754+rohit12371@users.noreply.github.com>
2018-08-02 16:27:42 +0530

    remove the 2017
    remove the 2017

--- 18/application/views/admin/dashboard/index.php
+++ 18/application/views/admin/dashboard/index.php
@@ -7,12 +7,4 @@
     </a>
     <div class="title">2018</div>
 </div>
-<div class="schools-statics"> <a href="#">
-        <div class="statics-circle"> <span>
-		2
-		</span>
-            <label>Phase</label>
-        </div>
-    </a>
-    <div class="title">2017</div>
-</div>
+
