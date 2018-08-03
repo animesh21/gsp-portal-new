610a57ad7270e85685f98e6ba33adbfd2f1f1afe
Sidd024 <siddhartha2488@gmail.com>
2018-08-03 18:29:06 +0530

    Change Code
    Change Code

--- 18/application/views/air.php
+++ 18/application/views/air.php
@@ -2165,7 +2165,7 @@ $("#airnext").click(function(e){
 /*Student (maximum of five staff can be part of the team): */
 $("#airnext").click(function(e){
    if($("#Q3A1S1").attr('placeholder')==""  || $("#Q3A1S2").attr('placeholder')==""  || $("#Q3A1S3").attr('placeholder')==""){ 
-    if($("#Q3A1S1").val()==""  || $("#Q3E1S2").val()==""  || $("#Q3E1S3").val()==""){ 
+    if($("#Q3A1S1").val()==""  || $("#Q3A1S2").val()==""  || $("#Q3A1S3").val()==""){ 
     alert("School Must Add Administrative Staff  First Name, Last Name & Grade.");
 	e.preventDefault();
 	}
