37c8728aad6681b4f3a8d03a03258e47932795c0
Sidd024 <siddhartha2488@gmail.com>
2018-08-13 12:01:44 +0530

    Change Code
    Change Code

--- 18/application/views/food.php
+++ 18/application/views/food.php
@@ -2436,7 +2436,7 @@ e.preventDefault();
 $("#foodnext").click(function(e){
 if($("#Q3F1S1").attr('placeholder')==""  || $("#Q3F1S2").attr('placeholder')==""  || $("#Q3F1S3").attr('placeholder')==""){ 
 if($("#Q3F1S1").val()==""  || $("#Q3F1S2").val()==""  || $("#Q3F1S3").val()==""){ 
-alert("School Must Add Administrative Staff  First Name, Last Name & Grade.");
+alert("School Must Add Student  First Name, Last Name & Grade.");
 e.preventDefault();
 }
 }
