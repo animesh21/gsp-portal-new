f68518954a2729d63d9ba267ddfe4a236dc07254
Sidd024 <siddhartha2488@gmail.com>
2018-08-16 16:28:16 +0530

    Change Code
    Change Code

--- 18/application/models/admin/Report.php
+++ 18/application/models/admin/Report.php
@@ -18,8 +18,10 @@ class Report extends CI_Model {
 	  {
 	   $this->db->where("state",$regions);
 	   $this->db->where("complete_status",'0');
-	   $this->db->where("progress <=",'100');
+	   //Old Code//$this->db->where("progress <=",'100');
 	   //$this->db->where("date_added <=",'2017-11-20'); 
+	   $this->db->where('progress>=', 10);
+	   $this->db->where('progress<=', 100);	  
 	   $startAudit=$this->db->count_all_results("gsp_school");
 	   $startAuditSum=$startAuditSum + $startAudit;
 	  }
