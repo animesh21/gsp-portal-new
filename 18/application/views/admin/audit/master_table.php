a438034bc9147c303624bec6e5537e6172364c9d
Sidd024 <siddhartha2488@gmail.com>
2018-08-21 18:33:35 +0530

    Change Code
    Change Code

--- 18/application/views/admin/audit/master_table.php
+++ 18/application/views/admin/audit/master_table.php
@@ -7,32 +7,27 @@
       <tr>
         <th>S.No</th>
         <th>School Id</th>
-        <th>Name of the Schools submitting GSP Audit</th>
+        <th>Name of the Schools submitting GSP Audit in 2017</th>
         <th>State</th>
         <th>District</th>
-		<?php $unique_year=getUniqueyear();  ?>
-		<?php for($i=0;$i<sizeof($unique_year);$i++){ ?>     
-        <th><?php echo $unique_year[$i]->year_ref; ?></th>
-		<?php } ?>
+        <th>2017 Submission</th>
+        <th>2016 Submission</th>
+        <th>2015 submission</th>
       </tr>
     </thead>
     <tbody>
-	 <?php 
-	 error_reporting(0);
-	        ini_set('memory_limit', '-1');
+	<?php 
 	 $sr_no=0;
-	 $r=0;
-	 for($i=0;$i<sizeof($record);++$i){ $r=0; ?>
+	 for($i=0;$i<sizeof($record);++$i){ ?>
       <tr>
         <td><?php echo ++$sr_no; ?></td>
         <td><?php echo $record[$i]->school_id; ?></td>
-        <td><?php $school_name=getSchoolDaTaByScHooLId($record[$i]->school_id);  echo $school_name[$r]->name; ?></td>
-        <td><?php $school_name=getSchoolDaTaByScHooLId($record[$i]->school_id);  echo $school_name[$r]->state_name; ?></td>
-        <td><?php $school_name=getSchoolDaTaByScHooLId($record[$i]->school_id);  echo $school_name[$r]->district_name; ?></td>
-       <?php $unique_year=getUniqueyear();  ?>
-		<?php for($i=0;$i<sizeof($unique_year);$i++){ ?>     
-        <td><?php echo getSchoolPointYOY($record[$i]->school_id,$unique_year[$i]->year_ref); ?></td>
-		<?php } ?>
+        <td><?php echo $record[$i]->school_name; ?></td>
+        <td><?php echo $record[$i]->state; ?></td>
+        <td><?php echo $record[$i]->district; ?></td>
+        <td><?php echo $record[$i]->data_2017; ?></td>
+        <td><?php echo $record[$i]->data_2016; ?></td>
+        <td><?php echo $record[$i]->data_2015; ?></td>
       </tr>
 	  <?php } ?>
     </tbody>
