13b4d09eb16cce7cee7ed9e8836d79bb1dd86aa3
Sidd024 <siddhartha2488@gmail.com>
2018-08-21 17:29:20 +0530

    Change Code
    Change Code

--- 18/application/views/admin/audit/master_table.php
+++ 18/application/views/admin/audit/master_table.php
@@ -7,69 +7,34 @@
       <tr>
         <th>S.No</th>
         <th>School Id</th>
-        <th>Name of the Schools submitting GSP Audit in 2017</th>
+        <th>Name of the Schools submitting GSP Audit</th>
         <th>State</th>
         <th>District</th>
-        <th>City</th>
-        <th>2017 Submission</th>
-        <th>2016 Submission</th>
-        <th>2015 submission</th>
+		<?php $unique_year=getUniqueyear();  ?>
+		<?php for($i=0;$i<sizeof($unique_year);$i++){ ?>     
+        <th><?php echo $unique_year[$i]->year_ref; ?></th>
+		<?php } ?>
       </tr>
     </thead>
     <tbody>
-     
+	 <?php 
+	 error_reporting(0);
+	        ini_set('memory_limit', '-1');
+	 $sr_no=0;
+	 $r=0;
+	 for($i=0;$i<sizeof($record);++$i){ $r=0; ?>
       <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-       <!-- <?php 
-		      /* if($r->submission_2017>=70):
-		   echo "<td style='background-color: green; color:#000;'>".$r->submission_2017."</td>";
-		  elseif($r->submission_2017>=50 && $percentage<=69.9):
-		  		   echo "<td style='background:yellow; color:000;'>".$r->submission_2017."</td>";
-		  elseif($r->submission_2017>=35 && $percentage<=49.9):
-		  		   echo "<td style='background:orange; color:000;'>".$r->submission_2017."</td>";
-		  elseif($r->submission_2017<=34.9):
-		  		   echo "<td style='background-color: red; color:#000;'>".$r->submission_2017."</td>";
-		  else:
-		    echo "<td>Not Rated</td>";
-		  endif;  */
-		  ?> -->
-       <!--  <?php 
-		  /* if($r->submission_2016_rank==1):
-		   echo "<td style='background-color: green; color:#000;'>".$r->submission_2016."</td>";
-		  elseif($r->submission_2016_rank==2):
-		  		   echo "<td style='background:yellow; color:000;'>".$r->submission_2016."</td>";
-		  elseif($r->submission_2016_rank==3):
-		  		   echo "<td style='background:orange; color:000;'>".$r->submission_2016."</td>";
-		  elseif($r->submission_2016_rank==4):
-		  		   echo "<td style='background-color: red; color:#000;'>".$r->submission_2016."</td>";
-		  else:
-		    echo "<td>NA</td>";
-		  endif; */
-		?> -->
-       <!--  <?php 
-		  /* if($r->submission_2015_rank==1):
-		   echo "<td style='background-color: green; color:#000;'>".$r->submission_2015."</td>";
-		  elseif($r->submission_2015_rank==2):
-		  		   echo "<td style='background:yellow; color:000;'>".$r->submission_2015."</td>";
-		  elseif($r->submission_2015_rank==3):
-		  		   echo "<td style='background:orange; color:000;'>".$r->submission_2015."</td>";
-		  elseif($r->submission_2015_rank==4):
-		  		   echo "<td style='background-color: red; color:#000;'>".$r->submission_2015."</td>";
-		  else:
-		    echo "<td>NA</td>";
-		  endif; */
-		
-		?> -->
+        <td><?php echo ++$sr_no; ?></td>
+        <td><?php echo $record[$i]->school_id; ?></td>
+        <td><?php $school_name=getSchoolDaTaByScHooLId($record[$i]->school_id);  echo $school_name[$r]->name; ?></td>
+        <td><?php $school_name=getSchoolDaTaByScHooLId($record[$i]->school_id);  echo $school_name[$r]->state_name; ?></td>
+        <td><?php $school_name=getSchoolDaTaByScHooLId($record[$i]->school_id);  echo $school_name[$r]->district_name; ?></td>
+       <?php $unique_year=getUniqueyear();  ?>
+		<?php for($i=0;$i<sizeof($unique_year);$i++){ ?>     
+        <td><?php echo getSchoolPointYOY($record[$i]->school_id,$unique_year[$i]->year_ref); ?></td>
+		<?php } ?>
       </tr>
-     
+	  <?php } ?>
     </tbody>
   </table>
 </div>
