20c66f541fa0f47418e97ffcd2cc6faf2c24a80b
Sidd024 <siddhartha2488@gmail.com>
2018-08-21 19:03:48 +0530

    Code Change
    Code Change

--- 18/application/views/admin/audit/master_table.php
+++ 18/application/views/admin/audit/master_table.php
@@ -16,7 +16,7 @@
       </tr>
     </thead>
     <tbody>
-	<?php 
+      <?php 
 	 $sr_no=0;
 	 for($i=0;$i<sizeof($record);++$i){ ?>
       <tr>
@@ -25,11 +25,20 @@
         <td><?php echo $record[$i]->school_name; ?></td>
         <td><?php echo $record[$i]->state; ?></td>
         <td><?php echo $record[$i]->district; ?></td>
-        <td><?php echo $record[$i]->data_2017; ?></td>
-        <td><?php echo $record[$i]->data_2016; ?></td>
-        <td><?php echo $record[$i]->data_2015; ?></td>
+        <td <?php 
+		     if($record[$i]->data_2017>=70){?>style='background-color: Green; color:#000;' <?php }elseif($record[$i]->data_2017>=50 && $record[$i]->data_2017<=69.9){ ?>style='background:yellow; color:000;' <?php }elseif($record[$i]->data_2017>=35 && $record[$i]->data_2017<=49.9){ ?> style='background:orange; color:000;'  <?php }elseif($record[$i]->data_2017<=34.9 && $record[$i]->data_2017!="NA"){?> style='background:red; color:000;' <?php } ?> >
+        <?php echo $record[$i]->data_2017; ?>
+        </td>
+        <td <?php 
+		     if($record[$i]->data_2016>=70){?>style='background-color: Green; color:#000;' <?php }elseif($record[$i]->data_2016>=50 && $record[$i]->data_2016<=69.9){ ?>style='background:yellow; color:000;' <?php }elseif($record[$i]->data_2016>=35 && $record[$i]->data_2016<=49.9){ ?> style='background:orange; color:000;'  <?php }elseif($record[$i]->data_2016<=34.9 && $record[$i]->data_2016!="NA"){?> style='background:red; color:000;' <?php } ?> >
+        <?php echo $record[$i]->data_2016; ?>
+        </td>
+        <td <?php 
+		     if($record[$i]->data_2015>=70){?>style='background-color: Green; color:#000;' <?php }elseif($record[$i]->data_2015>=50 && $record[$i]->data_2015<=69.9){ ?>style='background:yellow; color:000;' <?php }elseif($record[$i]->data_2015>=35 && $record[$i]->data_2015<=49.9){ ?> style='background:orange; color:000;'  <?php }elseif($record[$i]->data_2015<=34.9 && $record[$i]->data_2015!="NA"){?> style='background:red; color:000;' <?php } ?>>
+        <?php echo $record[$i]->data_2015; ?>
+        </td>
       </tr>
-	  <?php } ?>
+      <?php } ?>
     </tbody>
   </table>
 </div>
