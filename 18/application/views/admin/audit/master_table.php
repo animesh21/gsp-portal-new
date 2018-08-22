bcd403aceaef4571c6fcc900984803b0a44bf611
Sidd024 <siddhartha2488@gmail.com>
2018-08-22 17:30:25 +0530

    Change Code
    Change Code

--- 18/application/views/admin/audit/master_table.php
+++ 18/application/views/admin/audit/master_table.php
@@ -27,7 +27,7 @@
         <td><?php echo $record[$i]->state; ?></td>
         <td><?php echo $record[$i]->district; ?></td>
 	<td <?php 
-		     if($record[$i]->data_2018>=70){?>style='background-color: Green; color:#000;' <?php }elseif($record[$i]->data_2018>=50 && $record[$i]->data_2018<=69.9){ ?>style='background:yellow; color:000;' <?php }elseif($record[$i]->data_2018>=35 && $record[$i]->data_2018<=49.9){ ?> style='background:orange; color:000;'  <?php }elseif($record[$i]->data_2018<=34.9 && $record[$i]->data_2018!="NA" & $record[$i]->data_2018!=" "){?> style='background:red; color:000;' <?php } ?> >
+		     if($record[$i]->data_2018>=70){?>style='background-color: Green; color:#000;' <?php }elseif($record[$i]->data_2018>=50 && $record[$i]->data_2018<=69.9){ ?>style='background:yellow; color:000;' <?php }elseif($record[$i]->data_2018>=35 && $record[$i]->data_2018<=49.9){ ?> style='background:orange; color:000;'  <?php }elseif($record[$i]->data_2018<=34.9 && $record[$i]->data_2018!="NA" & $record[$i]->data_2018!=""){?> style='background:red; color:000;' <?php } ?> >
         <?php if(!empty($record[$i]->data_2018)){echo $record[$i]->data_2018;}else{echo "NA";} ?>
         </td>    
         <td <?php 
