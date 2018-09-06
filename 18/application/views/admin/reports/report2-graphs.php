f3567cdf512e886e9d92477227487c87066bab1b
Sidd024 <siddhartha2488@gmail.com>
2018-09-06 11:47:46 +0530

    Change Code
    Change Code

--- 18/application/views/admin/reports/report2-graphs.php
+++ 18/application/views/admin/reports/report2-graphs.php
@@ -10,6 +10,13 @@
 <div class="wrapper">
   <div id="container"> </div>
   <div id="containerDistrictWisePartner1"> </div>
+  <?php 
+    $statewisegraph1=getDataGraphByDistrict($state_id1);
+    $countArr1=count(array_chunk($statewisegraph1[0],7,true));
+    for($i=0;$i<$countArr1; $i++){
+    ?>
+	<div id="containerStateWisePartner1_<?php echo $i; ?>"> </div><br/>
+    <?php } ?>
 </div>
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/modules/data.js"></script>
@@ -43,13 +50,28 @@
             },
             series: [{
                 colorByPoint: true,
-                data: [{name: 'Registered for Audit', y: <?php echo $registerd_audit_1; ?>}, {name: 'Audit Not Started', y: <?php echo $total_notstarted_audit_1; ?>},{name: 'Audit Started', y: <?php echo $total_started_audit_1; ?>}, {name: 'Audit Completed', y: <?php echo $completed_1; ?>}]
+                data: [{name: 'Registered for Audit',color:'rgb(124, 181, 236)', y: <?php echo $registerd_audit_1; ?>}, {name: 'Audit Not Started' ,color:'#fc3300', y: <?php echo $total_notstarted_audit_1; ?>},{name: 'Audit Started' ,color:'#fcfc00', y: <?php echo $total_started_audit_1; ?>}, {name: 'Audit Completed' ,color:'#00ae4f', y: <?php echo $completed_1; ?>}]
             }]
         });
 		</script>
-<?php $districtwisegraph1=getDataGraphByDistrict($state_id1); ?>
+<?php 
+ $districtwisegraph1=getDataGraphByDistrict($state_id1);
+ $countArr1=count(array_chunk($districtwisegraph1[0],7,true));
+ $countTotalArr1=count($districtwisegraph1[0]);
+ $arrData1=array_chunk($districtwisegraph1[0],7,true);
+ $arrData1_1=array_chunk($districtwisegraph1[1],7,true);
+ $arrData1_2=array_chunk($districtwisegraph1[2],7,true);
+ $arrData1_3=array_chunk($districtwisegraph1[3],7,true);
+ $arrData1_4=array_chunk($districtwisegraph1[4],7,true);
+ $dataCount1=array();
+ $countInteration1=0;
+ $y=0;
+ $s=0;
+ for($i=0,$t=0;$i<$countArr1; $i++){$dataCount1[]=$i;}
+ for($i=0,$t=0;$i<$countArr1; $i++){
+?>
 <script type="text/javascript">
-        Highcharts.chart('containerDistrictWisePartner1',{
+       Highcharts.chart('containerStateWisePartner1_<?php echo $i; ?>',{
         chart: {
             type: 'column'
         },
@@ -63,9 +85,11 @@
 		},
         xAxis: {
             categories: [
-             <?php foreach($districtwisegraph1[0] as $districtWG){ ?>
-				<?php echo "'".$districtWG['districtame']."'"; ?>,
-			<?php } ?>
+			<?php 
+             for($r=0;$r<7;++$r){
+				  echo "'".$arrData1[$i][$y]['districtame']."'"; ?>,
+             <?php $y++; 
+			 }?>  
             ],
             crosshair: true
         },
@@ -100,24 +124,100 @@
         series: [{
             name: 'Registered for Audit',
 			color:'rgb(124, 181, 236)',
-            data: <?php echo json_encode(array_map('intval',$districtwisegraph1[1])); ?> //NORTH
+            data: <?php echo json_encode(array_map('intval',array_values($arrData1_1[$t]))); ?> //NORTH
             //Registration, Audit Started, Audit Completed, Feedback Recieved
         }, {
             name: 'Audit Not Started',
 			color:'#fc3300',
-            data: <?php echo json_encode(array_map('intval',$districtwisegraph1[2])); ?> //South
+            data: <?php echo json_encode(array_map('intval',array_values($arrData1_2[$t]))); ?> //South
 
         }, {
             name: 'Audit Started',
 			color:'#fcfc00',
-            data: <?php echo json_encode(array_map('intval',$districtwisegraph1[3])); ?> //South
+            data: <?php echo json_encode(array_map('intval',array_values($arrData1_3[$t]))); ?> //South
 
         }, {
             name: 'Audit Completed',
 			color:'#00ae4f',
-            data: <?php echo json_encode(array_map('intval',$districtwisegraph1[4])); ?> //East
+            data: <?php echo json_encode(array_map('intval',array_values($arrData1_4[$t]))); ?> //East
 
         }		
 		],
 }); 
+</script><?php
+  $t++;
+} ?>
+
+<script type="text/javascript">
+        /*Highcharts.chart('containerDistrictWisePartner1',{
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Participation Chart Of <?php //echo getStateById($state_id1); ?> By District'
+        },
+		yAxis: {
+		  title: {
+            text: 'No. of Schools'
+          }
+		},
+        xAxis: {
+            categories: [
+             <?php //foreach($districtwisegraph1[0] as $districtWG){ ?>
+				<?php //echo "'".$districtWG['districtame']."'"; ?>,
+			<?php //} ?>
+            ],
+            crosshair: true
+        },
+		
+		legend: {
+        enabled: true
+    },
+        exporting: { enabled: true },
+        credits: {enabled: false},
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0;font-size:12px;">{series.name}: </td>' +
+                '<td style="padding:0;font-size:12px;">{point.y:.1f}</td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+		 series: {
+            borderWidth: 0,
+            dataLabels: {
+                enabled: true,
+                format: '{point.y:.1f}'
+            }
+        },
+		
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Registered for Audit',
+			color:'rgb(124, 181, 236)',
+            data: <?php //echo json_encode(array_map('intval',$districtwisegraph1[1])); ?> //NORTH
+            //Registration, Audit Started, Audit Completed, Feedback Recieved
+        }, {
+            name: 'Audit Not Started',
+			color:'#fc3300',
+            data: <?php //echo json_encode(array_map('intval',$districtwisegraph1[2])); ?> //South
+
+        }, {
+            name: 'Audit Started',
+			color:'#fcfc00',
+            data: <?php //echo json_encode(array_map('intval',$districtwisegraph1[3])); ?> //South
+
+        }, {
+            name: 'Audit Completed',
+			color:'#00ae4f',
+            data: <?php //echo json_encode(array_map('intval',$districtwisegraph1[4])); ?> //East
+
+        }		
+		],
+}); */
 </script>
