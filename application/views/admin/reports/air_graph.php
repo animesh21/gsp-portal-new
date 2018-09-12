8b5eeb158b447eb8f62dabd0cc8b2d5de3b4bdbb
Sidd024 <siddhartha2488@gmail.com>
2018-09-12 12:19:37 +0530

    Divide Large Graphs Into Small Graphs
    Divide Large Graphs Into Small Graphs

--- application/views/admin/reports/air_graph.php
+++ application/views/admin/reports/air_graph.php
@@ -1,36 +1,35 @@
 <?php
-//echo '<pre>'; print_r($Bus);die();
 $petrol = $Bus['petrol'][0]->total;
 $diesel = $Bus['diesel'][0]->total;
 $cng = $Bus['cng'][0]->total;
 $electric = $Bus['electric'][0]->total;
 
-$final_array_poss = array($NotOwnVehicles_count,'');
+$final_array_poss = array($NotOwnVehicles_count);
 $final_NotOwnVehicles_count = json_encode($final_array_poss);
 
-$final_array_poss1 = array($Operatorowned_count,'');
+$final_array_poss1 = array($Operatorowned_count);
 $final_Operatorowned_count = json_encode($final_array_poss1);
 
 
-$final_array_poss2 = array($OwnVehicles_count,'');
+$final_array_poss2 = array($OwnVehicles_count);
 $final_OwnVehicles_count = json_encode($final_array_poss2);
 
-$final_array_poss3 = array($CombinationSchoolandOperatorvehicles,'');
+$final_array_poss3 = array($CombinationSchoolandOperatorvehicles);
 $final_CombinationSchoolandOperatorvehicles = json_encode($final_array_poss3);
 
-$final_array_poss4 = array($Hiredvehicles,'');
+$final_array_poss4 = array($Hiredvehicles);
 $final_Hiredvehicles = json_encode($final_array_poss4);
 
-$final_array_poss5 = array('',(int)$petrol);
+$final_array_poss5 = array((int)$petrol);
 $final_pet = json_encode($final_array_poss5);
 
-$final_array_poss6 = array('',(int)$diesel);
+$final_array_poss6 = array((int)$diesel);
 $final_die = json_encode($final_array_poss6); 
 
-$final_array_poss7 = array('',(int)$cng);
+$final_array_poss7 = array((int)$cng);
 $final_cng = json_encode($final_array_poss7);
 
-$final_array_poss8 = array('',(int)$electric);
+$final_array_poss8 = array((int)$electric);
 $final_electric = json_encode($final_array_poss8);
 
 //echo '<pre>'; 
@@ -58,63 +57,57 @@ $others = (round($np['Q7A11S4'][0]->avg));
 $air_no = $quality['Q9A1'][0]->total;
 $air_yes = $quality['Q9A1'][1]->total;
 // Final Arrays
-$final_array_poss6 = array('',$personal_four,'','');
+$final_array_poss6 = array($personal_four);
 $final_four = json_encode($final_array_poss6);
 
-$final_array_poss7 = array('',$personal_three,'','');
+$final_array_poss7 = array($personal_three);
 $final_three = json_encode($final_array_poss7);
 
-$final_array_poss = array($school_bus,'','','');
+$final_array_poss = array($school_bus);
 $final_school_bus = json_encode($final_array_poss);
 
-$final_array_poss1 = array($public_bus,'','','');
+$final_array_poss1 = array($public_bus);
 $final_public_bus = json_encode($final_array_poss1);
 
-$final_array_poss2 = array($school_transport,'','','');
+$final_array_poss2 = array($school_transport);
 $final_school_transport = json_encode($final_array_poss2);
 
-$final_array_poss3 = array($school_public_transport,'','','');
+$final_array_poss3 = array($school_public_transport);
 $final_school_public_transport = json_encode($final_array_poss3);
 
-$final_array_poss4 = array($four_wheeler,'','','');
+$final_array_poss4 = array($four_wheeler);
 $final_four_wheeler = json_encode($final_array_poss4);
 
-$final_array_poss5 = array($three_wheeler,'','','');
+$final_array_poss5 = array($three_wheeler);
 $final_three_wheeler = json_encode($final_array_poss5);
 
-$final_array_poss8 = array('','',$bicycle,'');
+$final_array_poss8 = array($bicycle);
 $final_bicycle = json_encode($final_array_poss8);
 
-$final_array_poss9 = array('','',$on_foot,'');
+$final_array_poss9 = array($on_foot);
 $final_on_foot = json_encode($final_array_poss9);
 
-$final_array_poss10 = array('','',$others,'');
+$final_array_poss10 = array($others);
 $final_other = json_encode($final_array_poss10);
 
 
-$final_array_poss11 = array('','','',(int)$air_no);
+$final_array_poss11 = array((int)$air_no);
 $final_yes = json_encode($final_array_poss11);
 
-$final_array_poss12 = array('','','',(int)$air_yes);
+$final_array_poss12 = array((int)$air_yes);
 $final_no = json_encode($final_array_poss12);
 ?>
-
-
 <h1>PARTICIPATION BY AIR </h1>
   <div class="wrapper">
-    <div id="container"> </div>
-	<div id="container1"> </div>
+    <div id="container" class="col-md-12"> </div>
+	<div id="container1" class="col-md-12"> </div>
+	<div id="container2" class="col-md-12"> </div>
+	<div id="container3" class="col-md-12"> </div>
   </div>
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/modules/data.js"></script>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
-
-
-
-
-
 <script type="text/javascript">
-
    Highcharts.chart('container',{
         chart: {
             type: 'column'
@@ -128,8 +121,6 @@ $final_no = json_encode($final_array_poss12);
         xAxis: {
             categories: [
 				'Ownership of Vehicles',
-                'Bus'
-                
             ],
             crosshair: true
         },
@@ -147,37 +138,100 @@ $final_no = json_encode($final_array_poss12);
             shared: true,
             useHTML: true
         },
-        exporting: { enabled: false },
+        exporting: { enabled: true },
         credits: {enabled: false},
         plotOptions: {
+		
+		 series: {
+            borderWidth: 0,
+            dataLabels: {
+                enabled: true,
+                format: '{point.y:.1f}'
+            }
+        },
+		
+		
             column: {
                 pointPadding: 0.2,
                 borderWidth: 0
             }
         },
         series: [{
-           name: 'NotOwnVehicles_count',
+           name: 'NotOwnVehicles Count',
            data: <?php echo $final_NotOwnVehicles_count; ?>
 
        }, {
-           name: 'Operatorowned_count',
-           data: <?php echo $final_Operatorowned_count; ?>
+           name: 'Operatorowned Count',
+           data:<?php echo $final_Operatorowned_count; ?> 
 
        }, {
-           name: 'OwnVehicles_count',
-           data: <?php echo $final_OwnVehicles_count; ?>
+           name: 'OwnVehicles Count',
+           data:<?php echo $final_OwnVehicles_count; ?> 
+
 
        }, {
            name: 'CombinationSchoolandOperatorvehicles',
-           data: <?php echo $final_CombinationSchoolandOperatorvehicles; ?>
+           data:<?php echo $final_CombinationSchoolandOperatorvehicles; ?> 
 
        }, {
            name: 'Hiredvehicles',
-           data: <?php echo $final_Hiredvehicles; ?>
+           data:<?php echo $final_Hiredvehicles; ?>
 
-       }, {
+       }]
+   });
+</script>
+<script type="text/javascript">
+   Highcharts.chart('container1',{
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Air Report'
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            categories: [
+                'Bus'
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Numbers'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        exporting: { enabled: true },
+        credits: {enabled: false},
+        plotOptions: {
+		
+		 series: {
+            borderWidth: 0,
+            dataLabels: {
+                enabled: true,
+                format: '{point.y:.1f}'
+            }
+        },
+		
+		
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
            name: 'Bus:Petrol',
-           data: <?php echo $final_pet; ?>
+           data:<?php echo $final_pet; ?>
 
        }, {
            name: 'Bus:Diesel',
@@ -187,6 +241,7 @@ $final_no = json_encode($final_array_poss12);
            name: 'Bus:CNG',
            data: <?php echo $final_cng; ?>
 
+
        }, {
            name: 'Cars:Electric',
            data: <?php echo $final_electric; ?>
@@ -194,10 +249,8 @@ $final_no = json_encode($final_array_poss12);
        }]
    });
 </script>
-
-
 <script type="text/javascript">
- Highcharts.chart('container1',{
+ Highcharts.chart('container2',{
         chart: {
             type: 'column'
         },
@@ -209,10 +262,8 @@ $final_no = json_encode($final_array_poss12);
         },
         xAxis: {
             categories: [
-        'Sustainable Motorised',
+                'Sustainable Motorised',
                 'Private Vehicles',
-                'Non-Polluting Mode',
-                'Air Quality Monitor'
             ],
             crosshair: true
         },
@@ -234,6 +285,16 @@ $final_no = json_encode($final_array_poss12);
         exporting: { enabled: false },
         credits: {enabled: false},
         plotOptions: {
+		
+		 series: {
+            borderWidth: 0,
+            dataLabels: {
+                enabled: true,
+                format: '{point.y:.1f}'
+            }
+        },
+		
+		
             column: {
                 pointPadding: 0.2,
                 borderWidth: 0
@@ -257,7 +318,59 @@ $final_no = json_encode($final_array_poss12);
         }, {
             name: 'Public transport (other than bus)',
             data: <?php echo $final_school_public_transport; ?>
-        }, {
+        }]
+    });
+</script>
+<script type="text/javascript">
+ Highcharts.chart('container3',{
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Air Analytics Report'
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            categories: [
+                'Non-Polluting Mode',
+                'Air Quality Monitor',
+            ],
+            crosshair: true
+        },
+        
+        yAxis: { 
+            min: 0,
+            title: {
+                text: 'Avg Vehicles'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                         '<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        exporting: { enabled: true },
+        credits: {enabled: false},
+        plotOptions: {
+		
+		 series: {
+            borderWidth: 0,
+            dataLabels: {
+                enabled: true,
+                format: '{point.y:.1f}'
+            }
+        },
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
             name: 'Taxi four-wheeler',
             data: <?php echo $final_four_wheeler; ?>
         }, {
@@ -283,6 +396,3 @@ $final_no = json_encode($final_array_poss12);
         }]
     });
 </script>
-
-
-
