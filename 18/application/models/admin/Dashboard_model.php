03249ded767024e0aec017bc6c073f5976798c1d
Sidd024 <siddhartha2488@gmail.com>
2018-09-24 15:40:19 +0530

    Change Code
    Change Code

--- 18/application/models/admin/Dashboard_model.php
+++ 18/application/models/admin/Dashboard_model.php
@@ -377,7 +377,8 @@ class Dashboard_model extends CI_Model {
       }
 	  
 	   public function getstartedtheaudit_18data(){
-	$this->db->where("a.make_school_disabled","1");	   
+	$this->db->where("a.make_school_disabled","1");
+	$this->db->where("a.complete_status",'0');
         return $this->db->select('a.*, b.name AS state_name,c.name As district_name')
         ->from('gsp_school AS a')
         ->join('states AS b', 'a.state=b.id', 'left')
