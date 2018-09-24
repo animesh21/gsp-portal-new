5304a64f6ed7c41b652ce4908d2a8ecac330875e
Sidd024 <siddhartha2488@gmail.com>
2018-09-24 13:09:27 +0530

    Change Code
    Change Code

--- 18/application/models/admin/Report.php
+++ 18/application/models/admin/Report.php
@@ -103,12 +103,14 @@ class Report extends CI_Model {
 	{
 	  if($region==0){ 
 	   $this->db->where("make_school_disabled","1");
+	   $this->db->where("complete_status","1");
 	   $this->db->where("progress =",'100');
 	   $data=$this->db->select("COUNT(id) AS countlabel")->from('gsp_school')->get()->row();
 	   return $data->countlabel;
 	  }else{
 	   $this->db->where("state",$region);
 	   $this->db->where("make_school_disabled","1");
+	   $this->db->where("complete_status","1");	  
 	   $this->db->where("progress =",'100');
 	   $data=$this->db->select("COUNT(id) AS countlabel")->from('gsp_school')->get()->row();
 	   return $data->countlabel;
