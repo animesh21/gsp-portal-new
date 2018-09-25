f5b835053f9d07dda9a5a7b3215df0d3000341f7
yhamender <yhamender@gmail.com>
2018-09-25 16:55:39 -0700

    folder name change

--- np/2015/website_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-     	define("DB_Hostname", "localhost");
-		define("DB_Username", "GSP_2015");
-		define("DB_Password", "Studio@123");
-		define("DB_databasename", "GSP_2015");
-	
-	$link = mysql_connect(DB_Hostname,DB_Username,DB_Password) or die('Error connecting to mysql');
-	$mysql_db = mysql_select_db(DB_databasename, $link);
-?>
