d7cf189070d8be671a3d6674d48dd0cb99399fe5
Sidd024 <siddhartha2488@gmail.com>
2018-09-25 12:04:47 +0530

    Change Code
    Change Code

--- 18/application/models/admin/Audit_started_model.php
+++ 18/application/models/admin/Audit_started_model.php
@@ -2040,6 +2040,7 @@ public function getExcel2017Data() {
 	public function getExcelStartedTheAudit() {
         $output = "";
 	$this->db->where("a.make_school_disabled","1");	
+	$this->db->where("a.complete_status","0");	
         $arrRecord = $this->db->select('a.*, b.name AS state_name, c.name AS district_name , d.password')
                         ->from('gsp_school AS a')
                         ->join('states AS b', 'a.state=b.id', 'left')
