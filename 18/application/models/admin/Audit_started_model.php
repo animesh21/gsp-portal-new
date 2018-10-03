fa842f18f658b76c0501c86f1df1243d4118f301
Sidd024 <siddhartha2488@gmail.com>
2018-10-03 18:23:25 +0530

    Change Code
    Change Code

--- 18/application/models/admin/Audit_started_model.php
+++ 18/application/models/admin/Audit_started_model.php
@@ -1580,6 +1580,7 @@ class Audit_started_model extends CI_Model {
 
     public function getExcelDataByState($argID) {
         $output = "";
+	    $this->db->where("a.make_school_disabled=","1");
         $arrRecord = $this->db->select('a.*, b.name AS state_name, c.name AS district_name, d.password')
                         ->from('gsp_school AS a')
                         ->join('states AS b', 'a.state=b.id', 'left')
