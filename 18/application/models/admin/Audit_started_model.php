255dd1a3b1d9ea041b7511ec3eae06f333fa4772
Sidd024 <siddhartha2488@gmail.com>
2018-10-03 18:20:10 +0530

    Change Code
    Change Code

--- 18/application/models/admin/Audit_started_model.php
+++ 18/application/models/admin/Audit_started_model.php
@@ -1379,7 +1379,7 @@ class Audit_started_model extends CI_Model {
       
 	if($state==1 && $school==2)
 	{
-		
+		  $this->db->where("a.make_school_disabled=","1");
 		  $sql = $this->db->select('a.*, b.name AS state_name,c.name As district_name')
                         ->from('gsp_school AS a')
                         ->join('states AS b', 'a.state=b.id', 'left')
@@ -1390,7 +1390,8 @@ class Audit_started_model extends CI_Model {
            return $sql;	
 	}    
       elseif($state != 1 && $school != 2){
-        if($school==1){  
+        if($school==1){
+	 $this->db->where("a.make_school_disabled=","1");	
         $sql= $this->db->select('a.*,s.name as state_name,c.name As district_name')
                        ->from('gsp_school AS a')
                         ->join('gsp_answers as b', 'a.userid=b.userid', 'left')
@@ -1405,6 +1406,7 @@ class Audit_started_model extends CI_Model {
         return $sql;
             
         }elseif ($school==0) {
+		 $this->db->where("a.make_school_disabled=","1");
                 $sql= $this->db->select('a.*,s.name as state_name,c.name As district_name')
                         ->from('gsp_school AS a')
                         ->join('gsp_answers as b', 'a.userid=b.userid', 'left')
@@ -1421,6 +1423,7 @@ class Audit_started_model extends CI_Model {
         
       }elseif ($state == 1 && $school != 2) {
           if($school==1){  
+	 $this->db->where("a.make_school_disabled=","1");	  
         $sql= $this->db->select('a.*,s.name as state_name,c.name As district_name')
                         ->from('states AS s')
                         ->from('gsp_school AS a')
@@ -1435,6 +1438,7 @@ class Audit_started_model extends CI_Model {
             return $sql;
             
         }elseif ($school==0) {
+		 $this->db->where("a.make_school_disabled=","1");  
                 $sql= $this->db->select('a.*,s.name as state_name,c.name As district_name')
                         ->from('states AS s')
                         ->from('gsp_school AS a')
@@ -1451,6 +1455,7 @@ class Audit_started_model extends CI_Model {
             }
             
         }elseif ($state != 1 && $school == 2) {
+	    $this->db->where("a.make_school_disabled=","1");   
            $sql = $this->db->select('a.*, b.name AS state_name,c.name As district_name')
                         ->from('gsp_school AS a')
                         ->join('states AS b', 'a.state=b.id', 'left')
