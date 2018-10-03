fefe2bf158c79745eab9c4589467eca6e2d273ad
Sidd024 <siddhartha2488@gmail.com>
2018-10-03 12:34:46 +0530

    Change Code
    Change Code

--- 18/application/models/admin/Report.php
+++ 18/application/models/admin/Report.php
@@ -721,7 +721,7 @@ class Report extends CI_Model {
 		
 		public function completeparticipationBystateall()
 		{
-			$this->db->where("complete_status","1"); 
+			$this->db->where("complete_status=","1"); 
 			return $this->db->select('a.*')
 			        ->from('gsp_school As a')
 				   ->where("a.progress =",'100')
