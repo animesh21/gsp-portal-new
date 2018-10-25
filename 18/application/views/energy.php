220957c5f7276dd14782a36174d8c54f5f48d6c8
Sidd024 <siddhartha2488@gmail.com>
2018-10-25 17:35:12 +0530

    Change Code
    Change Code

--- 18/application/views/energy.php
+++ 18/application/views/energy.php
@@ -1711,5 +1711,6 @@ nyanBtn.addEventListener('click', function(){
                 height: 20px;
                 position: relative;
                 padding-top: 0px;
+		     display:none; 
 		 }	
 </style>
