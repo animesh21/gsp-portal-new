468d2c2a21b82104c8c20ae84ab2ed0fe6418487
Sidd024 <siddhartha2488@gmail.com>
2018-10-25 16:20:42 +0530

    Change Code
    Change Code

--- 18/application/views/login.php
+++ 18/application/views/login.php
@@ -73,6 +73,8 @@ header('Location: http://www.greenschoolsprogramme.org/audit/18/login/browser_ch
                     });
                 </script>
     <div class="home_container">
+      <div class="mform" style="display: block;">
+        <h1>Please Log In</h1>
 	<h1 style="font-size: 20px;
     font-family: fantasy;
     margin-left: 42px;
@@ -81,8 +83,6 @@ header('Location: http://www.greenschoolsprogramme.org/audit/18/login/browser_ch
     text-shadow: 1px 1px 1px #000;
     text-align: center;
     border: none;">Extended Submission Deadline November 16, 2018 </h1>
-      <div class="mform" style="display: block;">
-        <h1>Please Log In</h1>
         <p>The <a href="http://www.greenschoolsprogramme.org/" target="_blank">Green Schools Programme</a> audit is only open to registered schools. If you have already registered, please log in using your
           registered email id and password you would have received via email. </p>
         <p>If you would like to register, please visit our <a href="<?php echo base_url('register') ?>"
