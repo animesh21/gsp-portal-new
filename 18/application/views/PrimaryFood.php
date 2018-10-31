5c1228a45e0118121f168e706ffa52034f3bd8fb
Sidd024 <siddhartha2488@gmail.com>
2018-10-31 23:34:09 +0530

    Change Code
    Change Code

--- 18/application/views/PrimaryFood.php
+++ 18/application/views/PrimaryFood.php
@@ -42,7 +42,7 @@
     top: -28px;
     font-weight: bolder;"><i class="h6"><strong>If the new changes are not saved, please press Ctrl+F5</strong></i></small></h1>
             <div class="video"><a class="lptext" href="#" data-toggle="modal" data-target="#videoModal"><img
-                            src="http://greenschoolsprogramme.org/audit2015/upload/templates/tfr_responsive/images/video-icon.png"
+                            src="<?php echo base_url(); ?>assets/img/video-icon.png"
                             style="width: 45px; height: 40px; margin-top:47px;"></a></div>
         </div>
         <?php echo form_open('food/set', array('id' => 'PrimaryFood')); ?>
