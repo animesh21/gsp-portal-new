a540903505edd59101d2ef4a822a7d63dafb7764
Sidd024 <siddhartha2488@gmail.com>
2018-10-31 22:24:12 +0530

    Change Code
    Change Code

--- 18/application/views/admin/includes/footer.php
+++ 18/application/views/admin/includes/footer.php
@@ -10,7 +10,7 @@
 </footer>
 <!--close footer-->
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="<?php echo base_url() ?>assets/front/js/bootstrap.min.js"></script>
 
