b343c7b3cd18351035630e99708d165a03ca8137
Sidd024 <siddhartha2488@gmail.com>
2018-10-31 22:11:47 +0530

    Change Code
    Change Code

--- 18/application/views/admin/audit/disabled_school.php
+++ 18/application/views/admin/audit/disabled_school.php
@@ -138,8 +138,8 @@ function getBadgeCode(userid)
         });   
 });
 </script>
-<link type="text/css" rel="stylesheet" type="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link type="text/css" rel="stylesheet" type="<?php echo base_url(); ?>assets/css/bootstrap.min.css" />
+<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
 <script type="text/javascript">
 function sendResponseReport(schoolid,coemail,name){
  var subject="Response Report 2017";
