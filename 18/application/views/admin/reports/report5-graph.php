588eb18050cabe7b37522fbd97897d0eeed4255e
Sidd024 <siddhartha2488@gmail.com>
2018-10-31 21:44:49 +0530

    Change Code
    Change Code

--- 18/application/views/admin/reports/report5-graph.php
+++ 18/application/views/admin/reports/report5-graph.php
@@ -10,9 +10,9 @@
 <div class="wrapper">
   <div id="container"> </div>
 </div>
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/data.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/highcharts.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/data.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/exporting.js"></script>
 <script type="text/javascript">
     Highcharts.chart('container',{
         chart: {
