ec0af269051bd40a04d28b5bf133307fa34fbe04
Sidd024 <siddhartha2488@gmail.com>
2018-10-31 23:28:48 +0530

    Change Code
    Change Code

--- 18/application/views/energy.php
+++ 18/application/views/energy.php
@@ -33,7 +33,7 @@
     top: -28px;
     font-weight: bolder;"><i class="h6"><strong>If the new changes are not saved, please press Ctrl+F5</strong></i></small></h1>
   <div class="video"><a class="lptext" href="#" data-toggle="modal" data-target="#videoModal"><img
-                        src="http://greenschoolsprogramme.org/audit2015/upload/templates/tfr_responsive/images/video-icon.png"
+                        src="<?php echo base_url(); ?>assets/img/video-icon.png"
                         style="width: 45px; height: 40px; margin-top:47px;"></a></div>
   <p> Prior to answering these questions, please consider using a meter to measure the monthly average amount
     of energy used from the respective energy sources.</p>
