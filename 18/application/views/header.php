52549c7a2bcbb7792e1fe0208158873d763d060e
Sidd024 <siddhartha2488@gmail.com>
2018-11-01 16:18:16 +0530

    Change Code
    Change Code

--- 18/application/views/header.php
+++ 18/application/views/header.php
@@ -131,12 +131,7 @@ ga('send', 'pageview');
 </style>	
 <script type="text/javascript">
 $(document).ready(function(){
-$(".hide_one").append('<h1 class="text-center" style="color:#fff; margin:250px auto; position:relative; 
-		        background:#transparent!important;">Green Schools Programme Audit 2018<p style="font-size:13px;">
-		      GSP Audit 2018 Processing Next Section</p>
-		      <div class="loader" style="margin:0px auto; z-index: 1000000;
-		      background:#transparent!important; position:relative;"></div></h1>');
-setTimeout(function(){ $(".hide_one").css("display","none"); }, 10000);
+$(".hide_one").append('<h1 class="text-center" style="color:#fff; margin:250px auto; position:relative; background:#transparent!important;">Green Schools Programme Audit 2018<p style="font-size:13px;">GSP Audit 2018 Processing Next Section</p><div class="loader" style="margin:0px auto; z-index: 1000000; background:#transparent!important; position:relative;"></div></h1>'); setTimeout(function(){ $(".hide_one").css("display","none"); }, 10000);
 });
 </script>	
 </head>
