9b00146e05a6b526c189f0af0b13dd1f5b6b2cd5
Sidd024 <siddhartha2488@gmail.com>
2018-11-12 14:48:07 +0530

    Change Code
    Change Code

--- 18/application/views/PrimaryFood.php
+++ 18/application/views/PrimaryFood.php
@@ -2057,18 +2057,11 @@ You would require at least 5 to 10 of your schoolmates in the Food audit team. T
                         <ul class="list-inline">
                           <li>
                             <label class="radio-inline text-gray">
-                            <input type="radio"
-                                                                                                                                   name="Q9F2"
-                                                                                                                                   onclick="ComputePackaged(1)"
-                                                                                                                                   value="<?php echo set_value('Q9F2', 'Y') ?>" <?php if (isset($data['Q9F2'])){ echo $data['Q9F2'] == 'Y' ? "checked" : "";} ?>>
-                            Yes </label>
+                            <input type="radio" name="Q9F2" onclick="ComputePackaged(1)" value="<?php echo set_value('Q9F2', 'Y') ?>" <?php if (isset($data['Q9F2'])){ echo $data['Q9F2'] == 'Y' ? "checked" : "";} ?>> Yes </label>
                           </li>
                           <li>
                             <label class="radio-inline text-gray">
-                            <input type="radio"
-                                                                                                                                   name="Q9F2"
-                                                                                                                                   onclick="ComputePackaged(2)"
-                                                                                                                                   value="<?php echo set_value('Q9F2', 'N') ?>" <?php if (isset($data['Q9F2'])){ echo $data['Q9F2'] == 'N' ? "checked" : "";} ?>/>
+                            <input type="radio" name="Q9F2" onclick="ComputePackaged(2)" value="<?php echo set_value('Q9F2', 'N') ?>" <?php if (isset($data['Q9F2'])){ echo $data['Q9F2'] == 'N' ? "checked" : "";} ?>/>
                             No </label>
                           </li>
                         </ul>
