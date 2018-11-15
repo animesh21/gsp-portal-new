026a95b9865a8a1560a277cc1599b7efb576151c
rohit12371 <31234754+rohit12371@users.noreply.github.com>
2018-11-15 12:16:41 +0530

    add rangita mam email id
    add rangita mam email id

--- 18/application/models/User_model.php
+++ 18/application/models/User_model.php
@@ -220,7 +220,7 @@ class User_model extends CI_Model
                 $config['mailtype'] = 'html';
                 $this->email->initialize($config);
                 $from = "support@greenschoolsprogramme.org";
-                $to =array($query->email,"siddhartha2488@gmail.com","Studiotesseractst@gmail.com");
+                $to =array($query->email,"siddhartha2488@gmail.com","Studiotesseractst@gmail.com","ranjita@cseindia.org");
                 $subject = "GSP Forget Password";
                 $msg = "Dear &nbsp;";
                 $msg .= $query->username . "," . "<br><br>";
