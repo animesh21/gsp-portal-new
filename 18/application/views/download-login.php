925494538402aabcf030cf22d55158f01adf873c
Sidd024 <siddhartha2488@gmail.com>
2018-11-19 17:44:06 +0530

    Change Code
    Change Code

--- 18/application/views/download-login.php
+++ 18/application/views/download-login.php
@@ -147,7 +147,7 @@
      Modal content
     <div class="modal-content">
       <div class="modal-header"  style="background:#e86549; color:#FFFFFF;">
-      <button type="button" class="close" data-dismiss="modal">&times;</button>
+      <!--<button type="button" class="close" data-dismiss="modal">&times;</button>-->
         <h4 class="modal-title">GSP Audit 2017</h4>
       </div>
       <div class="modal-body">
