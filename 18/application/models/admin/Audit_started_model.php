1ab19f5c2241cce5be011a95a0ba9d8f2828387a
Sidd024 <siddhartha2488@gmail.com>
2018-11-30 19:20:33 +0530

    Change Code
    Change Code

--- 18/application/models/admin/Audit_started_model.php
+++ 18/application/models/admin/Audit_started_model.php
@@ -2194,16 +2194,16 @@ public function getExcel2017Data() {
 	public function getExcelsumbittedtheaudit() {
         $output = "";
 	$this->db->where("a.make_school_disabled","1");
-	$this->db->where('a.complete_status','1');	
-        $arrRecord = $this->db->select('a.*, b.name AS state_name, c.name AS district_name , d.password,e.status as status')
-                        ->from('gsp_school AS a')
-                        ->join('states AS b', 'a.state=b.id', 'left')
-                        ->join('cities AS c', 'a.district=c.id', 'left')
-		         ->join('gsp_user AS d', 'a.userid=d.id', 'left')
-                         >join('gsp_aduit_submitted AS e','e.userid=a.userid', 'left')
-		                ->where('a.progress', '100')
-                                ->order_by('a.id', 'desc')
-                                ->get()->result();
+	$arrRecord = $this->db->select('a.*, b.name AS state_name, c.name AS district_name , d.password')
+                       ->from('gsp_school AS a')
+                       ->join('states AS b', 'a.state=b.id', 'left')
+                       ->join('cities AS c', 'a.district=c.id', 'left')
+                        ->join('gsp_user AS d', 'a.userid=d.id', 'left')
+
+                        ->where('a.progress', '100')
+                        ->where('a.complete_status','1')
+                       ->order_by('a.id', 'desc')
+                       ->get()->result();
         
         $k = 1;
         $isdCode = '+91';
