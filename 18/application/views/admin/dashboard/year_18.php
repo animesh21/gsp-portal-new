9da9f91b23b03bbfd643fe4922a8bd78ece6ce2d
Sidd024 <siddhartha2488@gmail.com>
2018-11-30 17:05:19 +0530

    Change Code
    Change Code

--- 18/application/views/admin/dashboard/year_18.php
+++ 18/application/views/admin/dashboard/year_18.php
@@ -7,7 +7,7 @@
     </a>
     <div class="title">Gsp Audit Phase 1</div>
 </div>
-<div class="schools-statics"> <a href="#">
+<div class="schools-statics"> <a href="<?php base_url()?>audit_pahse_2" target="_blank">
         <div class="statics-circle"> <span>
 		  0
 		</span>
