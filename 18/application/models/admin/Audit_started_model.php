0800c436784746251f27b943d16e6b15ddd03be5
Sidd024 <siddhartha2488@gmail.com>
2018-11-30 19:05:19 +0530

    Change Code
    Change Code

--- 18/application/models/admin/Audit_started_model.php
+++ 18/application/models/admin/Audit_started_model.php
@@ -2202,9 +2202,8 @@ public function getExcel2017Data() {
                        
 		                ->where('a.progress', '100')
 		                ->where('a.complete_status','1')
-		               ->where('a.make_school_disabled',1)
-                        ->order_by('a.id', 'desc')
-                        ->get()->result();
+                                ->order_by('a.id', 'desc')
+                                ->get()->result();
         
         $k = 1;
         $isdCode = '+91';
