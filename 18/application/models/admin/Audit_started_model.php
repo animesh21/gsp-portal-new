2d6f0472f5178913c3203d0205814d521b06cfbf
rohit12371 <31234754+rohit12371@users.noreply.github.com>
2018-12-03 14:07:04 +0530

    remove disable
    remove disable from getExcelData_phase2

--- 18/application/models/admin/Audit_started_model.php
+++ 18/application/models/admin/Audit_started_model.php
@@ -495,7 +495,7 @@ class Audit_started_model extends CI_Model {
 	
 	public function getExcelData_phase2() {
         $output = "";
-	$this->db->where("a.make_school_disabled","1");
+	
         $arrRecord = $this->db->select('a.*, b.name AS state_name, c.name AS district_name, d.password')
                         ->from('gsp_school AS a')
                         ->join('states AS b', 'a.state=b.id', 'left')
