9f4c9351f9cda65af359d72b0a96bc0ea7e857a7
Sidd024 <siddhartha2488@gmail.com>
2019-01-15 11:42:23 +0530

    Change Code
    Change Code

--- 18/application/models/admin/Dashboard_model.php
+++ 18/application/models/admin/Dashboard_model.php
@@ -433,7 +433,7 @@ class Dashboard_model extends CI_Model {
    
       public function getSubmittedTheAudit_18data(){
 	    $this->db->where("a.make_school_disabled","1");  
-		$this->db->where('d.date_on <', '2018-11-19');   
+		$this->db->where('d.date_on <=', '2018-11-19');   
         return $this->db->select('a.*, b.name AS state_name,c.name As district_name')
         ->from('gsp_school AS a')
         ->join('states AS b', 'a.state=b.id', 'left')
