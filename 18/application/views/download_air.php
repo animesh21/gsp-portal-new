3d0502f1168b804689cb6e6f5e30fb99de5953a4
yhamender <yhamender@gmail.com>
2019-02-08 18:09:06 +0530

    sdljd

--- 18/application/views/download_air.php
+++ 18/application/views/download_air.php
@@ -191,89 +191,89 @@ ul.nav li:hover > ul.dropdown-menu { display: block; z-index:10000;}
 <script src="https://code.highcharts.com/modules/data.js"></script>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script type="text/javascript">
-   var chart2 =  Highcharts.chart('grapSchoolStatus',{
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Schools Participation Report In All GSP Audit'
-        },
-        xAxis: {
-            categories: [
-                '2015',
-				'2016',
-				'2017',
-				'2018',
+//    var chart2 =  Highcharts.chart('grapSchoolStatus',{
+//         chart: {
+//             type: 'column'
+//         },
+//         title: {
+//             text: 'Schools Participation Report In All GSP Audit'
+//         },
+//         xAxis: {
+//             categories: [
+//                 '2015',
+// 				'2016',
+// 				'2017',
+// 				'2018',
 				
-            ],
-            crosshair: true
-        },
+//             ],
+//             crosshair: true
+//         },
 		
-		legend: {
-        enabled: true
-    },
+// 		legend: {
+//         enabled: true
+//     },
     
 		
-        exporting: { enabled: false },
-        credits: {enabled: false},
-        tooltip: {
-            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-            pointFormat: '<tr><td style="color:{series.color};padding:0;font-size:12px;">{series.name}: </td>' +
-                '<td style="padding:0;font-size:12px;">{point.y:.1f}</td></tr>',
-            footerFormat: '</table>',
-            shared: true,
-            useHTML: true
-        },
-        plotOptions: {
-		     series: {
-            borderWidth: 0,
-            dataLabels: {
-                enabled: true,
-                format: '{point.y}'
-            }
-        },
+//         exporting: { enabled: false },
+//         credits: {enabled: false},
+//         tooltip: {
+//             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+//             pointFormat: '<tr><td style="color:{series.color};padding:0;font-size:12px;">{series.name}: </td>' +
+//                 '<td style="padding:0;font-size:12px;">{point.y:.1f}</td></tr>',
+//             footerFormat: '</table>',
+//             shared: true,
+//             useHTML: true
+//         },
+//         plotOptions: {
+// 		     series: {
+//             borderWidth: 0,
+//             dataLabels: {
+//                 enabled: true,
+//                 format: '{point.y}'
+//             }
+//         },
 		
-            column: {
-                pointPadding: 0.2,
-                borderWidth: 0
-            }
-        },
-        series: [{
-            name: 'School Performace Each Year',
-			color:'rgb(124, 181, 236)',
-            data: [54.43,67.34,78.55,89.90] //NORTH
-            //Registration, Audit Started, Audit Completed, Feedback Recieved
-        }		
-		],
-});
-// the button handler    
-    $('#buttonExport1').click(function() {
-        var e = document.getElementById("ExportOption1");
-		var ExportAs = e.options[e.selectedIndex].value;   
+//             column: {
+//                 pointPadding: 0.2,
+//                 borderWidth: 0
+//             }
+//         },
+//         series: [{
+//             name: 'School Performace Each Year',
+// 			color:'rgb(124, 181, 236)',
+//             data: [54.43,67.34,78.55,89.90] //NORTH
+//             //Registration, Audit Started, Audit Completed, Feedback Recieved
+//         }		
+// 		],
+// });
+// // the button handler    
+//     $('#buttonExport1').click(function() {
+//         var e = document.getElementById("ExportOption1");
+// 		var ExportAs = e.options[e.selectedIndex].value;   
        
-        if(ExportAs == 'PNG')
-        {
-            chart2.exportChart({type: 'image/png', filename: 'my-png'}, {subtitle: {text:''}});
-        }
-        if(ExportAs == 'JPEG')
-        {
-            chart2.exportChart({type: 'image/jpeg', filename: 'my-jpg'}, {subtitle: {text:''}});
-        }
-        if(ExportAs == 'PDF')
-        {
-            chart2.exportChart({type: 'application/pdf', filename: 'my-pdf'}, {subtitle: {text:''}});
-        }
-        if(ExportAs == 'SVG')
-        {
-            chart2.exportChart({type: 'image/svg+xml', filename: 'my-svg'}, {subtitle: {text:''}});
-        }
-    }); 
+//         if(ExportAs == 'PNG')
+//         {
+//             chart2.exportChart({type: 'image/png', filename: 'my-png'}, {subtitle: {text:''}});
+//         }
+//         if(ExportAs == 'JPEG')
+//         {
+//             chart2.exportChart({type: 'image/jpeg', filename: 'my-jpg'}, {subtitle: {text:''}});
+//         }
+//         if(ExportAs == 'PDF')
+//         {
+//             chart2.exportChart({type: 'application/pdf', filename: 'my-pdf'}, {subtitle: {text:''}});
+//         }
+//         if(ExportAs == 'SVG')
+//         {
+//             chart2.exportChart({type: 'image/svg+xml', filename: 'my-svg'}, {subtitle: {text:''}});
+//         }
+//     }); 
 
-    $('#buttonPrint').click(function() {
-        chart.setTitle(null, { text: ' ' });
-        chart.print();
-        chart.setTitle(null, { text: 'Click and drag in the plot area to zoom in' });
-    });
+//     $('#buttonPrint').click(function() {
+//         chart.setTitle(null, { text: ' ' });
+//         chart.print();
+//         chart.setTitle(null, { text: 'Click and drag in the plot area to zoom in' });
+//     });
 
 
 </script>
