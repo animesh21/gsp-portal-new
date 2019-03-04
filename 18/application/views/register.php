f73d2c9136fa42e81eab5846f8c06c22a41c0c5c
rohit12371 <31234754+rohit12371@users.noreply.github.com>
2019-03-04 16:03:28 +0530

    chnage text
    chnage text

--- 18/application/views/register.php
+++ 18/application/views/register.php
@@ -217,12 +217,11 @@
 <div class="wrapper" style="display: block;">
   <div class="top-area">
     <h2>REGISTRATION Form</h2>
-    <p>The CSE's Green Schools Programme Audit 2019 is now open.
-      Please complete and submit this form to register for the GSP Audit 2019.
-      On submission of registration form, you will receive an email with login
-      credentials (username and password) for completing the Audit. Please
-      save your login credentials to be used when you want to access the GSP
-      Audit 2019. There are no fees for participation.</p>
+    <p>The CSE's Green Schools Programme Audit 2019 opens in the mid of the year 2019.
+	    Please complete and submit this form to register for the GSP Audit 2019.
+	    On submission of registration form, you will receive an email with login credentials (username and password).
+	    Please save your login credentials to be used when you want to access the GSP Audit 2019. 
+	    There is no fees for participation.  </p>
     <strong>Please spell the name of your school correctly – the name provided by you will appear in the digital certificates and other documents. </strong>
     <p class="required">All fields marked * are required.</p>
   </div>
