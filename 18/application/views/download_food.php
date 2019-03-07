c2d30547c2e848f76ef9e262b53debe93acafd30
yhamender <30542583+yhamender@users.noreply.github.com>
2019-03-07 12:42:20 +0530

    chnage code jeetu
    chnage code jeetu

--- 18/application/views/download_food.php
+++ 18/application/views/download_food.php
@@ -92,9 +92,9 @@
         </div>
       </div>
     </nav>
-    <div class="container" style="height:1200px;">
+    <div class="container">
       <div class="col-md-12">
-        <h1><strong>Food Section</strong></h1>
+        <h1><strong>Food Section, </strong>  <?php echo $this->session->userdata('USERNAME');?></h1>
         <hr/>
       </div>
       <div class="col-md-12">
