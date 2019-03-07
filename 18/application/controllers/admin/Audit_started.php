c6fdc5dd2f38fda3391b067215e217a6c339562d
rohit12371 <31234754+rohit12371@users.noreply.github.com>
2019-03-07 11:42:03 +0530

    chnage code
    chnage code

--- 18/application/controllers/admin/Audit_started.php
+++ 18/application/controllers/admin/Audit_started.php
@@ -442,7 +442,7 @@ class Audit_started extends CI_Controller {
 				$body->personalizations = array();
 				$body->personalizations[0] = new PepipostAPILib\Models\Personalizations;
 				$body->personalizations[0]->recipient = '7417rohitarora@gmail.com';
-				//$body->personalizations[0]->recipientCc = array($Totalemail);
+				$body->personalizations[0]->recipientCc = array($Totalemail);
 				             #To/Recipient email address
 
 				// Email Header
