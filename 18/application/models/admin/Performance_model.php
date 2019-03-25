3515600e1097453ed06c1ef79a4862f0b2be2f5d
yhamender <30542583+yhamender@users.noreply.github.com>
2019-03-25 19:12:16 +0530

    chnage code
    chnage code

--- 18/application/models/admin/Performance_model.php
+++ 18/application/models/admin/Performance_model.php
@@ -30,13 +30,12 @@ class Performance_model extends CI_Model {
 	return $data;
     }*/
 
-    public function getDataById($id = 1446) {
+    public function getDataById($id) {
 	return $this->db->select('a.*, b.name AS state_name, c.name AS district_name, d.password')
 			->from('gsp_school AS a')
 			->join('states AS b', 'a.state=b.id', 'left')
 			->join('cities AS c', 'a.district=c.id', 'left')
 			->join('gsp_user AS d', 'a.userid=d.id', 'left')
-			->where('a.progress', 100)
 			->where('a.id', $id)
 			->order_by('a.id', 'desc')
 			->get()->result();
