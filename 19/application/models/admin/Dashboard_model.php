9d9a0bba0fd5a8cb852b44a6d324f13341615b21
rohit12371 <31234754+rohit12371@users.noreply.github.com>
2019-04-16 14:47:17 +0530

    add getSchool_18data
    add getSchool_18data

--- 19/application/models/admin/Dashboard_model.php
+++ 19/application/models/admin/Dashboard_model.php
@@ -435,6 +435,17 @@ class Dashboard_model extends CI_Model {
         ->get()->result();
         }
 	
+	public function getSchool_18data(){
+	$this->db->where("a.make_school_disabled","1");      
+        return $this->db->select('a.*, b.name AS state_name,c.name As district_name')
+        ->from('gsp_school AS a')
+        ->join('states AS b', 'a.state=b.id', 'left')
+		->join('cities AS c', 'a.district=c.id', 'left')
+        ->where('a.date_added >=', '2018-02-21 00:00:00')
+        ->order_by('a.id', 'desc')
+        ->get()->result();
+      }
+	
 	
       public function getSchool_19data(){
 	$this->db->where("a.make_school_disabled","1");      
