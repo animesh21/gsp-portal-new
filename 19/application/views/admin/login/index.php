6c01fe2bd3af747847946082477cec2f21909be5
rohit12371 <31234754+rohit12371@users.noreply.github.com>
2019-04-18 13:45:59 +0530

    chnage code
    chnage code

--- 19/application/views/admin/login/index.php
+++ 19/application/views/admin/login/index.php
@@ -42,7 +42,8 @@
         <option value="2015">2015</option>
         <option value="2016">2016</option>
         <option value="2017">2017</option>
-        <option value="2017" selected="selected">2018</option>
+        <option value="2018">2018</option>
+	<option value="2017" selected="selected">2019</option>
       </select>
     </div>
     <button type="submit" class="btn org-btn width100">Next</button>
