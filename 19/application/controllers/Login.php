d68618cdfb02328099f31779d1da867643c3a3a8
rohit12371 <31234754+rohit12371@users.noreply.github.com>
2019-04-23 17:22:19 +0530

    chnage code
    chnage code

--- 19/application/controllers/Login.php
+++ 19/application/controllers/Login.php
@@ -32,7 +32,7 @@
 
 	GSP Audit 2018 is closed and GSP Audit 2019 will be open for participation by mid-2019. Please go to the GSP website for further updates. In case you would like to update information such as GSP co-ordinator's name, principal's name, mobile number, email address, etc, please send a mail with changes required to  support@greenschoolsprogramme.org
 
-	If your school has not registered earlier for the GSP Audit, please <a href='http://greenschoolsprogramme.org/audit/18/register' target='blank_'>register here</a>.
+	If your school has not registered earlier for the GSP Audit, please <a href='http://greenschoolsprogramme.org/audit/19/register' target='blank_'>register here</a>.
 
 	");
 					}
