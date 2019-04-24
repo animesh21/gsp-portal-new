2fc0fd0200c40135082e186689758437f47c9ff6
rohit12371 <31234754+rohit12371@users.noreply.github.com>
2019-04-24 12:53:02 +0530

    chnage code
    chnage code

--- 19/application/views/admin/dashboard/index.php
+++ 19/application/views/admin/dashboard/index.php
@@ -1,12 +1,12 @@
 <h1>Dashboard</h1>
 
-<div class="schools-statics "> <a href="<?php base_url()?>dashboard/year18" target="_blank">
+<!-- <div class="schools-statics "> <a href="<?php // base_url()?>dashboard/year18" target="_blank">
         <div class="statics-circle"> <span>2</span>
             <label>Phase</label>
         </div>
     </a>
     <div class="title">2018</div>
-</div>
+</div> -->
 
 <div class="schools-statics "> <a href="<?php base_url()?>dashboard/year19" target="_blank">
         <div class="statics-circle"> <span>2</span>
