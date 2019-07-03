492d3f005df86069ab8604033e105877be57c33e
yhamender <30542583+yhamender@users.noreply.github.com>
2019-07-03 12:01:11 +0530

    code change
    code change

--- 19/application/views/school.php
+++ 19/application/views/school.php
@@ -80,7 +80,7 @@ function checkUpdate(){
   <div class="content-form">
     <div class="log-nav">
       <div class="welcome">Welcome <span
-                        style="font-weight:bold"><?php echo $this->session->userdata('USERNAME'); ?></span></div>
+                        style="font-weight:bold"><?php echo $data["name"]; ?></span></div>
       <ul>
         <!--<li><a href="http://www.greenschoolsprogramme.org/GSP-Audit2016-Questions.pdf" target="_blank" onClick="_gaq.push([\" _trackevent\",\"download\",\"pdf\",this.href])"=""><img src="http://www.greenschoolsprogramme.org/audit2016/images/pdf-icon16.png"> Audit Questionnaire</a> |</li>-->
         <li><a href="<?php echo base_url('logout'); ?>">Logout</a></li>
