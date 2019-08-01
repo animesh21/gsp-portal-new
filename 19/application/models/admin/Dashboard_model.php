09a40047e35817b2bc5619238e2e3d5f6e68c20f
yhamender <30542583+yhamender@users.noreply.github.com>
2019-08-01 17:36:25 +0530

    code change
    code change

--- 19/application/models/admin/Dashboard_model.php
+++ 19/application/models/admin/Dashboard_model.php
@@ -555,8 +555,30 @@ class Dashboard_model extends CI_Model {
 		   ->like("make_school_disabled","0")
            ->get()->result();
 		   return $countDisbaled[0]->labelCount;
-       }	
+       }
+	
+	/******************************  2019 **************************/
+
+
+       public function getstartedtheaudit_19data(){
+		$this->db->where("a.make_school_disabled","1");
+		$this->db->where("a.complete_status",'0');
+        return $this->db->select('a.*, b.name AS state_name,c.name As district_name')
+        ->from('gsp_school AS a')
+        ->join('states AS b', 'a.state=b.id', 'left')
+		->join('cities AS c', 'a.district=c.id', 'left')
+		->where('a.progress>=', 10)
+		->where('a.progress<=', 100)
+        ->order_by('a.id', 'desc')
+        ->get()->result();
+        
+      }
+	
+	
+	
 }
+
+
 ?>
 
 
