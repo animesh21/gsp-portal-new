471ad9ad13795f616ba6fbd429c6fdfd1a6ee852
yhamender <30542583+yhamender@users.noreply.github.com>
2019-08-28 17:57:59 +0530

    code change
    code change

--- 19/application/models/admin/Dashboard_model.php
+++ 19/application/models/admin/Dashboard_model.php
@@ -498,7 +498,7 @@ class Dashboard_model extends CI_Model {
 	    ->where('a.progress', '100')
 		 ->where('a.complete_status','1')
 	    ->where('d.status','1')
-	    ->where('a.make_school_disabled',1)
+	     
         ->order_by('a.id', 'desc')
         ->get()->result();
 		
