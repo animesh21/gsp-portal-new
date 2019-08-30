46353f290e836379a1561638dccdd907210aa784
yhamender <30542583+yhamender@users.noreply.github.com>
2019-08-30 17:35:53 +0530

    code change
    code change

--- 19/application/models/admin/Audit_started_model.php
+++ 19/application/models/admin/Audit_started_model.php
@@ -2346,7 +2346,7 @@ public function getExcel2017Data() {
 	public function getExcelsumbittedtheaudit() {
     $output = "";
 	$this->db->where("a.make_school_disabled","1");
-	$this->db->where('e.date_on <=', '2018-11-19');
+	//$this->db->where('e.date_on <=', '2018-11-19');
 	$arrRecord = $this->db->select('a.*, b.name AS state_name, c.name AS district_name , d.password')
                        ->from('gsp_school AS a')
                        ->join('states AS b', 'a.state=b.id', 'left')
