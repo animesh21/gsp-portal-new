d7f723ec96e557f13c0f82086dd8734289c16914
yhamender <30542583+yhamender@users.noreply.github.com>
2019-11-08 16:46:44 +0530

    code change
    code change

--- 19/application/views/admin/audit/SubmittedTheAudit_school19.php
+++ 19/application/views/admin/audit/SubmittedTheAudit_school19.php
@@ -17,11 +17,11 @@
             <th>Co-ord. Email</th>
             <th>Co-ord. Mobile</th>
             <th>Progress</th>
-
+            <th>School Staus</th>
             <th>Action</th>
           </tr>
         </thead>
-        <?php //echo '<pre>'; print_r($record); ?>
+        <!-- <?php //echo '<pre>'; print_r($records); ?> -->
         <tbody>
           <?php $i=1; foreach($records as $r){ ?>
             <tr role="row" class="<?php echo ($i%2==0) ? "even" : "odd"; ?>">
@@ -37,6 +37,12 @@
                 <td><?php echo $r->coemail; ?></td>
                 <td><?php echo $r->comobile; ?></td>
                 <td><?php echo $r->progress; ?></td>
+                <?php if($r->make_school_disabled=='1'){ ?>
+                  <td>Yes</td>
+                <?php } else { ?>
+                  <td>No</td>
+                <?php } ?>
+                
                 <td class="action"><a target="_blank" href="<?php echo base_url('admin/audit_started_2017/response/').$r->id; ?>" title="View Responses"><img src="<?php echo base_url() ?>assets/front/images/1446146277_view6.png"></a></td>
             </tr>
           <?php $i++; } ?>
