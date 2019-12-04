636b4a8ab4084ccd0b8a22ab68f83c6a7b7bf9e2
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-04 12:01:58 +0530

    code change
    code change

--- 19/application/models/User_model.php
+++ 19/application/models/User_model.php
@@ -40,54 +40,53 @@ class User_model extends CI_Model
         return $msg;
     }
    
-   public function UserLoginDownload($argPost)
+  public function UserLoginDownload($argPost)
     {
-		$msg=''; 
-        $query = $this->db->select('a.*, a.username AS username, a.id AS id, a.status AS status, b.status AS status' )
+    $msg=''; 
+        $query = $this->db->select('a.*, a.username AS username, a.id AS id, a.status AS status, b.complete_status AS complete_status, b.progress AS progress')
             ->from('gsp_user As a')
-            ->join('gsp_aduit_submitted AS b', 'b.userid=a.id', 'left') 		
-            ->where(array('a.email' => $argPost['email'], 'a.password' => $argPost['password'], 'b.status'=>'1'))
+            ->join('gsp_school AS b', 'b.coemail=a.email', 'left')    
+            ->where(array('a.email' => $argPost['email'], 'a.password' => $argPost['password'], 'b.progress'=>'100', 'b.complete_status'=>'1'))
             ->get();
         if ($query->num_rows() > 0) {
             $row = $query->row();
-			if($row->login_status==0)
-			{
-				$userData = array(
-					'USERNAME' => $row->username,
-					'USER_ID' => $row->id,
-					'status' => $row->status,
-					'lastid' => $row->lastQuestionId,
+      if($row->login_status==0)
+      {
+        $userData = array(
+          'USERNAME' => $row->username,
+          'USER_ID' => $row->id,
+          'status' => $row->status,
+          'lastid' => $row->lastQuestionId,
                     'email' => $row->email
-				);
-            	$this->session->set_userdata($userData);
-            	$msg='success';
-			}
-			else
-			{
-			 	$msg='warning';
-			}
+        );
+              $this->session->set_userdata($userData);
+              $msg='success';
+      }
+      else
+      {
+        $msg='warning';
+      }
         }
-	   elseif($row->complete_status==0 || $row->progress<100)
-			{
-			  $msg='incomplete';
-	    }
-	       else
-		{
-		   $msg='error';
-		}
-	   
-		       
+     elseif($row->complete_status==0 || $row->progress<100)
+      {
+        $msg='incomplete';
+      }
+         else
+    {
+       $msg='error';
+    }
+     
+           
         return $msg;
     }
-	
 
 
-  public function UserLoginDownload2($argPost)
+    public function UserLoginDownload2($argPost)
     {
     $msg=''; 
         $query = $this->db->select('a.*, a.username AS username, a.id AS id, a.status AS status, b.status AS status' )
             ->from('gsp_user As a')
-            ->join('gsp_aduit_submitted_17 AS b', 'b.userid=a.id', 'left')     
+            ->join('gsp_aduit_submitted AS b', 'b.userid=a.id', 'left')     
             ->where(array('a.email' => $argPost['email'], 'a.password' => $argPost['password'], 'b.status'=>'1'))
             ->get();
         if ($query->num_rows() > 0) {
