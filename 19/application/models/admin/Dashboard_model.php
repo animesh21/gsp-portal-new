a3aaa2c741a5710612e829fa6b37d2a77be755ed
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-09 17:46:04 +0530

    code change
    code change

--- 19/application/models/admin/Dashboard_model.php
+++ 19/application/models/admin/Dashboard_model.php
@@ -429,6 +429,7 @@ class Dashboard_model extends CI_Model {
 	
 	public function getSchool_alldata(){
 	$this->db->where("a.make_school_disabled","1");
+	$this->db->where("a.date_added <=","2019-11-11 00:00:00");
         return $this->db->select('a.*, b.name AS state_name,c.name As district_name')
         ->from('gsp_school AS a')
         ->join('states AS b', 'a.state=b.id', 'left')
