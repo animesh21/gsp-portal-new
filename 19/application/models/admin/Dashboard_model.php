8d13fc6922db4508eb064fad3562e51488828efc
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-12 18:34:09 +0530

    code change
    code change

--- 19/application/models/admin/Dashboard_model.php
+++ 19/application/models/admin/Dashboard_model.php
@@ -397,7 +397,7 @@ class Dashboard_model extends CI_Model {
 		$this->db->where('progress = 100');
 		$this->db->where('progress = 100');
 		$this->db->where('complete_status','0');
-		$this->db->where('date_added <=', '2019-11-29 00:00:00');
+		$this->db->where('date_added >=', '2019-11-12 00:00:00');
 		$this->db->where("make_school_disabled","1");
 		return $this->db->count_all_results('gsp_school');
         
