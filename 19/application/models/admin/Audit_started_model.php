257203167a0f2b0bf19eec6687fa0f4bf81ddcda
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-12 16:17:48 +0530

    code change
    code change

--- 19/application/models/admin/Audit_started_model.php
+++ 19/application/models/admin/Audit_started_model.php
@@ -2430,7 +2430,7 @@ public function getExcel2017Data() {
 	 
     public function getExcelstartedtheauditbutnotcomplete() {
         $output = "";
-	    $this->db->where("a.date_added <=","2019-11-11 00:00:00");
+	    $this->db->where("a.date_added <=","2019-11-10 00:00:00");
 	    $this->db->where("a.make_school_disabled","1");
         $arrRecord = $this->db->select('a.*, b.name AS state_name, c.name AS district_name , d.password')
                         ->from('gsp_school AS a')
@@ -2501,7 +2501,7 @@ public function getExcel2017Data() {
 
 	 public function getExcelnotstatedtheaudit() {
         $output = "";
-	 $this->db->where("a.date_added <=","2019-11-11 00:00:00");
+	 $this->db->where("a.date_added <=","2019-11-10 00:00:00");
 	 $this->db->where("a.make_school_disabled","1");
         $arrRecord = $this->db->select('a.*, b.name AS state_name, c.name AS district_name , d.password')
                         ->from('gsp_school AS a')
