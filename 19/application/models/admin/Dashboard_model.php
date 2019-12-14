299c4fc311031eeb59c296d9bafef88592545e89
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-14 15:52:06 +0530

    code change
    code change

--- 19/application/models/admin/Dashboard_model.php
+++ 19/application/models/admin/Dashboard_model.php
@@ -210,7 +210,7 @@ class Dashboard_model extends CI_Model {
     }
 
     public function getData_phase_2() {
-	    $this->db->where('date_added >=', '2019-11-12 00:00:00');
+// 	    $this->db->where('date_added >=', '2019-11-12 00:00:00');
         return $this->db->select('a.*, b.name AS state_name,c.name As district_name')
                         ->from('gsp_school AS a')
                         ->join('states AS b', 'a.state=b.id', 'left')
