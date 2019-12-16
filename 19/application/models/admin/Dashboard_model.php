6c1198544899ebe6444826f8859f954ebe1beae7
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-16 18:16:12 +0530

    code change
    code change

--- 19/application/models/admin/Dashboard_model.php
+++ 19/application/models/admin/Dashboard_model.php
@@ -487,14 +487,14 @@ class Dashboard_model extends CI_Model {
       }
    
       public function getSubmittedTheAudit_19data(){
-	   $this->db->where("a.date_added <=","2019-11-11 00:00:00");
+	   $this->db->where("a.date_added <=","2019-11-10 00:00:00");
 	   return $this->db->select('a.*, b.name AS state_name,c.name As district_name')
           ->from('gsp_school AS a')
           ->join('states AS b', 'a.state=b.id', 'left')
 	    ->join('cities AS c', 'a.district=c.id', 'left')        
 	    ->where('a.progress', '100')
 	    ->where('a.complete_status','1')
-// 	    ->where('a.make_school_disabled',1)
+	    ->where('a.make_school_disabled',1)
         ->order_by('a.id', 'desc')
         ->get()->result();		
 		
