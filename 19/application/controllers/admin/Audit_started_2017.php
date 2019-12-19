b2a2ed2b848b3cab3fb60881e71aaeb65ca1bc80
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-19 14:06:04 +0530

    code change
    code change

--- 19/application/controllers/admin/Audit_started_2017.php
+++ 19/application/controllers/admin/Audit_started_2017.php
@@ -824,12 +824,13 @@ public function digital_certificate_for_principal_coordinator($argID)
 	     $data=$this->Audit_started_model->getZipImagesOfSchool($argID);		
 	      foreach($data as $r):
 		   $filedata=$r->file_name;		
-		   $filename=base_url()."uploads/files/".$r->file_name;		
+		   $filename=base_url()."uploads/files/".$r->file_name;	
+		endforeach;
 		   if(file_exists("uploads/files/".$filedata))
 		   { 
                      $this->zip->add_data($filedata,file_get_contents($filename)); 
 		   }
-	      endforeach; 
+	       
             $this->zip->download(date('d-M-Y'));
 	}
 	/*School Generate Badge Code*/
