f2701eb55530778bba14899438b804c72a2e6a01
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-19 14:12:12 +0530

    code change
    code change

--- 19/application/controllers/admin/Audit_started_2017.php
+++ 19/application/controllers/admin/Audit_started_2017.php
@@ -818,19 +818,15 @@ public function digital_certificate_for_principal_coordinator($argID)
     }
 	/*Download zip image*/
 	public function downloadzip($argID){
-		ini_set('memory_limit', '-1');
-	 ini_set('max_execution_time', 300);
-		$this->load->helper('file');
-	     $data=$this->Audit_started_model->getZipImagesOfSchool($argID);		
+	     $data=$this->Audit_started_model->getZipImagesOfSchool($argID);
 	      foreach($data as $r):
-		   $filedata=$r->file_name;		
-		   $filename=base_url()."uploads/files/".$r->file_name;	
-		endforeach;
+		   $filedata=$r->file_name;
+		   $filename=read_file(base_url()."uploads/files/".$r->file_name);
 		   if(file_exists("uploads/files/".$filedata))
-		   { 
-                     $this->zip->add_data($filedata,file_get_contents($filename)); 
+		   {
+           $this->zip->archive($filedata,file_get_contents($filename)); 
 		   }
-	       
+	      endforeach; 
             $this->zip->download(date('d-M-Y'));
 	}
 	/*School Generate Badge Code*/
