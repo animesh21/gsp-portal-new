34b6d600a49fc4840a06e18f18a5a72a242713d1
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-20 19:20:13 +0530

    code
    code

--- 19/application/controllers/admin/Audit_started_2017.php
+++ 19/application/controllers/admin/Audit_started_2017.php
@@ -821,7 +821,7 @@ public function digital_certificate_for_principal_coordinator($argID)
 	     $data=$this->Audit_started_model->getZipImagesOfSchool($argID);
 	      foreach($data as $r):
 		   $filedata=$r->file_name;
-		   $filename=base_url()."uploads/files/".$r->file_name;
+		   $filename=FCPATH."uploads/files/".$r->file_name;
 		   if(file_exists("uploads/files/".$filedata))
 		   {
                     $this->zip->add_data($filedata,$filename); 
