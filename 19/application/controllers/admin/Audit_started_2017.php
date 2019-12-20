89226ef00a906a163afd7a7cb8943a817622f2c9
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-20 18:29:19 +0530

    code
    code

--- 19/application/controllers/admin/Audit_started_2017.php
+++ 19/application/controllers/admin/Audit_started_2017.php
@@ -819,18 +819,16 @@ public function digital_certificate_for_principal_coordinator($argID)
 	/*Download zip image*/
 	public function downloadzip($argID){
 	     $data=$this->Audit_started_model->getZipImagesOfSchool($argID);
-	      foreach($data as $r){
+	      foreach($data as $r):
 		   $filedata=$r->file_name;
 		   $filename=base_url()."uploads/files/".$r->file_name;
 		   if(file_exists("uploads/files/".$filedata))
 		   {
-			              $this->zip->add_data($filedata,$filename,true)); 
-//                        $this->zip->read_file($filename, true); 
+           $this->zip->add_data($filedata,file_get_contents($filename)); 
 		   }
-	      } 
+	      endforeach; 
             $this->zip->download(date('d-M-Y'));
-	}
-	
+	}	
 	
 	/*School Generate Badge Code*/
 	public function generatebadge($argID){
