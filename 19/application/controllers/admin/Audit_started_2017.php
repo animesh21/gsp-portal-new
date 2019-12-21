1e5ae1512e1465b1c0c885bd22a79fecb0c84452
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-22 00:15:09 +0530

    zip image
    824 825 add upload

--- 19/application/controllers/admin/Audit_started_2017.php
+++ 19/application/controllers/admin/Audit_started_2017.php
@@ -822,8 +822,8 @@ public function digital_certificate_for_principal_coordinator($argID)
 	     $data=$this->Audit_started_model->getZipImagesOfSchool($argID);
 	      foreach($data as $r){
 		   $filedata=$r->file_name;
-		   $filename=base_url()."uploads/files/".$r->file_name;
-		   if(file_exists("uploads/files/".$filedata))
+		   $filename=base_url()."uploads/uploads/files/".$r->file_name;
+		   if(file_exists("uploads/uploads/files/".$filedata))
 		   {
                     $this->zip->add_data($filedata,$filename); 
 		    $this->zip->read_dir($filename);
