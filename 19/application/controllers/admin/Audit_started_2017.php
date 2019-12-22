a9c19eca27883802ce85d1a987821a7620f471c3
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-22 15:00:14 +0530

    df
    df

--- 19/application/controllers/admin/Audit_started_2017.php
+++ 19/application/controllers/admin/Audit_started_2017.php
@@ -824,7 +824,7 @@ public function digital_certificate_for_principal_coordinator($argID)
 	      foreach($data as $r):
 		   $filedata=$r->file_name;
 		   $filename=base_url()."uploads/files/".$r->file_name;
-		   if(file_exists("/uploads/files/".$filedata))
+		   if(file_exists("uploads/files/".$filedata))
 		   {
            $this->zip->add_data($filedata,file_get_contents($filename)); 
 		   }
