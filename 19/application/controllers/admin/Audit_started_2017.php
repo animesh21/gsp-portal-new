e41e7b66b5416d9396c56be9183ce55c123faba6
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-22 18:35:29 +0530

    gh
    bhj

--- 19/application/controllers/admin/Audit_started_2017.php
+++ 19/application/controllers/admin/Audit_started_2017.php
@@ -829,9 +829,9 @@ public function digital_certificate_for_principal_coordinator($argID)
 		   $filename=base_url()."uploads/files/".$r->file_name;
 		   if(file_exists("uploads/files/".$filedata))
 		   {
-                    $this->zip->add_data($filename,$filedata); 
-// 	            $this->zip->archive($filedata,$filename);
-// 		    $this->zip->read_dir($filedata,$filename);
+                    $this->zip->add_data($filedata,$filename); 
+ 	            $this->zip->archive($filedata,$filename);
+		    $this->zip->read_dir($filedata,$filename);
 			   
              } 		   
 	}	 
