d61768c4eb46ded7f3cd2339c77e31b6bf7acb5a
yhamender <30542583+yhamender@users.noreply.github.com>
2019-12-22 16:39:56 +0530

    code change
    code change

--- 19/application/controllers/admin/Audit_started_2017.php
+++ 19/application/controllers/admin/Audit_started_2017.php
@@ -826,7 +826,7 @@ public function digital_certificate_for_principal_coordinator($argID)
 	      foreach($data as $r){
 		   $filedata=$r->file_name;
 		   $filename=base_url()."uploads/files/".$r->file_name;
-		   if(file_exists("uploads/uploads/files/".$filedata))
+		   if(file_exists("uploads/files/".$filedata))
 		   {
 //                     $this->zip->add_data($filedata,$filename); 
 	             $this->zip->archive($filename);
