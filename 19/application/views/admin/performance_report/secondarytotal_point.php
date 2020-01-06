89557189b76350f4ca634d71b994b83c95c56635
yhamender <30542583+yhamender@users.noreply.github.com>
2020-01-06 17:33:04 +0530

    code change
    code change

--- 19/application/views/admin/performance_report/secondarytotal_point.php
+++ 19/application/views/admin/performance_report/secondarytotal_point.php
@@ -1758,7 +1758,7 @@
           <td><?php 
 		  global $percentage;
 		  $total=$record1[$i]->air_points + $record1[$i]->energy_points + $record1[$i]->food_points + $record1[$i]->land_points + $record1[$i]->water_points + $record1[$i]->waste_points; 
-		  $percentage= ($total/164)*100;
+		  $percentage= ($total/171)*100;
 	          $percentage=number_format($percentage,1);
 		  echo $percentage;
 		 ?></td>
