b76bef608effb83b1dfcc3bcbd7a616fe7d49a62
yhamender <30542583+yhamender@users.noreply.github.com>
2020-01-06 15:43:13 +0530

    code change
    code change

--- 19/application/views/admin/survey/digital-certificate.php
+++ 19/application/views/admin/survey/digital-certificate.php
@@ -30,7 +30,7 @@
 
 		if ($principal->principle_name !="") {
 		    echo "<div class='container'>
-		    <div class='image'><img src='.assets/img/images/certificate_new_19_1.jpg' width='1000' height='700'></div>
+		    <div class='image'><img src='assets/img/images/certificate_new_19_1.jpg' width='1000' height='700'></div>
 
   <div class='principal'>" . $principal->principle_name ."</div>&nbsp;
   <div class='school_name'>". $certificate->certificate_schoolname."</div>
@@ -48,7 +48,7 @@
 
 		if ($staffadmin !="") {
 		   echo "<div class='container'>
-		    <div class='image'><img src='.assets/img/images/certificate_new_19_1.jpg' width='1000' height='700'></div>
+		    <div class='image'><img src='assets/img/images/certificate_new_19_1.jpg' width='1000' height='700'></div>
 
   <div class='principal'>" . ucwords($staffadmin)  ."</div>&nbsp;
   <div class='school_name'>". $certificate->certificate_schoolname."</div>
