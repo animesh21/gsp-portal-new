26afae1387e5647d7a2c603b1f469e219a7b5c0f
yhamender <30542583+yhamender@users.noreply.github.com>
2020-01-06 17:47:37 +0530

    code change
    code change

--- 19/application/views/admin/performance_report/primarytotal_point.php
+++ 19/application/views/admin/performance_report/primarytotal_point.php
@@ -1304,7 +1304,7 @@ else{echo $record1[$i]->Q8W2S2;}
 <td><?php 
 global $percentage;
 $total=$record1[$i]->air_points + $record1[$i]->energy_points + $record1[$i]->food_points + $record1[$i]->land_points + $record1[$i]->water_points + $record1[$i]->waste_points; 
-$percentage= ($total/95)*100;
+$percentage= ($total/101)*100;
 $percentage=number_format($percentage,1);
 echo $percentage;
 ?></td>
