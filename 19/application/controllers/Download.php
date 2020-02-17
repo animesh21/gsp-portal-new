87c6a60b90ecabc11dbfb3d507a9c22dadae23b5
yhamender <30542583+yhamender@users.noreply.github.com>
2020-02-17 14:41:40 +0530

    code change
    code change

--- 19/application/controllers/Download.php
+++ 19/application/controllers/Download.php
@@ -215,14 +215,14 @@ function getdigitalCertificate($argID) {
 //    $this->dompdf->stream("Digital Certificate.pdf", array("Attachment" => false));
 //   }
 	
- public function count_certificates_stuents($argID) {
+public function count_certificates_stuents($argID) {
    //Stuents
    $arrStudents = array();
    $question_alphabet = array("A", "E", "F", "L", "W", "Wa");
-   $username=$this->db->select('name')
-				->from('gsp_school')
-				->where('userid',$argID)
-				->get()->row();
+    $username=$this->db->select('name,state,district')
+        ->from('gsp_school')
+        ->where('userid',$argID)
+        ->get()->row();
    foreach ($question_alphabet as $q) {
        for ($i = 1; $i <= 10; $i++) {
        //Students
@@ -244,23 +244,23 @@ function getdigitalCertificate($argID) {
    $arrStaff = array();
    $question_alphabet = array("A", "E", "F", "L", "W", "Wa");
    $username=$this->db->select('name')
-				->from('gsp_school')
-				->where('userid',$argID)
-				->get()->row();
+        ->from('gsp_school')
+        ->where('userid',$argID)
+        ->get()->row();
    foreach ($question_alphabet as $q) {
        //Teachers
        for ($i = 1; $i <= 3; $i++) {
        if((getFiled("Q1" . $q . $i . "S1", $argID) !="0") && (getFiled("Q1" . $q . $i . "S1", $argID) !='') || (getFiled("Q1" . $q . $i . "S3", $argID) !="0") && (getFiled("Q1" . $q . $i . "S3", $argID) !=''))
        {
        $airTeachers[] = array("teacher"=>getFiled("Q1" . $q . $i . "S1", $argID) . " " . getFiled("Q1" . $q . $i . "S3", $argID),"school"=>$username->name);
-	   
+     
        }
        }
        //Staff
        for ($i = 1; $i <= 5; $i++) {
        if((getFiled("Q2" . $q . $i . "S1", $argID) !="0") && (getFiled("Q2" . $q . $i . "S1", $argID) !='') || (getFiled("Q2" . $q . $i . "S3", $argID) !="0") && (getFiled("Q2" . $q . $i . "S3", $argID) !='')){
        $arrStaff[] = array("teacher"=>getFiled("Q2" . $q . $i . "S1", $argID) . " " . getFiled("Q2" . $q . $i . "S3", $argID),"school"=>$username->name);
-	
+  
        }
        }
    }
@@ -269,7 +269,7 @@ function getdigitalCertificate($argID) {
 //    print_r($temp);
    //echo '<pre>'; print_r($airTeacher['school']); exit;
    //echo '<pre>'; print_r($arrStaff);
-   return array_merge($airTeachers, $arrStaff);
+   return array_merge($airTeachers);
   }
   
   public function digital_certificate_for_principal_coordinator($argID)
