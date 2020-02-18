cbb406c925a62b1ef5614b82a15a2e2cf77f056c
yhamender <30542583+yhamender@users.noreply.github.com>
2020-02-18 11:51:48 +0530

    code change
    code change

--- 19/application/models/admin/Dashboard_model.php
+++ 19/application/models/admin/Dashboard_model.php
@@ -316,7 +316,7 @@ class Dashboard_model extends CI_Model {
 	}
 	
 	public function getschool_that_submit_audit_data_phase_2() {
-	$this->db->where("a.make_school_disabled","1");	
+// 	$this->db->where("a.make_school_disabled","1");	
 	return $this->db->select('a.*,b.name AS state_name,c.name As district_name')
         ->from('gsp_school AS a')
         ->join('states AS b', 'a.state=b.id', 'left')
