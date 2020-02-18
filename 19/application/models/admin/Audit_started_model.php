0125406bba3c244aefe2adc5b0e24b00db24cea1
yhamender <30542583+yhamender@users.noreply.github.com>
2020-02-18 11:51:16 +0530

    code change
    code change

--- 19/application/models/admin/Audit_started_model.php
+++ 19/application/models/admin/Audit_started_model.php
@@ -1426,7 +1426,7 @@ class Audit_started_model extends CI_Model {
 
     public function getExcelDataByProgress1_phase2($progress) {
         $output = "";
-	    $this->db->where("a.make_school_disabled","1");
+// 	    $this->db->where("a.make_school_disabled","1");
       $this->db->where("e.date_on>=","2019-11-12 00:00:00");
       $arrRecord = $this->db->select('a.*,b.name AS state_name,c.name As district_name , d.password')
                                 ->from('gsp_school AS a')
