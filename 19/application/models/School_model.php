78ac4dec6b0a5e57dce4b894a775b4ce183d1a18
yhamender <30542583+yhamender@users.noreply.github.com>
2020-03-02 13:31:52 +0530

    code change
    code change

--- 19/application/models/School_model.php
+++ 19/application/models/School_model.php
@@ -105,8 +105,8 @@ class School_model extends CI_Model
             $subject = "GSP Audit Registration".$date;
             $msg = "Dear &nbsp;";
             $msg .= $query->coname . "," . "<br/><br/>";
-            $msg .= "Thank you for registering your school '" . $query->name . "', for GSP (Green Schools Programme) Audit 2019. Your account has been successfully created.<br><br>";
-            $msg .= "To participate in GSP Audit 2019, please remember to save your username and password given below.<br><br>";
+            $msg .= "Thank you for registering your school '" . $query->name . "', for GSP (Green Schools Programme) Audit 2020. Your account has been successfully created.<br><br>";
+            $msg .= "GSP Audit 2020 will now open in July 2020. To participate, please remember to save your username and password given below.<br><br>";
 //            $msg .= "The launch date of GSP Audit 2017 will be announced at www.greenschoolsprogramme.org, in July 2017.<br><br>";
             $msg .= "URL: http://www.greenschoolsprogramme.org/audit/19 <br/><br/>";
             $msg .= "Username: " . $query->coemail . "<br><br>";
