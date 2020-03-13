3ce1900af3bb84ff1cbb26b4911ac9dcb4e9d789
yhamender <30542583+yhamender@users.noreply.github.com>
2020-03-13 13:51:53 +0530

    code change
    code change

--- 19/application/models/admin/Dashboard_model.php
+++ 19/application/models/admin/Dashboard_model.php
@@ -338,12 +338,13 @@ class Dashboard_model extends CI_Model {
 					->order_by('a.id', 'desc')
 					->get()->result(); */
 		$this->db->where("a.make_school_disabled","1");
+		$this->db->where('a.date_added >=', '2019-11-12 00:00:00');
 		return $this->db->select('a.*, b.name AS state_name,c.name As district_name')
 					->from('gsp_school AS a')
 					->join('states AS b', 'a.state=b.id', 'left')
 					->join('cities AS c', 'a.district=c.id', 'left')
 					->where('a.complete_status','0')
-                                        ->where('a.progress',100)
+                                        ->where('a.progress_phase_2',100)
 					->get()->result();
 		
 		
