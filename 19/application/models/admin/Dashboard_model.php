1e4c2a801b38186c8676721f73cc61acbe26770e
yhamender <30542583+yhamender@users.noreply.github.com>
2020-03-24 00:40:21 +0530

    463 commit
    463

--- 19/application/models/admin/Dashboard_model.php
+++ 19/application/models/admin/Dashboard_model.php
@@ -460,7 +460,7 @@ class Dashboard_model extends CI_Model {
       }
 	  
 	   public function getstartedtheaudit_19data(){
-	$this->db->where("a.date_added <=","2019-11-10 00:00:00");
+	//$this->db->where("a.date_added <=","2019-11-10 00:00:00");
 	$this->db->where("a.make_school_disabled","1");
 	$this->db->where("a.complete_status",'0');
         return $this->db->select('a.*, b.name AS state_name,c.name As district_name')
