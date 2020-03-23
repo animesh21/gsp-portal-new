6dbfe1712622accadfb20a42aeffaba7386a8dd5
yhamender <30542583+yhamender@users.noreply.github.com>
2020-03-24 00:52:35 +0530

    662 648
    commit

--- 19/application/models/admin/Dashboard_model.php
+++ 19/application/models/admin/Dashboard_model.php
@@ -632,7 +632,7 @@ class Dashboard_model extends CI_Model {
 	public function combinded_completecount()
 	{		
 		$this->db->where("a.make_school_disabled","1");
-		$this->db->where("a.date_added >=","2019-03-03 00:00:00");
+		//$this->db->where("a.date_added >=","2019-03-03 00:00:00");
 		return $this->db->select('a.*, b.name AS state_name,c.name As district_name, d.password')
 					->from('gsp_school AS a')
 					->join('states AS b', 'a.state=b.id', 'left')
@@ -659,7 +659,7 @@ class Dashboard_model extends CI_Model {
 	}
 
 	public function combinded_uncompletecount()
-	{	$this->db->where('a.date_added >=', '2019-03-03 00:00:00');
+	{	//$this->db->where('a.date_added >=', '2019-03-03 00:00:00');
 		$this->db->where("a.make_school_disabled","1");
 		return $this->db->select('a.*, b.name AS state_name,c.name As district_name')
 					->from('gsp_school AS a')
