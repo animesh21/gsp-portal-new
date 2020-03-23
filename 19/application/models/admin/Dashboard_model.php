4dab73bf805e66a48b705414524460428476e4e2
yhamender <30542583+yhamender@users.noreply.github.com>
2020-03-23 13:45:44 +0530

    commit 355
    355

--- 19/application/models/admin/Dashboard_model.php
+++ 19/application/models/admin/Dashboard_model.php
@@ -352,7 +352,7 @@ class Dashboard_model extends CI_Model {
 	}
 	
 	public function school_start_but_not_complete_phase_2()
-	{	$this->db->where('a.date_added >=', '2019-11-12 00:00:00');
+	{	//$this->db->where('a.date_added >=', '2019-11-12 00:00:00');
 		$this->db->where("a.make_school_disabled","1");
 		return $this->db->select('a.*, b.name AS state_name,c.name As district_name')
 					->from('gsp_school AS a')
