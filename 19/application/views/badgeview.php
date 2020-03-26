08ac6ba2644b12ff3343bcb1a41c420ea2d4f6d9
yhamender <30542583+yhamender@users.noreply.github.com>
2020-03-26 14:00:04 +0530

    code change
    code change

--- 19/application/views/badgeview.php
+++ 19/application/views/badgeview.php
@@ -75,7 +75,7 @@
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
           </div>
           <h2>
-      "https://www.greenschoolsprogramme.org/audit/18/assets/js/badge-code.js"> <br/>
+      "https://www.greenschoolsprogramme.org/audit/18/assets/js/badge-code.js" <br/>
       /*School Badge Code*/ <br/>
       window.onload = function(){
       <br/>
