1073c0f7c10dacd0214cc69d1859fc6531961ce7
yhamender <yhamender@gmail.com>
2020-04-20 16:22:58 +0530

    dsdssf

---
+++ 19/assets/js/test.js
@@ -0,0 +1,341 @@
+function computeeSegregate()
+{
+	var sumHor = 0;
+	
+		var id1 = "Q12Wa1S1";
+		var id2 = "Q12Wa2S2";
+		var id3 = "Q12Wa3S3";
+		var id4 = "Q12Wa4S4";
+		var id5 = "Q12Wa5S5";
+		var id6 = "Q12Wa6S5";
+		var id7 = "Q12Wa7S5";
+		var id8 = "Q12Wa8S5";
+		var id9 = "Q12Wa9S5";
+		var id10 = "Q12Wa10S5";
+		var id11 = "Q12Wa11S5";
+		var id12 = "Q12Wa12S5";
+		var id13 = "Q12Wa13S5";
+		var id14 = "Q12Wa14S5";
+		var id15 = "Q12Wa15S5";
+		var id16 = "Q12Wa16S5";
+		var id17 = "Q12Wa17S5";
+		var id18 = "Q12Wa18S5";
+		var id19 = "Q12Wa19S5";
+		var id20 = "Q12Wa20S5";
+		var id21 = "Q12Wa21S5";
+		var id22 = "Q12Wa22S5";
+		var id23 = "Q12Wa23S5";
+		var id24 = "Q12Wa24S5";
+		var id25 = "Q12Wa25S5";
+		var id26 = "Q12Wa26S5";
+		var id27 = "Q12Wa27S5";
+		var id28 = "Q12Wa28S5";
+		
+		var element1 = document.getElementById(id1);
+		var element2 = document.getElementById(id2);
+		var element3 = document.getElementById(id3);
+		var element4 = document.getElementById(id4);
+		var element5 = document.getElementById(id5);
+		var element6 = document.getElementById(id6);
+		var element7 = document.getElementById(id7);
+		var element8 = document.getElementById(id8);
+		var element9 = document.getElementById(id9);
+		var element10 = document.getElementById(id10);
+		var element11 = document.getElementById(id11);
+		var element12 = document.getElementById(id12);
+		var element13 = document.getElementById(id13);
+		var element14 = document.getElementById(id14);
+		var element15 = document.getElementById(id15);
+		var element16 = document.getElementById(id16);
+		var element17 = document.getElementById(id17);
+		var element18 = document.getElementById(id18);
+		var element19 = document.getElementById(id19);
+		var element20 = document.getElementById(id20);
+		var element21 = document.getElementById(id21);
+		var element22 = document.getElementById(id22);
+		var element23 = document.getElementById(id23);
+		var element24 = document.getElementById(id24);
+		var element25 = document.getElementById(id25);
+		var element26 = document.getElementById(id26);
+		var element27 = document.getElementById(id27);
+		var element28 = document.getElementById(id28);
+		 		
+		element1 = parseInt(element1.value);
+		var element101 = parseInt(element1.placeholder);
+		
+		element2 = parseInt(element2.value);
+		var element201 = parseInt(element2.placeholder);
+		
+		element3 = parseInt(element3.value);
+		var element31 = parseInt(element3.placeholder);
+		
+		element4 = parseInt(element4.value);
+		var element41 = parseInt(element4.placeholder);
+		
+		element5 = parseInt(element5.value);
+		var element51 = parseInt(element5.placeholder);
+		
+		element6 = parseInt(element6.value);
+		var element61 = parseInt(element6.placeholder);
+		
+		element7 = parseInt(element7.value);
+		var element71 = parseInt(element7.placeholder);
+		
+		element8 = parseInt(element8.value);
+		var element81 = parseInt(element8.placeholder);
+		
+		element9 = parseInt(element9.value);
+		var element91 = parseInt(element9.placeholder);
+		
+		element10 = parseInt(element10.value);
+		var element1001 = parseInt(element10.placeholder);
+		
+		element1 = parseInt(element11.value);
+		var element1101 = parseInt(element11.placeholder);
+		
+		element12 = parseInt(element12.value);
+		var element1201 = parseInt(element12.placeholder);
+		
+		element13 = parseInt(element13.value);
+		var element1301 = parseInt(element13.placeholder);
+		
+		element14 = parseInt(element14.value);
+		var element1401 = parseInt(element14.placeholder);
+		
+		element15 = parseInt(element15.value);
+		var element1501 = parseInt(element15.placeholder);
+		
+		element16 = parseInt(element16.value);
+		var element1601 = parseInt(element16.placeholder);
+		
+		element17 = parseInt(element17.value);
+		var element1701 = parseInt(element17.placeholder);
+		
+		element18 = parseInt(element18.value);
+		var element1801 = parseInt(element18.placeholder);
+		
+		element19 = parseInt(element19.value);
+		var element1901 = parseInt(element19.placeholder);
+		
+		element20 = parseInt(element20.value);
+		var element2000 = parseInt(element20.placeholder);
+		
+		element21 = parseInt(element21.value);
+		var element2101 = parseInt(element21.placeholder);
+		
+		element22 = parseInt(element22.value);
+		var element2201 = parseInt(element22.placeholder);
+		
+		element23 = parseInt(element23.value);
+		var element2301 = parseInt(element23.placeholder);
+		
+		element24 = parseInt(element24.value);
+		var element2401 = parseInt(element24.placeholder);
+		
+		element25 = parseInt(element25.value);
+		var element2501 = parseInt(element25.placeholder);
+		
+		element26 = parseInt(element26.value);
+		var element2601 = parseInt(element26.placeholder);
+		
+		element27 = parseInt(element27.value);
+		var element2701 = parseInt(element27.placeholder);
+		
+		element28 = parseInt(element28.value);
+		var element2801 = parseInt(element28.placeholder);
+		
+		if(isNaN(element1))
+			if(isNaN(element101))
+				element1 = 0;
+			else
+				element1 = element101; 		
+		
+		if(isNaN(element2))
+			if(isNaN(element201))
+				element2 = 0;
+			else
+				element2 = element201;
+		
+		if(isNaN(element3))
+			if(isNaN(element31))
+				element3 = 0;
+			else
+				element3 = element31;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		if(isNaN(element4))
+			if(isNaN(element41))
+				element4 = 0;
+			else
+				element4 = element41;
+		
+		sumHor = element1 + element2 + element3 + element4;
+		$('#'+id5).val(sumHor);
+		sumVert1 += element1;
+		sumVert2 += element2;
+		sumVert3 += element3;
+		sumVert4 += element4;
+		sumVert5 += sumHor;
+		sumHor = 0;
+		element1 = 0;
+		element2 = 0;
+		element3 = 0;
+		element4 = 0;	 
+	
+
+}
