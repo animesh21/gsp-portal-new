975dad62d20583b8b7f7cb67b5137d8e2b593c58
yhamender <30542583+yhamender@users.noreply.github.com>
2020-04-20 09:42:57 +0530

    code change
    code change

--- 19/application/models/User_model.php
+++ 19/application/models/User_model.php
@@ -11,42 +11,42 @@ class User_model extends CI_Model
 
    public function UserLogin($argPost)
     {
-		$msg=''; 
+    $msg=''; 
         $query = $this->db->select('a.*, a.username AS username, a.id AS id, a.status AS status, a.login_status as login_status, b.complete_status, c.date_on' )
             ->from('gsp_user As a')
             ->join('gsp_school AS b', 'b.userid=a.id', 'left')
-	    ->join('gsp_aduit_submitted AS c', 'b.id=c.school_id', 'left')
+      ->join('gsp_aduit_submitted AS c', 'b.id=c.school_id', 'left')
             ->where(array('a.email' => $argPost['email'], 'a.password' => $argPost['password']))
-		->order_by('c.date_on', 'desc')
+    ->order_by('c.date_on', 'desc')
             ->get();
 //             echo $this->db->last_query();
         if ($query->num_rows() > 0) {
             $row = $query->row();
-// 		if($row->complete_status==1 || $row->date_on >='2019-11-12 00:00:00')
-			if($row->login_status==1)
-			{
-			$userData = array(
-			'USERNAME' => $row->username,
-			'USER_ID' => $row->id,
-			'status' => $row->status,
-			'lastid' => $row->lastQuestionId
-			);
-            	$this->session->set_userdata($userData);
-            	$msg='success';
-		}
-		
-// 	 elseif($row->complete_status==1) {
+//    if($row->complete_status==1 || $row->date_on >='2019-11-12 00:00:00')
+      if($row->login_status==1)
+      {
+      $userData = array(
+      'USERNAME' => $row->username,
+      'USER_ID' => $row->id,
+      'status' => $row->status,
+      'lastid' => $row->lastQuestionId
+      );
+              $this->session->set_userdata($userData);
+              $msg='success';
+    }
+    
+//   elseif($row->complete_status==1) {
 //         $msg = 'complitesurveyschool';
 //          }
-	
-	else
-	{
-	$msg='warning';
-	}
+  
+  else
+  {
+  $msg='warning';
+  }
         } else
-		{
-		   $msg='error';
-		}
+    {
+       $msg='error';
+    }
         return $msg;
     }
    
@@ -130,7 +130,7 @@ class User_model extends CI_Model
            
         return $msg;
     }
-	
+  
    public function UserLoginDownload1($argPost)
     {
        $query = $this->db->select('*')
@@ -142,15 +142,15 @@ class User_model extends CI_Model
         }
         return false;
     }
-	
-	
-	
-	
-	
-	
-	
-	
-	
+  
+  
+  
+  
+  
+  
+  
+  
+  
     public function CreateUser($argPost)
     {
         $query = $this->db->select('COUNT(*) count')
@@ -164,7 +164,7 @@ class User_model extends CI_Model
             'password' => $argPost['Password'],
             'username' => $argPost['username']
         );
-		 $query1 = $this->db->select('COUNT(*) as count')
+     $query1 = $this->db->select('COUNT(*) as count')
                 ->from('gsp_user')
                 ->where('email', $argPost['email'])    
                 ->get()->result();
@@ -213,7 +213,7 @@ class User_model extends CI_Model
     public function getCIties($argID)
     {
         $cities = array();    
-	$this->db->order_by("name");
+  $this->db->order_by("name");
         $data = $this->db->get_where('cities', array('state_id =' => $argID))->result();
         return $data;
     }
@@ -257,7 +257,7 @@ class User_model extends CI_Model
     {
        
         $this->load->helper('string');
-		$oneTimeCounter =$this->db->get_where('gsp_school',array("coemail"=>$this->input->post('val')))->num_rows();	
+    $oneTimeCounter =$this->db->get_where('gsp_school',array("coemail"=>$this->input->post('val')))->num_rows();  
         $email_count = $this->db->get_where('gsp_user', array('email' => $this->input->post('val')))->num_rows();
         if ($email_count > 0) {
             $varNewPass = strtolower(random_string('alnum', 5));
@@ -270,9 +270,9 @@ class User_model extends CI_Model
                     ->from('gsp_user')
                     ->where('email', $this->input->post('val'))
                     ->get()->row();
-		        $this->db->where('coemail',$this->input->post('val'));
-		        $this->db->update('gsp_school',array("forgetpassword_email_date"=>date("Y-m-d")));   
-		        $this->load->library('email');
+            $this->db->where('coemail',$this->input->post('val'));
+            $this->db->update('gsp_school',array("forgetpassword_email_date"=>date("Y-m-d")));   
+            $this->load->library('email');
                 $config['mailtype'] = 'html';
                 $this->email->initialize($config);
                 $from = "support@greenschoolsprogramme.org";
@@ -288,7 +288,7 @@ class User_model extends CI_Model
                 $this->email->subject($subject);
                 $this->email->message($msg);
                 $this->email->send();
-		        return $varNewPass;
+            return $varNewPass;
                 } else {
                    return false;
                 }
@@ -296,31 +296,37 @@ class User_model extends CI_Model
                  return false;
         }
     }*/
-	public function forgetPassword()
+  public function forgetPassword($tt)
     {
+      
     date_default_timezone_set('Asia/Kolkata');
         $this->load->helper('string');
     
-        $email_count =$this->db->select("id")->from('gsp_user')->get()->row();
+        $email_count =$this->db->select("id")->from('gsp_user')->get()->result();
       if (count($email_count)>0) {
-        $this->db->like('coemail',$this->input->post('val'));
+        $this->db->like('coemail',$tt);
           $this->db->like('forgetpassword_email_time',date("Y-m-d"));
-              $dataResult = $this->db->select("*")->from('gsp_school')->get()->row();
-          $update_counter=$dataResult->counter+1; 
-        $this->db->like('coemail',$this->input->post('val'));
+              $dataResult = $this->db->select("counter")->from('gsp_school')->get()->result();
+              
+          $update_counter=$dataResult[0]->counter+1; 
+        $this->db->where('coemail',$tt);
        $this->db->update('gsp_school',array("forgetpassword_email_time"=>date("Y-m-d"),"counter"=>$update_counter));  
-         if($dataResult->counter>=1){
-           return $dataResult->counter;
+
+         if($dataResult[0]->counter>=1){
+
+           return "You Can Send Request For Forgetpassword Only Once in A Day. Please Send Next Query After Half An Hour & If You Have Any Doubts, Mail support@greenschoolsprogramme.org OR CALL 011-4061600";
          }
          else{
+          echo "Your password request has been accepted and the new password will be sent to the registered email address of the GSP co-ordinator in the next five - ten minutes. If the password is not received in the next 10 minutes, then please check the spam folder. If not in the spam folder, then please send an email saying that you haven't received the new password to: support@greenschoolsprogramme.org, Please note that passwords can be changed only once in a day.";
     $query = $this->db->select('*')
                     ->from('gsp_user')
                     ->like('email', $this->input->post('val'))
                     ->get()->row();      
                  $varNewPass = strtolower(random_string('alnum', 5));
                  $arr = array('password' => $varNewPass);
-                 $this->db->like('email',$this->input->post('val'));
-               $this->db->update('gsp_user', $arr);		 
+                 $this->db->like('email',$tt);
+               $this->db->update('gsp_user', $arr); 
+
                $this->load->library('email');
              
 
@@ -340,6 +346,7 @@ class User_model extends CI_Model
                  $this->email->initialize($config);
                  $from = "support@greenschoolsprogramme.org";
                  $to =array($query->email,"support@greenschoolsprogramme.org","Studiotesseractst@gmail.com","ranjita@cseindia.org");
+                 $subject = "GSP Forget Password";
                  $msg = "Dear &nbsp;";
                  $msg .= $query->username . "," . "<br><br>";
                  $msg .= "Your new password for GSP Login.<br><br>";
