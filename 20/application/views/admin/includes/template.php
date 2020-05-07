fb6f328c8b170346efee857ec41724cd35e0e03c
yhamender <yhamender@gmail.com>
2020-05-08 04:33:37 +0530

    fg

--- 20/application/views/admin/includes/template.php
+++
@@ -1,3 +0,0 @@
-<?php $this->load->view('admin/includes/header');?>
-<?php $this->load->view($main); ?>
-<?php $this->load->view('admin/includes/footer');?>
--- 20/scripts/gitpull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Hello, World!"
-
-git pull origin master
-service nginx restart
-
-echo "Done with the pull and nginx restart"
