7e666a314cd44412cd4ec558a6abcc1cc154d87b
yhamender <30542583+yhamender@users.noreply.github.com>
2020-05-21 14:57:47 +0530

    code change
    code change

--- 20/application/controllers/admin/Audit_started.php
+++ 20/application/controllers/admin/Audit_started.php
@@ -287,7 +287,8 @@ class Audit_started extends CI_Controller {
         $bySchoolName = $this->input->post('schoolname');
         $rating = $this->input->post('rating');
         $byAlphabate = $this->input->post('alphabate');
-        $byBoard=$this->input->post('board');   
+        $byBoard=$this->input->post('board');
+	$byPartner=$this->input->post('partner_status');
        // $query = "SELECT a.id, a.udise, a.userid, a.name, a.country, a.state, a.district, a.city, a.progress, $filed, b.name AS state_name, c.name AS district_name, d.remark, d.phase FROM gsp_school AS a LEFT JOIN states AS b ON a.state=b.id LEFT JOIN cities AS c ON a.district=c.id LEFT JOIN tbl_sendmail AS d ON a.id=d.school_id  WHERE";
       // $query = "SELECT a.id,a.school_id,a.Q1S1,a.Q2G1,a.Q9G1, a.udise, a.userid,a.country, a.name, a.city, a.progress, $filed, b.name AS state_name, c.name AS district_name, a.remark, a.phase FROM tbl_sendmail AS a LEFT JOIN states AS b ON a.state=b.id LEFT JOIN cities AS c ON a.district=c.id  WHERE";
 //      $query = "SELECT a.date_added,a.address1,a.coname,a.address2,a.pincode,a.std,a.telephone,a.schoolemail,a.principle_name,a.coemail,a.comobile,a.password,a.mobile,a.principle_name,a.id,a.Q3G1,a.Q1S1,a.Q2G1,a.Q9G1, a.udise, a.userid,a.country, a.name, a.city, a.progress, $filed, b.name AS state_name, c.name AS district_name, a.remark, a.phase FROM gsp_filter AS a LEFT JOIN states AS b ON a.state=b.id LEFT JOIN cities AS c ON a.district=c.id  WHERE"; 
