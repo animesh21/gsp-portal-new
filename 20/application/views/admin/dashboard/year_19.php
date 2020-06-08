a106f6dc93973f00ddfef58ac46cf1a1ef0e2c37
yhamender <30542583+yhamender@users.noreply.github.com>
2020-06-08 16:07:47 +0530

    code change
    code change

--- 20/application/views/admin/dashboard/year_19.php
+++ 20/application/views/admin/dashboard/year_19.php
@@ -7,7 +7,7 @@
     </a>    
     <div class="title">Gsp Audit Phase 1</div>
 </div>
-<div class="schools-statics"> <!-- <a href="#"> --> <a href="<?php base_url()?>audit_pahse_2" target="_blank">
+<!-- <div class="schools-statics"> <a href="<?php base_url()?>audit_pahse_2" target="_blank">
         <div class="statics-circle"> <span>
 		 1
 		</span>
@@ -25,3 +25,4 @@
     </a>
     <div class="title">Gsp Audit 2019 Combined</div>
 </div>
+ -->
