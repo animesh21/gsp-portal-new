e9abb39816b34e164dec6be8a355d8d61914827b
yhamender <30542583+yhamender@users.noreply.github.com>
2020-06-10 13:51:31 +0530

    sx
    xs

--- 20/application/views/footer.php
+++ 20/application/views/footer.php
@@ -91,7 +91,7 @@ $("ol li").click(function(){
    <li class="portal"><a href="http://www.indiaenvironmentportal.org.in/" target="_blank"><img src="<?php echo base_url(); ?>assets/img/portal.jpg" class="img-responsive"></a></li>
    <li class="earth"><a href="http://www.downtoearth.org.in/" target="_blank"><img src="<?php echo base_url(); ?>assets/img/earth.jpg" class="img-responsive"></a></li>
    <li class="rain"><a href="http://www.rainwaterharvesting.org/" target="_blank"><img src="<?php echo base_url(); ?>assets/img/rain.jpg" class="img-responsive"></a></li>
-   <li class="times"><a href="http://www.gobartimes.org/" target="_blank"><img src="<?php echo base_url(); ?>assets/img/times.jpg" class="img-responsive"></a></li>
+   <li class="times"><a href="https://www.downtoearth.org.in/category/young" target="_blank"><img src="<?php echo base_url(); ?>assets/img/times.jpg" class="img-responsive"></a></li>
   </ul>
  </div-->
   <div class="text-center" id="copyright">Copyright © 2017 Centre for Science and Environment</div>
