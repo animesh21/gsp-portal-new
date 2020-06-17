677c48b6ad4a1e7f4ab650ff061196999b6cb23d
yhamender <30542583+yhamender@users.noreply.github.com>
2020-06-17 13:18:15 +0530

    code change
    code change

--- 20/application/views/food.php
+++ 20/application/views/food.php
@@ -310,7 +310,7 @@ style="width: 45px; height: 40px; margin-top:47px;"></a></div> -->
 <?php echo form_open('food/set', array('id' => 'food')); ?>
 <div class="form-group">
 <label>
-<h6> Task 1: Choose your Food Audit Team </h6>
+<h6> Task 1: Choose your Food Audit Team <a class="tt" data-tooltip="The first name of need to have both first and last name. In the rest of the rows, if a team member does not have a last name, then they can press the space bar once and move to the next field."><span class="badge">?</span></a> </h6>
 </label>
 <br>
 You would require at least 5 to 10 of your schoolmates in the Food audit team. The teacher co-ordinator may
