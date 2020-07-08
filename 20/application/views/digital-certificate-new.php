0df3219ef917d2d4c28f1d3f4cbcc1a8d6691bc6
yhamender <30542583+yhamender@users.noreply.github.com>
2020-07-08 10:22:27 +0530

    code change
    code change

--- 20/application/views/digital-certificate-new.php
+++ 20/application/views/digital-certificate-new.php
@@ -16,15 +16,15 @@
 
 		if ($teacher->name_of_participant !="") {
 		    echo "<div class='container'>
-		    <div class='image'><img src='assets/img/images/Certificate-new.jpg' width='1000' height='700'></div>";
+		    <div class='image'><img src='assets/img/images/certificate_gsp_ecamp.jpg' width='1000' height='700'></div>";
 
   echo "<div class='principal'>" . strtoupper($teacher->name_of_participant) ."</div>&nbsp;";
     $length = strlen($teacher->school_name.$teacher->district.$teacher->state);
   if($length > 61) {
-  echo	"<div class='school_name1'>". strtoupper($teacher->school_name).', '.strtoupper($teacher->district).', '.strtoupper($teacher->state). "</div>";
-  echo "<div class='airquality'> To find out the air quality in your city in real-time, <br/> you can visit CPCB (Central Pollution Control Board) <br/> website: <a href='https://app.cpcbccr.com/AQI_India/'>https://app.cpcbccr.com/AQI_India/</a> </div>";
+  echo	"<div class='school_name1'>". strtoupper($teacher->school_name)."</div>";
+  
   }else{
-  	echo "<div class='school_name'>". strtoupper($teacher->school_name).', '.strtoupper($teacher->district).', '.strtoupper($teacher->state). "</div>";
+  	echo "<div class='school_name'>". strtoupper($teacher->school_name). "</div>";
   	echo "<div class='airquality'> To find out the air quality in your city in real-time, <br/> you can visit CPCB (Central Pollution Control Board) <br/> website: <a href='https://app.cpcbccr.com/AQI_India/'>https://app.cpcbccr.com/AQI_India/</a> </div>";
   }
   
@@ -52,10 +52,10 @@ body{
  
 
 .certificateno{ position:absolute; top:670px; left: 10px;}
-.principal{ position:relative; text-align: center; top:330px;}
-.school_name1{ position:relative; text-align: center; top:328px; font-size: 17px; margin-left: 25px;}
+.principal{ position:relative; text-align: center; top:345px;}
+.school_name1{ position:relative; text-align: center; top:355px; font-size: 17px; margin-left: 25px;}
 .school_name{ position:relative; text-align: center; top:325px; margin-left: 25px;}
-.airquality{ position:relative; text-align: left; top:520px; margin-left: 22px; font-size: 13px;}
+.airquality{ position:relative; text-align: left; top:510px; margin-left: 22px; font-size: 13px;}
 .image{position:absolute; margin-left: -40px; margin-top: -20px;}
 </style>
 
