393422e06d72bd798ce142a10ba1e8d94e6210ce
yhamender <30542583+yhamender@users.noreply.github.com>
2020-07-13 12:36:57 +0530

    code change
    code change

--- 20/application/views/digital-certificate-new.php
+++ 20/application/views/digital-certificate-new.php
@@ -2,34 +2,34 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title><?php echo $title; ?></title>
+  <title><?php echo $title; ?></title>
     </head>
     <body>
-	<?php
-	if (!empty($teachers)) {
-	    $i=0;
-	    foreach($teachers as $teacher) {
+  <?php
+  if (!empty($teachers)) {
+      $i=0;
+      foreach($teachers as $teacher) {
 
-		if ($teacher->name_of_participant !="") {
-		    echo "<div class='container'>
-		    <div class='image'><img src='assets/img/images/certificate_gsp_ecamp.jpg' width='1000' height='700'></div>";
+    if ($teacher->name_of_participant !="") {
+        echo "<div class='container'>
+        <div class='image'><img src='assets/img/images/certificate_gsp_ecamp.jpg' width='1000' height='700'></div>";
 
   echo "<div class='principal'>" . strtoupper($teacher->name_of_participant) ."</div>&nbsp;";
-    $length = strlen($teacher->school_name.$teacher->district.$teacher->state);
+    $length = strlen($teacher->school_name);
   if($length > 61) {
-  echo	"<div class='school_name1'>". strtoupper($teacher->school_name)."</div>";
+  echo  "<div class='school_name1'>". strtoupper($teacher->school_name)."</div>";
   
   }else{
-  	echo "<div class='school_name'>". strtoupper($teacher->school_name). "</div>";
-  	echo "<div class='airquality'> To find out the air quality in your city in real-time, <br/> you can visit CPCB (Central Pollution Control Board) <br/> website: <a href='https://app.cpcbccr.com/AQI_India/'>https://app.cpcbccr.com/AQI_India/</a> </div>";
+    echo "<div class='school_name'>". strtoupper($teacher->school_name). "</div>";
+     
   }  
         echo "<div class='certificateno'>Certificate No : "."GSP Skill Building E-Camp/2020/0".$teacher->id." </div>
    </div> ";
-		}
-	  }
-	}
+    }
+    }
+  }
 
-	?>
+  ?>
 
     </body>
 </html>
@@ -42,17 +42,16 @@
 }
 
 body{
-	font-family: mtcorsva; font-size: 20px;
+  font-family: mtcorsva; font-size: 20px;
 } 
 
 .certificateno{ position:absolute; top:670px; left: 10px;}
 .principal{ position:relative; text-align: center; top:345px;}
-.school_name1{ position:relative; text-align: center; top:355px; font-size: 17px; margin-left: 25px;}
-.school_name{ position:relative; text-align: center; top:325px; margin-left: 25px;}
-.airquality{ position:relative; text-align: left; top:510px; margin-left: 22px; font-size: 13px;}
+.school_name1{ position:relative; text-align: center; top:355px; font-size: 20px; margin-left: 25px;}
+.school_name{ position:relative; text-align: center; top:355px; margin-left: 25px;}
 .image{position:absolute; margin-left: -40px; margin-top: -20px;}
 </style>
 
-<style type="text/css">		
-		.page{ text-align:right; page-break-after:always;}
+<style type="text/css">   
+    .page{ text-align:right; page-break-after:always;}
 </style>
