8db400727eb42acec9586ae66511f571dd92d3ea
yhamender <30542583+yhamender@users.noreply.github.com>
2020-07-23 17:17:40 +0530

    code change
    code change

--- 20/application/controllers/Audit_home_scorecard.php
+++ 20/application/controllers/Audit_home_scorecard.php
@@ -24,13 +24,52 @@ class Audit_home_scorecard extends CI_Controller{
         $mobile = $this->input->post('parent_mob');
         $this->db->where("parent_mob",$mobile);
         $dataCertificate=$this->db->select("*")->from("gsp_audit_home_scorecard")->get()->result();
-         
+
 
         // $this->session->set_flashdata('data_name', 'The link to download the GSP Online Air Seminar April 2020 certificate is now closed.');
         //     return redirect('GetCertificate');
 
         if(!empty($dataCertificate)){
+       if(count($dataCertificate)==1){
+        $data['teachers'] = $dataCertificate;
+
+        $data['title']="GSP Student Audit Home Score";
+        $this->load->library('dompdf_lib');
+        ini_set('memory_limit', '-1');
        
+        $html = $this->load->view('gsp_audit_home_scorecard', $data, true);
+        $html = preg_replace('/>\s+</', "><", $html);
+
+        $this->dompdf->load_html($html);
+        $this->dompdf->set_paper('A4', 'portrait');
+        $this->dompdf->render();
+        $this->dompdf->stream("Student Audit Home Score.pdf", array("Attachment" => false));
+        $this->db->where('id', $dataCertificate[0]->id);
+        $this->db->update('gsp_audit_home_scorecard', array('download_count'=>$dataCertificate[0]->download_count+1));
+        }else{
+            $data['fullstudent'] = $dataCertificate;
+            $this->load->view('audit_home_link', $data);
+             
+
+        }
+    }
+        else{
+            $this->session->set_flashdata('data_name', 'Your password is incorrect. Please try again. For any assistance, write to us at : support@greenschoolprogramme.org');
+            return redirect('Audit_home_scorecard');
+        }
+
+    }
+
+
+    public function getAuditScoreByName(){
+         $id = $_GET['id'];
+
+         // $mobile = $this->input->post('parent_mob');
+        $this->db->where("id",$id);
+        $dataCertificate=$this->db->select("*")->from("gsp_audit_home_scorecard")->get()->result(); 
+
+        if(!empty($dataCertificate)){
+       if(count($dataCertificate)==1){
         $data['teachers'] = $dataCertificate;
 
         $data['title']="GSP Student Audit Home Score";
@@ -47,14 +86,9 @@ class Audit_home_scorecard extends CI_Controller{
         $this->db->where('id', $dataCertificate[0]->id);
         $this->db->update('gsp_audit_home_scorecard', array('download_count'=>$dataCertificate[0]->download_count+1));
         }
-        else{
-            $this->session->set_flashdata('data_name', 'Your password is incorrect. Please try again. For any assistance, write to us at : support@greenschoolprogramme.org');
-            return redirect('Audit_home_scorecard');
-        }
-
-    }
-
+}
 
 
 }
 
+}
