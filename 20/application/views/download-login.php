4f2835730f3ad2a1ad150f249ae751b5f901f290
yhamender <30542583+yhamender@users.noreply.github.com>
2020-08-03 17:16:56 +0530

    code change
    code change

--- 20/application/views/download-login.php
+++ 20/application/views/download-login.php
@@ -31,8 +31,8 @@
             .alert-danger {
                 border: 2px solid red;
             }
-			.btn-danger{ background:#e86549; border:1px solid #e86549;}
-			.btn-danger:hover{ background:#e86549; border:1px solid #e86549;}
+      .btn-danger{ background:#e86549; border:1px solid #e86549;}
+      .btn-danger:hover{ background:#e86549; border:1px solid #e86549;}
         </style>
 </head>
 <body class="lang-en groupbygroup showqnumcode-X">
@@ -106,11 +106,11 @@
     <script src="http://www.greenschoolsprogramme.org/audit2017/assets/js/jquery.min.js"></script>
     <script src='http://www.greenschoolsprogramme.org/audit2017/assets/js/badge-code.js'></script>
     <script>
-			/*School Badge Code*/
-			window.onload = function(){
-			  get_date_difference('12-24-2017');
-			}
-			</script>
+      /*School Badge Code*/
+      window.onload = function(){
+        get_date_difference('12-24-2017');
+      }
+      </script>
   </div>
 </div>
 <p class="m-browser">Please use a modern browser such as <a href="https://www.mozilla.org/en-US/firefox/new/"
@@ -178,16 +178,17 @@
   </div>
 </div>
 <script type="text/javascript">
-            $(document).ready(function () {
-                $('#btnForgetPassword').on('click', function () {
-                    var emailval = $('input[name="email_id"]').val();
+           $(document).ready(function () {
+                $('#btnForgetPassword').click(function () {
+               var emailval = $('input[name="email_id"]').val();
                     
                     $.ajax({
                       url: "<?php echo base_url('login/forgetpassword');  ?>",
                       type:"POST",
                       data: {"emailval":emailval},
-                      success: function(resp){
-                        alert(resp);
+                      dataType:'Json',
+                      success: function(status){
+                        alert(status);
                       }
 
                     });
@@ -196,30 +197,10 @@
                         $('#msgForget').html('<p style="color:red; font-weight:bold">Please enter email address !</p>');
                     } else if (!pattern.test(emailval)) {
                         $('#msgForget').html('<p style="color:red; font-weight:bold">Please enter a valid email address !</p>');
-                    } else {
-                        record = {
-                            val: emailval
-                        };
-                        $.post('<?php echo base_url('login/forgetpassword') ?>', record, function (data) {
-
-                            console.log(data);
-                            if (data == "success") {
-                                $('#msgForget').html('<div class="alert alert-success">' +
-                                        '<strong>&#10004; Success!</strong> New password successfully send to your email !.' +
-                                        '</div>');
-                            } else if (data == "error") {
-                                $('#msgForget').html('<div class="alert alert-danger">' +
-                                        '<strong>&#x2716; Error!</strong> The email you have entered is not registered.' +
-                                        '</div>');
-                            }
-                            setTimeout(function () {
-                                location.reload();
-                            }, 2000);
-                        }, 'json');
-                    }
+                    }  
                 });
             });
-	</script>
+  </script>
     </body>
-		
+    
 </html>
