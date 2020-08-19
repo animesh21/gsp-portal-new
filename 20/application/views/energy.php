f8a0fa54d280c10c07c48491ab61913e389147e7
yhamender <30542583+yhamender@users.noreply.github.com>
2020-08-19 11:44:03 +0530

    code change
    code change

--- 20/application/views/energy.php
+++ 20/application/views/energy.php
@@ -90,7 +90,7 @@
                         style="width: 45px; height: 40px; margin-top:47px;"></a></div> -->
   <p> Prior to answering these questions, please consider using a meter to measure the monthly average amount
     of energy used from the respective energy sources.</p>
-  <p class="unit"><strong>The unit of measurement to be used in the Energy section is megajoule (mj).</strong> </p>
+  <p class="unit"><strong>The unit of measurement to be used in the Energy section is megajoule (MJ).</strong> </p>
 </div>
 <?php echo form_open('energy/set', array('id' => "energy")); ?>
 <!---Store Value For Vidation----->
@@ -604,10 +604,15 @@
       <label>Quantity consumed/monthly average</label>
     </div>
     <div class="col-xs-3">
-      <label>Energy consumed in Megajoules (mj)</label>
+      <label>Energy consumed in Megajoules (MJ)</label>
     </div>
     <div class="col-xs-3">
       <label>Type of use <a class="tt" data-tooltip="Type of use/purpose (mention the type of use in the above table): 1. Lighting 2. Housekeeping 3. Cooking 4. Transport 5. Teaching/ learning aid  6. Burning/Incineration of Waste. If your school does not use a particular source of energy, enter 0."><span class="badge">?</span></a> </label>
+      <style type="text/css">
+      .tooltip:after, [data-tooltip]:after{
+        min-width: 300px;
+      }
+    </style>
     </div>
   </div>
   <div class="form-group1 row">
