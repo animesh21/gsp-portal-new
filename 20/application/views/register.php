6fbdb17bb8450f6f4af87675e224df6ba0be80fc
yhamender <30542583+yhamender@users.noreply.github.com>
2020-09-01 13:44:32 +0530

    code change
    code change

--- 20/application/views/register.php
+++ 20/application/views/register.php
@@ -236,7 +236,7 @@ p{
 <body>
 <div id="loader"><img src="<?php echo base_url(); ?>assets/img/loader.gif" width="64" height="64"></div>
 <div class="container">
-  <header> <a href="http://www.greenschoolsprogramme.org/registration/"> <img src="<?php echo base_url(); ?>assets/img/header-2.jpg" width="1028" height="117"></a> <img src="<?php echo base_url(); ?>assets/img/icons.png" class="icon" style="height:110px" > </header>
+  <header> <a href="http://www.greenschoolsprogramme.org/"> <img src="<?php echo base_url(); ?>assets/img/logo.png" style="padding: 20px 1px 20px 1px;"></a> <img src="<?php echo base_url(); ?>assets/img/top-bnr.png" class="icon" style="height:110px;" > </header>
 </div>
 <main class="bdr">
 <div class="container">
