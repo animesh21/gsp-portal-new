9d1e188b35057cdee3d349c7ac94b6a75321432c
yhamender <yhamender@gmail.com>
2020-10-26 01:43:25 +0530

    remove files

--- GSP/2015/regist.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-session_unset();
-//$page = "index.php?proc=out";
-$page = "index.php";
-header("location:$page");
-?>
--- GSP/regist.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-session_unset();
-$page = "index.php?proc=out";
-header("location:$page");
-?>
