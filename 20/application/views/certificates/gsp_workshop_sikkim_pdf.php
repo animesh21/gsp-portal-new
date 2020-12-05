47cf99926a8b503e829a7da7075ff1760977b3c8
yhamender <yhamender@gmail.com>
2020-12-06 01:42:22 +0530

    sdf

--- 20/application/views/certificates/gsp_workshop_sikkim_pdf.php
+++ 20/application/views/certificates/gsp_workshop_sikkim_pdf.php
@@ -47,9 +47,9 @@ body{
 } 
 /* .date{position:relative; text-align: center; top:490px; margin-left: 480px;} */
 .certificateno{ position:absolute; top:633px; left: 10px;}
-.principal{ position:relative; text-align: center; top:278px;}
-.school_name1{ position:relative; text-align: center; top:225px; font-size: 16px; margin-left: 25px;}
-.school_name{ position:relative; text-align: center; top:225px; margin-left: 25px;}
+.principal{ position:relative; text-align: center; top:268px;}
+.school_name1{ position:relative; text-align: center; top:265px; font-size: 16px; margin-left: 25px;}
+.school_name{ position:relative; text-align: center; top:265px; margin-left: 25px;}
 .image{position:absolute; margin-left: -40px; margin-top: -45px;}
 </style>
 
