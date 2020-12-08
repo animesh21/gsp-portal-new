465509db9f9b1ebad5764a876a3f74c32b5e2450
yhamender <yhamender@gmail.com>
2020-12-09 01:01:26 +0530

    hame

--- Audit@Home/application/views/footer.php
+++ Audit@Home/application/views/footer.php
@@ -2,7 +2,7 @@
 <footer>
   <div class="footer-bottom bg-success text-center">
     <div class="container">
-      <p class="mb-0" style="color: white; font-size: 16px;">&copy; <?php echo date("Y"); ?> Centre for Science and Environment. For help, email <a href="mailto:ranjita@cseindia.org">support@greenschoolsprogramme.org</a> or call 011-4061600, ext – 219, 300. </p>
+      <p class="mb-0" style="color: white; font-size: 16px;">&copy; <?php echo date("Y"); ?> Centre for Science and Environment. For help, email <a href="mailto:ranjita@cseindia.org">support@greenschoolsprogramme.org</a> </p>
     </div>
   </div>
 </footer>
