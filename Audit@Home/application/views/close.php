4594a97be1f52d567e37e036eb1c4d2851acfcd4
yhamender <30542583+yhamender@users.noreply.github.com>
2020-12-14 12:44:06 +0530

    a
    a

--- Audit@Home/application/views/close.php
+++ Audit@Home/application/views/close.php
@@ -18,17 +18,7 @@ For those who have submitted the survey successfully, a scorecard will be emaile
          </div>      
          
 
-          <div class="card-body">
-            <?php echo form_open('login',array('id'=>'frmLogin', 'class'=>'px-lg-4')); ?>
-              <div class="input-group input-group--focus mb-3">
-                <div class="input-group-prepend">
-                  <span class="input-group-text bg-white ti-email"></span>
-                </div>
-                <p></p>
-                
-           <?php echo form_close(); ?>
-          </div>
-        </div>
+          
       </div> 
     </div> <!-- END row-->
   </div> <!-- END container-->
