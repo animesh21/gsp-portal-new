6cce75f8c0667ed62a41de17fb94d23a283bc2e3
yhamender <yhamender@gmail.com>
2020-12-23 11:11:37 +0530

    df

--- 18/application/models/User_model.php
+++ 18/application/models/User_model.php
@@ -43,8 +43,9 @@ class User_model extends CI_Model
    
    public function UserLoginDownload($argPost)
     {
-		$msg=''; 
-        $query = $this->db->select('a.*, a.username AS username, a.id AS id, a.status AS status, b.complete_status AS complete_status, b.progress AS progress')
+        $msg=''; 
+        $db2 = $this->load->database('db2', TRUE);
+        $query = $db2->select('a.*, a.username AS username, a.id AS id, a.status AS status, b.complete_status AS complete_status, b.progress AS progress')
             ->from('gsp_user As a')
             ->join('gsp_school AS b', 'b.coemail=a.email', 'left') 		
             ->where(array('a.email' => $argPost['email'], 'a.password' => $argPost['password'], 'b.progress'=>'100', 'b.complete_status'=>'1'))
