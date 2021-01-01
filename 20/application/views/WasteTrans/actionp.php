c20738975e1897e77a3c934e2db9afbc4fdb89ac
yhamender <yhamender@gmail.com>
2021-01-02 00:53:32 +0530

    sdf

--- 20/application/views/WasteTrans/actionp.php
+++ 20/application/views/WasteTrans/actionp.php
@@ -504,26 +504,15 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($files as $f) { ?>
-            <tr id="index<?php echo $f->id; ?>">
-            <?php 
-                        
-                    $array = explode('.',$f->file_name); 
-                $count = count($array);
-            $extension = $array[$count-1];
-                    ?>
-            <?php if($extension == "jpg" || $extension == "jpeg"){ ?>
-            <td><img style="width:62px; height:46px;" src="<?php echo base_url() ?>uploads/files/<?php echo $f->file_name ?>" class="img-responsive" /></td>
-            <?php }else{ ?>
-            <td><img style="width:62px; height:46px;" src="<?php echo base_url() ?>assets/img/download.jpg" class="img-responsive" /></td>
-            <?php }?>
-            <?php $name = str_replace(" ", "_", $f->name . "_Supporting_Document_Air_"); ?>
-            <td class="upload edit"><?php echo str_replace($name, "", $f->file_name); ?></td>
-            <td><a href="javascript:void(0)" class="air-delete-files" data-id="<?php echo $f->id; ?>"><img src="<?php echo base_url(); ?>assets/front/images/delete.png" style="position:relative; top:5px" /></a></td>
-            <td><a href="<?php echo base_url() ?>uploads/files/<?php echo $f->file_name; ?>" download="<?php echo $f->file_name; ?>"><span class="glyphicon glyphicon-download-alt"></span></a></td>
-            </tr>
-            <?php } ?>
-        </tbody>
+        <?php foreach ($supportDocWater as $f) { ?>
+        <tr id="index<?php echo $f->id; ?>">
+          <?php $name = str_replace(" ", "_", $f->name . "_Supporting_Document_Water_"); ?>
+          <td class="upload edit"><?php echo str_replace($name, "", $f->file_name); ?></td>
+          <td><a href="javascript:void(0)" class="air-delete-files" data-id="<?php echo $f->id; ?>"><img src="<?php echo base_url(); ?>assets/front/images/delete.png" style="position:relative; top:5px" /></a></td>
+         <td><a href="<?php echo base_url() ?>uploads/files/<?php echo $f->file_name; ?>" download="<?php echo $f->file_name; ?>"><span class="glyphicon glyphicon-download-alt"></span></a></td>
+	  </tr>
+        <?php } ?>
+      </tbody>
         </table>
 
 
