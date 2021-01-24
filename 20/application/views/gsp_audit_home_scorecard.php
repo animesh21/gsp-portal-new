3433428775f4f7b17ad6ae4bd67a0cdee35fc86b
yhamender <yhamender@gmail.com>
2021-01-25 01:35:27 +0530

    df

--- 20/application/views/gsp_audit_home_scorecard.php
+++ 20/application/views/gsp_audit_home_scorecard.php
@@ -13,10 +13,20 @@
     if ($teacher->participant_name !="") {
         echo "<div class='container'>
         <div class='image'><img src='assets/img/images/page-1.jpg' width='794' height='1100'></div>";
+  $parlen = strlen($teacher->participant_name);
+  if($parlen > 30){
+    echo "<div class='principal1'>" . strtoupper($teacher->participant_name) ."</div>&nbsp;";
 
-  echo "<div class='principal'>" . strtoupper($teacher->participant_name) ."</div>&nbsp;";
+  }
+  else{
+    echo "<div class='principal'>" . strtoupper($teacher->participant_name) ."</div>&nbsp;";
+
+  }
+
+
+  // echo "<div class='principal'>" . strtoupper($teacher->participant_name) ."</div>&nbsp;";
     $length = strlen($teacher->school_name);
-  if($length < 25) {
+  if($length > 61) {
   echo  "<div class='school_name1'>". strtoupper($teacher->school_name." ".$teacher->state )."</div>";
   
   }else{
@@ -91,6 +101,7 @@ body{
 .link-4{ position:absolute; top:368px; left: 125px; font-size:16px; font-weight:bold;}
 .link-5{ position:absolute; top:425px; left: 315px; font-size:16px; font-weight:bold;}
 .principal{ position:relative; text-align: center; top:565px; font-size: 18px;}
+.principal1{ position:relative; text-align: center; top:565px; font-size: 14px;}
 .finalScore{ position:relative;top:295px; left: 242px; font-weight: bold; font-size: 25px;}
 .school_name1{ position:relative; text-align: center; top:562px; font-size: 14px;}
 .school_name{ position:relative; text-align: center; top:562px; font-size: 18px;}
