ba5648130b4efb148505ebb2013c8d0b1fb56e13
yhamender <yhamender@gmail.com>
2021-01-25 01:36:13 +0530

    df

--- 20/application/views/gsp_audit_home_scorecard.php
+++ 20/application/views/gsp_audit_home_scorecard.php
@@ -101,7 +101,7 @@ body{
 .link-4{ position:absolute; top:368px; left: 125px; font-size:16px; font-weight:bold;}
 .link-5{ position:absolute; top:425px; left: 315px; font-size:16px; font-weight:bold;}
 .principal{ position:relative; text-align: center; top:565px; font-size: 18px;}
-.principal1{ position:relative; text-align: center; top:565px; font-size: 14px;}
+.principal1{ position:relative; text-align: center; top:565px; font-size: 15px;}
 .finalScore{ position:relative;top:295px; left: 242px; font-weight: bold; font-size: 25px;}
 .school_name1{ position:relative; text-align: center; top:562px; font-size: 14px;}
 .school_name{ position:relative; text-align: center; top:562px; font-size: 18px;}
