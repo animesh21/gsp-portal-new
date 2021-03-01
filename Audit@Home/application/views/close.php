e27dc2701fdcd268443fa121de6ed4713eac49f8
yhamender <yhamender@gmail.com>
2021-03-01 11:46:43 +0530

    sd

--- Audit@Home/application/views/close.php
+++ Audit@Home/application/views/close.php
@@ -15,8 +15,11 @@
          <div class="card-header border-bottom">
           <h4 class="mt-4">
             
-            <p style="font-size:18px">GSP Audit@Home is now closed.</br>
-For those who have submitted the survey successfully, a scorecard will be emailed to you by 11 Jan 2020. For any further queries, please visit<span style="color:blue"> <a href="https://cdn.cseindia.org/userfiles/gsp-FAQs.pdf">here.</a></span>
+            <p style="font-size:18px"><span style="font-size:20px; font:bold;">GSP Audit@Home: Waste Warriors is now closed.</span></br>
+            
+For those who have submitted the survey successfully, a scorecard will be emailed to you by April 2020. For any further queries, please write to gsphomeaudit@gmail.com. 
+<!-- For those who have submitted the survey successfully, a scorecard will be emailed to you by 11 Jan 2020. For any further queries, please visit -->
+<!-- <span style="color:blue"> <a href="https://cdn.cseindia.org/userfiles/gsp-FAQs.pdf">here.</a></span> -->
  </p>
             
             
