500d69009e5fd51e19bfcf0a23f3de38c1bb856e
yhamender <yhamender@gmail.com>
2021-03-16 00:47:26 +0530

    hj

--- 20/application/views/digital-certificate-new.php
+++ 20/application/views/digital-certificate-new.php
@@ -47,8 +47,8 @@ body{
 
 .certificateno{ position:absolute; top:680px; left: 10px;}
 .principal{ position:relative; text-align: center; top:260px;}
-.school_name1{ position:relative; text-align: center; top:280px; font-size: 20px; margin-left: 25px;}
-.school_name{ position:relative; text-align: center; top:280px; margin-left: 25px;}
+.school_name1{ position:relative; text-align: center; top:250px; font-size: 20px; margin-left: 25px;}
+.school_name{ position:relative; text-align: center; top:250px; margin-left: 25px;}
 .image{position:absolute; margin-left: -40px; margin-top: -20px;}
 </style>
 
