364cde22c213afbc4046b1671ce98744b957f004
yhamender <yhamender@gmail.com>
2021-03-18 01:52:32 +0530

    df

--- 20/application/views/admin/survey/school_wt.php
+++ 20/application/views/admin/survey/school_wt.php
@@ -685,9 +685,9 @@ ol.support-docs { margin: 0px; padding:0 0 15px 15px;  }
     </ul>
     </div>
   </div>
-  
+  <div style="padding-top:10px;"></div>
   <div class="task-area">
-  <h3 class="task">The Team</h3>
+  <h3 class="task">THE TEAM</h3>
   <p>Please provide the details of the SWM Action Plan team. A teacher with interest in waste and five to 10 students from Grades 6 to 12 will be ideal for your team. With schools closed, the Administrative staff is important as they will provide the permissions required for the housekeeping and cleaning staff to help you with the plan.</p>
   <p>Please enter the details below carefully as the same will be printed on the digital certificates.</p>
   
