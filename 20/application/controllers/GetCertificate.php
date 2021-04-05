d63e586857c5ec53071b3c8519b2a5b48373bbcd
yhamender <yhamender@gmail.com>
2021-04-06 00:39:33 +0530

    df

--- 20/application/controllers/GetCertificate.php
+++ 20/application/controllers/GetCertificate.php
@@ -9,7 +9,9 @@ class GetCertificate extends CI_Controller{
     public function __construct() {
         parent::__construct();
         $this->load->helper(array('form', 'security'));
-        $this->load->library('form_validation');        
+        $this->load->library('form_validation');   
+        $this->load->helper(array('download', 'common_helper', 'form'));
+     
     }
     
     /*
@@ -55,40 +57,44 @@ class GetCertificate extends CI_Controller{
     }
 
 
-    public function getPoster($argID){
-        ini_set('memory_limit', '-1');      
-        ini_set('max_execution_time', 300);   
+    public function getPoster($argsID){
+        // ini_set('memory_limit', '-1');      
+        // ini_set('max_execution_time', 300);   
            $data['title'] = 'Home';
-           $data['id'] = $argID;
-        $schoolId=getSchoolId($this->session->userdata('USER_ID'));
-        $phase2 = $this->db->select('schoolno')->from('poster')->where('schoolno',$schoolId)->get()->result();
+        //    $data['id'] = $argID;
+        // $schoolId=getSchoolId($this->session->userdata('USER_ID'));
+        $mobile=42;
+        $this->db->where("schoolno",$mobile);
+        $phase2=$this->db->select("*")->from("poster")->get()->result();
         
-        print_r($phase2);
-        
-        // $this->session->set_flashdata('data_name', 'The link to download the GSP Online Air Seminar April 2020 certificate is now closed.');
-        //     return redirect('GetCertificate');
 
-        if(!empty($dataCertificate)){
 
-        $data['teachers'] = $dataCertificate;
+        if(!empty($phase2)){
 
-        $data['title']="GSP Digital Certificates";
+        $data['teachers'] = $phase2;
+        // echo'<pre>';
+        // print_r($phase2);
+
+
+        $data['title']="GSP Poster";
         $this->load->library('dompdf_lib');
         ini_set('memory_limit', '-1');
        
-        $html = $this->load->view('digital-certificate-new', $data, true);
+        $html = $this->load->view('digital-poster', $data, true);
          $html = preg_replace('/>\s+</', "><", $html);
-
+       
         $this->dompdf->load_html($html);
         $this->dompdf->set_paper(array(0, 0, 380, 560), 'portrait');
         $this->dompdf->render();
         $this->dompdf->stream("Digital Certificate.pdf", array("Attachment" => false));
-		$this->db->where('id', $dataCertificate[0]->id);
-        $this->db->update('gsp_skill_building_camp', array('download_count'=>$dataCertificate[0]->download_count+1));
+		// $this->db->where('id', $dataCertificate[0]->id);
+        // $this->db->update('gsp_skill_building_camp', array('download_count'=>$dataCertificate[0]->download_count+1));
         }
         else{
             $this->session->set_flashdata('data_name', 'Your password is incorrect. Please try again. For any assistance, write to us at : support@greenschoolprogramme.org');
             return redirect('GetCertificate');
+
+            
         }
 
     }
---
+++ 20/application/views/digital-poster.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title><?php echo $title; ?></title>
+    </head>
+    <body>
+  <?php
+  if (!empty($title)) {
+    $i=0;
+    foreach($teachers as $teacher) {
+
+    if ($teacher->school !="") {
+        echo "<div class='container'>
+        <div class='image'><img src='assets/img/images/poster.png' width='500' height='707'></div>";
+
+    echo "<div class='sc_name'>" . strtoupper($teacher->school) ."</div>&nbsp;";
+    echo "<div class='sc_state'>" . strtoupper($teacher->district) .",". strtoupper($teacher->state) ."</div>&nbsp;";
+    echo "<div class='sc_sc'>" . strtoupper($teacher->sc) ."</div>&nbsp;";
+    echo "<div class='sc_st'>" . strtoupper($teacher->st) ."</div>&nbsp;";
+    echo "<div class='sc_ll_hl'>" . strtoupper($teacher->ll) ." - ". strtoupper($teacher->hl) ."</div>&nbsp;";
+    echo "<div class='sc_p'>" . strtoupper($teacher->tp) ."</div>&nbsp;";
+    echo "<div class='sc_sw'>" . strtoupper($teacher->tswg) ."</div>&nbsp;";
+    echo "<div class='sc_pcwg'>" . strtoupper($teacher->pc) ."</div>&nbsp;";
+    echo "<div class='sc_tbwg'>" . strtoupper($teacher->tbwg) ."</div>&nbsp;";
+    echo "<div class='sc_pww'>" . strtoupper($teacher->pww) ."</div>&nbsp;";
+    echo "<div class='sc_tdwg'>" . strtoupper($teacher->tdwg) ."</div>&nbsp;";
+    echo "<div class='sc_pdw'>" . strtoupper($teacher->pdw) ."</div>&nbsp;";
+    echo "<div class='sc_towg'>" . strtoupper($teacher->towg) ."</div>&nbsp;";
+    echo "<div class='sc_pow'>" . strtoupper($teacher->pow) ."</div>&nbsp;";
+    echo "<div class='sc_ap'><p>" . html_entity_decode($teacher->ap) ."</p></div>&nbsp;";
+    echo "<div class='sc_pw'><p>" . html_entity_decode($teacher->pw) ."</p></div>&nbsp;";
+    echo "<div class='sc_si'><p>" . html_entity_decode($teacher->si) ."</p></div>&nbsp;";
+
+
+  echo "<div class='principal'>" . strtoupper($teacher->school_name) ."</div>&nbsp;";
+    $length = strlen($teacher->school_name);
+  if($length > 61) {
+  echo  "<div class='school_name1'>". strtoupper($teacher->name_of_participant)."</div>";
+  
+  }else{
+    echo "<div class='school_name'>". strtoupper($teacher->name_of_participant). "</div>";
+     
+  }  echo "<div class='date'>"  . strtoupper($teacher->state). "</div> ";
+  //       echo "<div class='certificateno'>Certificate No : "."GSP".$teacher->school." </div>
+  //  </div> ";
+    }
+    }
+}
+
+  ?>
+
+    </body>
+</html>
+
+
+<style type="text/css">
+          @font-face {
+  font-family: myFirstFont;
+  src: url(18/assets/scriptina/mtcorsva.ttf);
+}
+
+body{
+  font-family: mtcorsva; font-size: 20px;
+} 
+
+
+.sc_sc{position:absolute;text-align:center;top:208px; margin-left: 50px;font-size: 9px;color:#41ad49;}
+.sc_st{position:absolute;text-align:center;top:233px; margin-left: 50px;font-size: 9px;color:#41ad49;}
+.sc_ll_hl{position:absolute;text-align:center;top:183px; margin-left: 147px;font-size: 10px;color:#41ad49;}
+.sc_p{position:absolute;text-align:center;top:212px; margin-left: 147px;font-size: 10px;color:#41ad49;}
+.sc_sw{position:absolute;text-align:center;top:192px; margin-left: 243px;font-size: 9px;color:#41ad49;}
+.sc_pcwg{position:absolute;text-align:center;top:237px; margin-left: 243px;font-size: 9px;color:#41ad49;}
+.sc_name{position:absolute;text-align:center;top:110px; margin-left: 0px;font-weight:bold;width:100%;}
+.sc_state{position:absolute;text-align:center;top:135px; margin-left: 0px;font-size: 12px;color:#41ad49;width:100%;}
+.sc_tbwg{position:absolute;text-align:center;top:342px; margin-left: 83px;font-size: 9px;color:#41ad49;}
+.sc_pww{position:absolute;text-align:center;top:367px; margin-left: 83px;font-size: 9px;color:#41ad49;}
+.sc_tdwg{position:absolute;text-align:center;top:342px; margin-left: 150px;font-size: 9px;color:#41ad49;}
+.sc_pdw{position:absolute;text-align:center;top:367px; margin-left: 150px;font-size: 9px;color:#41ad49;}
+.sc_towg{position:absolute;text-align:center;top:342px; margin-left: 220px;font-size: 9px;color:#41ad49;}
+.sc_pow{position:absolute;text-align:center;top:367px; margin-left: 220px;font-size: 9px;color:#41ad49;}
+.sc_ap{position:absolute;text-align:left;top:423px; margin-left: 22px;width:270px;height:115px;font-size: 9px;}
+.sc_pw{position:absolute;text-align:left;top:543px; margin-left: 22px;width:270px;height:55px;font-size: 9px;}
+.sc_si{position:absolute;text-align:left;top:597px; margin-left: 22px;width:270px;height:55px;font-size: 9px;}
+
+
+
+
+
+
+.date{position:absolute; text-align: center; top:510px; margin-left: 480px;}
+.certificateno{ position:absolute; top:680px; left: 10px;}
+.principal{ position:relative; text-align: center; top:260px;}
+.school_name1{ position:relative; text-align: center; top:250px; font-size: 20px; margin-left: 25px;}
+.school_name{ position:relative; text-align: center; top:250px; margin-left: 25px;}
+.image{position:absolute; margin-left: -40px; margin-top: -20px;}
+</style>
+
+<style type="text/css">   
+    .page{ text-align:right; page-break-after:always;}
+</style> 
