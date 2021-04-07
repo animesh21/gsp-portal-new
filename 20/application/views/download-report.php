f32beae1023cf55aea4dd04965d72e20b2aa319d
yhamender <yhamender@gmail.com>
2021-04-07 11:26:31 +0530

    df

--- 20/application/views/download-report.php
+++ 20/application/views/download-report.php
@@ -129,6 +129,24 @@
           </div>
           <div class="col-md-4">
             <div class="schools-statics" style="width:100%;">
+             <a href="" target="_blank">
+              <div class="statics-circle"> <span><i class="fa fa-file-text-o fa-2x"></i></span> </div>
+              
+              <div class="title"><strong>Performance Report</strong></div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="schools-statics" style="width:100%;" id="flat">
+	         	<!-- <a href="<?php echo base_url('/download/getdigitalCertificate/').$this->
+              session->userdata('USER_ID') ?>" target="_blank"> -->
+              <div class="statics-circle"> <span><i class="fa fa-file-text-o fa-2x"></i></span> </div>
+             </a>
+              <div class="title"><strong>Digital Certificate</strong></div>
+            </div>
+          </div>
+          
+          <!-- <div class="col-md-4">
+            <div class="schools-statics" style="width:100%;">
              <a href="<?php echo base_url('/download/PdfById/').getSchoolId($this->session->userdata('USER_ID')); ?>" target="_blank">
               <div class="statics-circle"> <span><i class="fa fa-file-text-o fa-2x"></i></span> </div>
               
@@ -143,7 +161,8 @@
              </a>
               <div class="title"><strong>Digital Certificate</strong></div>
             </div>
-          </div>
+          </div> -->
+          
         </div>
       </div>
     </div>
