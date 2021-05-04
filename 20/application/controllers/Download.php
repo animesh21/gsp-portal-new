eb5320c9dc03486cf422e756b8fdd7ae03f14e8a
yhamender <yhamender@gmail.com>
2021-05-04 11:38:25 +0530

    df

--- 20/application/controllers/Download.php
+++ 20/application/controllers/Download.php
@@ -180,8 +180,9 @@ function getdigitalCertificate($argID) {
    $dataCertificate=$this->db->select("certificate_username,certificate_schoolname,id")
    ->from("tblcertificate")->group_by("certificate_username")->order_by('id','ASC')->get()->result();
    $data['school_certificates']=$dataCertificate;
-   $principal = $this->db->select("a.userid,a.principle_name,a.name,a.district,a.state")
+   $principal = $this->db->select("a.userid,a.principle_name,a.name,a.district,b.name as state_name")
                                  ->from("gsp_school AS a")
+                                 ->join('states as b','a.state=b.id','left')
                                  ->where("userid", $argID)
                                  ->get()
                                  ->result();
