39363f249739d5b702a105cd5e8987f1357a64a8
yhamender <yhamender@gmail.com>
2021-05-17 13:35:38 +0530

    df

--- Audit@Home/application/controllers/Login.php
+++ Audit@Home/application/controllers/Login.php
@@ -96,6 +96,33 @@ class Login extends CI_Controller {
     //         redirect('user/forgot-password');
     //     }
     // }
+
+    public function gsp()
+	{
+	    $data['title'] = 'LOGIN';
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        if ($this->form_validation->run() == FALSE) {
+         $this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
+        } else {
+            $post = $this->security->xss_clean($this->input->post());
+            $varCheckLogin = $this->Login_model->checkLogin($post);
+            $checkSubmitted = $this->User_model->UserSubmitted($post);
+             
+            if (!empty($varCheckLogin)) { 		  
+                redirect(base_url('feb/general'));
+            } elseif($checkSubmitted) { 
+                $this->session->set_flashdata('error', 'You have already submitted the survey and will not be able to login again. Please stay tuned to our emails for further updates on your scorecard! ');
+            } 
+            else 
+            { 
+                $this->session->set_flashdata('error', 'Invalid Email/Password!');
+            }
+        }
+		// $this->load->view('close');
+		$this->load->view('login');
+
+    }
 }
 
 
